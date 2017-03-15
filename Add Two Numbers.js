eedc01114628207275de7da985ed185419e9cd27
jorge.bayona <jorge.bayona@oracle.com>
2017-03-15 00:32:49 -0600

    Adding comments and test cases

--- Add Two Numbers.js
+++ Add Two Numbers.js
@@ -34,21 +34,28 @@ var addTwoNumbers = function(l1, l2) {
     }
     
     while (l1 && l2) {
+		//En la primera iteracion comienza con el primer elemento de la lista
+		//+= del val porque consideramos el carry
         val += l1.val + l2.val;
+		//No queremos numero mayores a 10, por eso sacamos el modulo, nuevo nodo
         node = new ListNode(val % 10);
         
         if (newHead) {
             newTail.next = node;
-            newTail = newTail.next;
+            newTail = newTail.next; //Esta variable tiene el ultimo nodo
         } else {
+			//En la primera iteracion newHead no esta definido y a partir de ese elemento
+			//Comenzamos a crear los nodos
             newHead = node;
             newTail = node;
         }
         
+		//Esta verificamos es para contar el acarreo
         val = (val >= 10)? 1 : 0;
+		//Avanzaremos hasta que sea null y ya no entremos al while
         l1 = l1.next;
         l2 = l2.next;
-    }
+    } //Hasta este punto ya tenemos formado nuestra lista con la suma
     
     while (l1) {
         val += l1.val;
@@ -75,3 +82,20 @@ var addTwoNumbers = function(l1, l2) {
     
     return newHead;
 };
+
+function ListNode(val, node) {
+  this.val = val;
+  this.next = node ? node : null;
+}
+
+function ListNode2(val, node){
+  this.val = val;
+  this.next = node ? node : null;
+}
+
+
+l1 = new ListNode(2,new ListNode(4,new ListNode(3)));
+l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+//console.log(l1);
+//console.log(l2);
+console.log(addTwoNumbers(l1,l2));
