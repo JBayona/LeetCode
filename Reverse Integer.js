003649f2d9db1d7ce350504b60eab9d164f15925
JBayona <jordavids_22@hotmail.com>
2017-03-17 00:19:50 -0600

    Reverse Integer updated

---
+++ Reverse Integer.js
@@ -0,0 +1,32 @@
+/*
+Reverse digits of an integer.
+
+Example1: x = 123, return 321
+Example2: x = -123, return -321
+*/
+
+
+var reverse = function(x) {
+    var isNegative = x < 0 ? true: false,
+    var result = 0,
+    var reminder = 0;
+    
+    if (isNegative) {
+        x = x * (-1);
+    }
+    
+    while (x !== 0) {
+        reminder = x % 10;
+        result = result * 10 + reminder;
+        x = Math.floor(x / 10);
+    }
+    
+    if (result >= 2147483648) {
+        return 0;
+    }
+    
+    return isNegative ? result * (-1) : result;
+};
+
+number = 123;
+console.log(reverse(number)); 
