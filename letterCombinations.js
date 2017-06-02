146c09beba28f67eb44a60193e8a5488b531e7e5
JBayona <jordavids_22@hotmail.com>
2017-06-01 20:54:08 -0500

    letterCombination LeetCode

---
+++ letterCombinations.js
@@ -0,0 +1,42 @@
+/*
+Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+https://leetcode.com/problems/letter-combinations-of-a-phone-number/#/description
+*/
+
+
+var letterCombinations = function(digits) {
+  var length = digits.length;
+  var hash = {
+    '1' : '',
+    '2' : ['a', 'b', 'c'],
+    '3' : ['d', 'e', 'f'],
+    '4' : ['g', 'h', 'i'],
+    '5' : ['j', 'k', 'l'],
+    '6' : ['m', 'n', 'o'],
+    '7' : ['p', 'q', 'r', 's'],
+    '8' : ['t', 'u', 'v'],
+    '9' : ['w', 'x', 'y', 'z']
+  }
+  if(digits.length === 0) return [];
+  return combineLettersKey([''], 0, length, digits, hash);
+};
+
+function combineLettersKey(currentCombinations, index, length, digits, hash){
+  var next = [];
+  var tmp = '';
+  if(index === length) return currentCombinations;
+
+  for(var i = 0; i < length; i++){
+    for(var j = 0; j < hash[digits.charAt(index)].length; j++){
+      tmp = currentCombinations[i] + hash[digits.charAt(index)][j];
+      next.push(tmp);
+    }
+  }
+  return combineLettersKey(next, index+1, length, digits, hash);
+}
+
+digits = "42";
+console.log(letterCombinations(digits));
