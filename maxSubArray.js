bf9ee7536fc3808c71ca3e473314861dde991f25
JBayona <jordavids_22@hotmail.com>
2017-09-12 00:48:52 -0500

    Max Sum Consecutive Subarray

---
+++ maxSubArray.js
@@ -0,0 +1,26 @@
+/*
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+
+https://leetcode.com/problems/maximum-subarray/description/
+*/
+
+var maxSubArray = function(nums) {
+    var max = Number.MIN_SAFE_INTEGER;
+    var sum = 0;
+    for(let i = 0; i < nums.length; i++){
+      sum += nums[i];
+      if(sum > max){
+        max = sum;
+      }
+      if(sum < 0){
+        sum = 0;
+      }
+    }
+    return max;
+};
+
+array = [-2,1,-3,4,-1,2,1,-5,4];
+console.log(maxSubArray(array));
