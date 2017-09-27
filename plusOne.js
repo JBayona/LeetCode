411ecc45a5b2478c14be4d1a32815863094ec786
JBayona <jordavids_22@hotmail.com>
2017-09-26 21:00:54 -0500

    Plus One

---
+++ plusOne.js
@@ -0,0 +1,63 @@
+/*
+Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+
+You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+https://leetcode.com/problems/plus-one/description/
+*/
+
+//Option 1
+
+var plusOne = function(digits) {
+	var len = digits.length,
+	var overflow = 1,
+
+	for (var i = len - 1; i >= 0; i--) {
+	    digits[i] = digits[i] + overflow;
+	    if (digits[i] === 10) {
+	        overflow = 1;
+	        digits[i] = 0;
+	    } else {
+	        return digits;
+	    }
+	}
+
+	/*Por si queda un elemento de overflow*/
+	if (overflow === 1) {
+	    digits.unshift(1);
+	    return digits;
+    }
+};
+
+//array = [8,9,9,9];
+array = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3];
+console.log(plusOne(array));
+
+//Option 2
+
+var plusOne = function(digits) {
+  let result = [];
+  let value = 0;
+  let carry = false;
+  for(let i = digits.length - 1; i >= 0; i--){
+    if(i === digits.length-1){
+      value = digits[i] + 1;
+      if(value >= 10){
+        carry = true;
+      }
+      result.unshift(value%10);
+    }else{
+      value = carry ? (digits[i]+1) : digits[i];
+      result.unshift(value%10);
+      carry = value >=10 ? true : false;
+    }
+  }
+  if(carry) result.unshift(1);
+  return result;
+};
+
+//array = [8,9,9,9];
+array = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3];
+console.log(plusOne(array));
