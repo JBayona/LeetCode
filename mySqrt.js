64d2c1169a12b0809626c3f6b5874d88a1b56fd7
JBayona <jordavids_22@hotmail.com>
2017-09-26 23:50:12 -0500

    My Sqrt

---
+++ mySqrt.js
@@ -0,0 +1,24 @@
+/*
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+https://leetcode.com/problems/sqrtx/description/
+*/
+
+var mySqrt = function(x) {
+    //Base cases
+    if(x === 0 || x === 1) return x;
+    let pow = 1;
+    let result = 1;
+    while(result <= x){
+      if(result === x){
+          return pow;
+      }
+      pow++;
+      result = pow * pow;
+    }
+    return pow-1;
+};
+
+n = 9;
+console.log(mySqrt(n));
