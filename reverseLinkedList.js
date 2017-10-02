2aaead9328acc962392e099f294e56e8c23d66c8
JBayona <jordavids_22@hotmail.com>
2017-10-01 21:58:24 -0500

    Reverse Linked LisReverse Linked List

---
+++ reverseLinkedList.js
@@ -0,0 +1,25 @@
+/*
+Reverse Linked List
+*/
+
+function ListNode(val, next){
+  this.val = val;
+  this.next = next || null;
+}
+
+function reverseList(list){
+  let current = list;
+  let prev = null;
+  let next;
+
+  while(current){
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+}
+
+list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+console.log(reverseList(list));
