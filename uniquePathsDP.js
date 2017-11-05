6dcec43ae7c4290b0dc674ca0cd710a694fc69c1
JBayona <jordavids_22@hotmail.com>
2017-11-05 13:46:03 -0600

    Unique Paths DP

---
+++ uniquePathsDP.js
@@ -0,0 +1,38 @@
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. 
+The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+https://leetcode.com/problems/unique-paths/description/
+*/
+
+var uniquePaths = function(m, n) {
+    if(m === 0 || n === 0) return 0;
+    if(m === 1 || n === 1) return 1;
+    
+    let dp = [];
+    for(let i = 0; i < m; i++){
+        dp[i] = new Array(n);
+    }
+    //Left Column
+    for(let i = 0; i < m; i++){
+        dp[i][0] = 1;
+    }
+    //Top row
+    for(let j = 0; j < n; j++){
+        dp[0][j] = 1;
+    }
+    //Fill the table
+    for(let i = 1; i < m; i++){
+        for(let j = 1; j < n; j++){
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+    }
+    return dp[m-1][n-1];
+};
+
+m = 3;
+n = 7;
+console.log(uniquePaths(m,n));
