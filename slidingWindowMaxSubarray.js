004f6b036e7031047c40926472077b61f2debf9c
JBayona <jordavids_22@hotmail.com>
2018-06-24 23:46:56 -0700

    Opcion 2 - Queue O(N) sliding windows max.

--- slidingWindowMaxSubarray.js
+++ slidingWindowMaxSubarray.js
@@ -19,6 +19,46 @@ function slidingWindowMax(array, k) {
   return result;
 }
 
+// Option 2 O(n)
+// https://www.youtube.com/watch?v=J6o_Wz-UGvc
+
+Array.prototype.front = function() {
+  return this[0];
+};
+
+Array.prototype.back = function() {
+  return this[this.length-1]
+}
+
+function slidingWindowMax(array, k) {
+  let queue = [];
+  let result = [];
+
+  for(let i = 0; i < array.length; i++) {
+    // Si ya nos excedimos de k, debemos recorrer la ventana (eliminamos el primer elemento del queue)
+    if(queue.length && queue.front() === i - k) {
+      queue.shift();
+    }
+
+    /*If our current element is greater than the back of the queue it 
+    means that the back of the queue can never be a max element
+    in subsequent windows, so we can remove it from the queue */
+    while(queue.length && array[queue.back()] < array[i]) {
+      queue.pop(); // We remove the element from the pop
+    }
+
+    // Insertamos los indexes
+    /*Less element than the back of the queue could be max elments in subsequen windows*/
+    queue.push(i);
+    /* If we reach k we can insert the front (which is the max element) in our result array */
+    if(i >= k - 1) {
+      result.push(array[queue.front()]);
+    }
+  }
+  return result;
+}
+
 array = [1, 2, 3, 1, 4, 5, 2, 3, 6];
 k = 3;
 console.log(slidingWindowMax(array,k));
+
