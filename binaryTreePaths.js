3913dca8fcf2b2d3a756be30f75b090eac0f5b9b
JBayona <jordavids_22@hotmail.com>
2018-07-07 19:57:14 -0700

    Binary Tree Paths Op2

--- binaryTreePaths.js
+++ binaryTreePaths.js
@@ -30,3 +30,32 @@ function dfs(node, helper, result){
         dfs(node.right, helper.concat(), result);
     }
 }
+
+// Opción 2
+function Node(val, left, right){
+this.val = val;
+this.left = left || null;
+this.right = right || null;
+}
+
+
+var binaryTreePaths = function(root) {
+    let result = [];
+    let tmp = [];
+    dfs(root, tmp, result);
+    return result;
+};
+
+function dfs(node, helper, result){
+    if(node) {
+        helper.push(node.val);
+        if(!node.left && !node.right){
+            result.push(helper.join('->'));
+        }
+        dfs(node.left, [...helper], result);
+        dfs(node.right,[...helper], result);
+    }
+}
+
+tree = new Node(1, new Node(2, null, new Node(5)), new Node(3))
+console.log(binaryTreePaths(tree));
