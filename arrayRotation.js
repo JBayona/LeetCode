e885826d94a70cc11de8e3c80c290e16aa4d32b7
JBayona <jordavids_22@hotmail.com>
2018-09-03 16:30:19 -0700

    Right array rotation in place.

--- arrayRotation.js
+++ arrayRotation.js
@@ -33,3 +33,33 @@ function rotateRight(array, k) {
   }
   return result;
 }
+
+/*
+Complexity O(n)
+Space O(1)
+Original List                   : 1 2 3 4 5 6 7
+After reversing all numbers     : 7 6 5 4 3 2 1
+After reversing first k numbers : 5 6 7 4 3 2 1
+After revering last n-k numbers : 5 6 7 1 2 3 4 --> Result
+*/
+var rotateRightInPlace = function(nums, k) {
+    // This may be greater
+    k = k%nums.length;
+    // Reverse all
+    reverseArray(nums, 0, nums.length - 1);
+    // Reverse first k elements
+    reverseArray(nums, 0, k - 1);
+    // Reverse last k elements
+    reverseArray(nums, k, nums.length - 1);
+    console.log(nums);
+};
+
+function reverseArray(array, start, end) {
+    while(start < end) {
+        let tmp = array[start];
+        array[start] = array[end];
+        array[end] = tmp;
+        start++;
+        end--;
+    }
+}
