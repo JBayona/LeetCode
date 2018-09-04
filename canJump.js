12a2d9f258a87f77c4251f2d5cbea8e3315738b4
JBayona <jordavids_22@hotmail.com>
2018-09-03 19:39:57 -0700

    Can Jump.

--- canJump.js
+++ canJump.js
@@ -4,12 +4,12 @@ index i abd returns wether it´s possible to advance to the last index starting 
 array.
 
 Solution: It´s natural to try to advance as much as possible but this aproach does not always work, we can
-reach some point where it´s not possible to advance, For example if A = [2,4,1,1,0,2,3] if we vadvance to index 2
+reach some point where it´s not possible to advance, For example if A = [2,4,1,1,0,2,3] if we advance to index 2
 which contains 1, which leads to index 3, after which it cannot make progress. However advancing to index 1, which
 contains a 4 lets us proceed to index 5, from which we can advance to index 6.
 
 Los números en el array son el número máximo de posiciones que podemos avanzar, pero no necesariamente las debemos
-usar.
+usar, la idea es mantener un máximo y verificar si ese máximo es mayor al index.
 */
 
 var canJump = function(nums) {
