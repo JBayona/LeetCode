63d51fd64e74fc40b2d257a1ee36ff8c20d55d8b
JBayona <jordavids_22@hotmail.com>
2018-09-12 21:12:17 -0700

    Merge K Sorted Arrays using PriorityQueue.

--- mergeKSortedArrays.js
+++ mergeKSortedArrays.js
@@ -24,3 +24,57 @@ function bubbleSort(array) {
 
 arrays = [[1,4,7], [2,5,8], [3,6,9]];
 console.log(mergeKSortedArrays(arrays));
+
+// Complexity O(kn Log(N))
+
+/*
+[1, 4, 7]
+[2, 5, 8]
+[3, 6, 9]
+
+Al inicio la queue tendrá 1,2,3
+*/
+
+private class QueueNode implements Comparable<QueueNode> {
+  int array; // Array where it comes from
+  int index; // index of the element
+  int value; // Value
+
+  public QueueNode(int array, int index, int value) {
+    this.array = array;
+    this.index = index;
+    this.value = value;
+  }
+
+  public int compareTo(QueueNode n) {
+    if (value > n.value) return 1;
+    if (value < n.value) return -1;
+    return 0;
+  }
+}
+
+public int[] merge(int[][] arrays) {
+  PriorityQueue<QueueNode> pq = new PriorityQueue<PriorityQueue>();
+
+  int size = 0;
+  for(int i = 0; i < arrays.length; i++) {
+    size += arrays[i].length;
+    // To avoid check empty arrays
+    if(arrays[i].length > 0) {
+      // At this point me add the first tre elements in the arrays
+      pq.add(new QueueNode(i, 0, arrays[i][0]));
+    }
+  }
+
+  int[] result = new int[size];
+  for(int i = 0; !pq.isEmpty(); i++) {
+    QueueNode n = pq.poll();
+    int newIndex = n.index + 1;
+    if(newIndex < arrays[n.array].length) {
+      pd.add(new QueueNode(n.array, newIndex, arrays[n.array][newIndex]));
+    }
+  }
+
+  return result;
+
+}
