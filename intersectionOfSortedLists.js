f1f3dca11dd08727f7e3e017fcbeab28573ba654
JBayona <jordavids_22@hotmail.com>
2018-09-25 21:28:31 -0700

    Intersection Of Sorted Lists.

---
+++ intersectionOfSortedLists.js
@@ -0,0 +1,28 @@
+function Node(val, next) {
+  this.val = val;
+  this.next = next;
+}
+
+var getIntersectionNode = function(headA, headB) {
+  let listA = headA;
+  let listB = headB;
+
+  while(listA && listB) {
+    if(listA.val < listB.val) {
+      listA = listA.next;
+    } else if(listB.val < listB.val) {
+      listB = listB.next;
+    } else {
+      // Both are equals
+      let tmp = listA;
+      tmp.next = null;
+      return tmp;
+    }
+  }
+  return null;
+}
+
+
+listA = new Node(1, new Node(2, new Node(4, new Node(5, new Node(9, new Node(11))))));
+listB = new Node(5, new Node(10, new Node(12)));
+console.log(getIntersectionNode(listA, listB));
