3efa890d04a50fa283e8b955db8eeeba338510e5
JBayona <jordavids_22@hotmail.com>
2018-10-17 22:00:39 -0700

    Two Sum With Sorted Array.

---
+++ Copia de houseRobber.js
@@ -0,0 +1,48 @@
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only
+constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically
+contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you
+can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+
+https://leetcode.com/problems/house-robber/description/
+*/
+
+// Max sum non consecutive array
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if(nums.length === 0) {
+        return 0;
+    }
+    
+    let inclusive = nums[0];
+    let exclusive = 0;
+    let tmp = 0
+    
+    for(let i = 1; i < nums.length; i++) {
+        tmp = inclusive;
+        inclusive = Math.max(inclusive, exclusive + nums[i]);
+        exclusive = tmp;
+    }
+    
+    // Inclusive is the one that holds the max value
+    return inclusive;
+};
