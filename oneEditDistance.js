35a780ae004b720cc797cc9490dbdeadf2d5d8be
JBayona <jordavids_22@hotmail.com>
2018-10-27 22:21:36 -0700

    One Edit Distance Opt 2.

--- oneEditDistance.js
+++ oneEditDistance.js
@@ -32,3 +32,54 @@ s1 = "ab";
 s2 = "aa";
 
 console.log(onEditDistance(s1,s2));
+
+// Opt2
+// O(N)
+
+function onEditDistance(s1,s2) {
+    let len1 = s1.length;
+    let len2 = s2.length;
+  
+    // If the difference between both is greater than 1
+    // it's not possible to get the string
+    if(Math.abs(len1 - len2) > 1) {
+        return false;
+    }
+  
+    let i = 0;
+    let j = 0;
+    let count = 0;
+  
+    while(i < len1 && j < len2) {
+        if(s1[i] === s2[j]) {
+            i++;
+            j++;
+        } else { // If the're different
+            count++;
+            if(count > 1) {
+                return false;
+            }
+            if(len1 > len2) { // increase variable that controls s1 (delete char from s1)
+                i++;
+            } else if(len1 < len2) { // increase variable that conttrols s2 (delete char from s2)
+                j++;
+            } else { // both has the same length, so we just need to advance (replace)
+                i++;
+                j++;
+            }
+        }
+    }
+  
+  // If we have not analyzed part of some our of strings
+  if(i < len1 || j < len2) {
+    count++;
+  }
+  
+  return count <= 1;
+  
+}
+
+s1 = "ad";
+s2 = "a";
+
+console.log(onEditDistance(s1,s2));
