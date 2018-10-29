20e28be6e2b059340334e64f0f773c409d2f5be9
JBayona <jordavids_22@hotmail.com>
2018-10-28 20:55:05 -0700

    BST Iterator.

--- BSTIterator.js
+++ BSTIterator.js
@@ -8,6 +8,8 @@ Note: next() and hasNext() should run in average O(1) time and uses O(h) memory,
 https://leetcode.com/problems/binary-search-tree-iterator/description/
 */
 
+
+// INORDER ITERATOR
 function TreeNode(val, left, right) {
     this.val = val;
     this.left = left || null;
@@ -62,3 +64,53 @@ while(i.hasNext()) {
 }
 console.log(a);
 
+
+/** Including All **/
+
+function TreeNode(val, left, right) {
+    this.val = val;
+    this.left = left || null;
+    this.right = right || null;
+}
+
+
+var BSTIterator = function(root) {
+    this.stack = [];
+    this.pushAll(root);
+};
+
+
+// Function to push the most left element into the stack
+BSTIterator.prototype.pushAll = function(root) {
+    while(root) {
+        this.stack.push(root);
+        root = root.left;
+    }
+};
+
+
+/**
+ * @this BSTIterator
+ * @returns {boolean} - whether we have a next smallest number
+ */
+BSTIterator.prototype.hasNext = function() {
+    return !(this.stack.length === 0);
+};
+
+/**
+ * @this BSTIterator
+ * @returns {number} - the next smallest number
+ */
+BSTIterator.prototype.next = function() {
+    let node = this.stack.pop();
+    // Insert right nodes in our stack considering also the left nodes
+    this.pushAll(node.right);
+    return node.val;
+};
+
+BSTIterator.prototype.all = function() {
+    let result = [];
+    while(this.hasNext()) {
+        result.push(this.next());
+    }
+}
