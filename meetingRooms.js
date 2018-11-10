98469de6322fa68be0f907f640c6fa152653be16
JBayona <jordavids_22@hotmail.com>
2018-11-09 21:20:52 -0800

    Meeting Rooms

--- meetingRooms.js
+++ meetingRooms.js
@@ -6,6 +6,7 @@ https://www.youtube.com/watch?v=i2bBG7CaVxs
 https://github.com/JBayona/Facebook-Interview-Coding-1/blob/master/253.%20Meeting%20Rooms%20II.java
 */
 
+// Greedy algorithm
 var canAttendMeetings = function(intervals) {
     // Sort the intervals per end time so we can check if collapses
     let sorted = intervals.sort((a,b) => a.end - b.end);
@@ -22,3 +23,23 @@ var canAttendMeetings = function(intervals) {
 //intervals = [{start: 0, end: 30}, {start: 5, end: 10}, {start: 15, end: 20}]; // false
 intervals = [{start: 7, end: 10}, {start: 2, end: 4}]; // true
 console.log(canAttendMeetings(intervals));
+
+// Opción 2
+var canAttendMeetings = function(intervals) {
+    // Sort the intervals per end time so we can check if collapses
+    let starts = [];
+    let ends = [];
+
+    for(let i = 0; i < intervals.length; i++) {
+        starts[i] = intervals[i].start;
+        ends[i] = intervals[i].end;
+    }
+  
+    for(let i = 0 ; i < intervals.length - 1; i++) {
+        if(start[i + 1] < ends[i]) {
+            return false;
+        }
+    }
+  
+  return true;
+};
