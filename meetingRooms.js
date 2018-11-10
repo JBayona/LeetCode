669853eb3998d35b97cd077d1c6d282ca6e129f9
JBayona <jordavids_22@hotmail.com>
2018-11-09 21:14:05 -0800

    Meeting Rooms I

---
+++ meetingRooms.js
@@ -0,0 +1,24 @@
+/*
+Given an array of meeting time intervalsof start and end times [[s1,ed], [s2, e2]..] determine if a person coudld attend al meetings
+
+https://leetcode.com/problems/meeting-rooms/
+https://www.youtube.com/watch?v=i2bBG7CaVxs
+https://github.com/JBayona/Facebook-Interview-Coding-1/blob/master/253.%20Meeting%20Rooms%20II.java
+*/
+
+var canAttendMeetings = function(intervals) {
+    // Sort the intervals per end time so we can check if collapses
+    let sorted = intervals.sort((a,b) => a.end - b.end);
+  
+    for(let i = 1 ; i < intervals.length; i++) {
+        if(sorted[i].start < sorted[i-1].end) {
+            return false;
+        }
+    }
+  
+  return true;
+};
+
+//intervals = [{start: 0, end: 30}, {start: 5, end: 10}, {start: 15, end: 20}]; // false
+intervals = [{start: 7, end: 10}, {start: 2, end: 4}]; // true
+console.log(canAttendMeetings(intervals));
