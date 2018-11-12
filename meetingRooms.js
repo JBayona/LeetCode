29eb58c506ec2775065083f479eaeff632cf2d36
JBayona <jordavids_22@hotmail.com>
2018-11-11 19:16:22 -0800

    Meting Rooms II

--- meetingRooms.js
+++ meetingRooms.js
@@ -61,6 +61,7 @@ var canAttendMeetings = function(intervals) {
 
 /*
 Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] find the minimum number of conference rooms required.
+https://www.youtube.com/watch?v=GmpyAMpjpUY
 */
 
 // Opción 1
@@ -94,3 +95,47 @@ function minMeetingRooms(intervals) {
   return count = 0;
   
 }
+
+//Opción 2
+/*
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] find the minimum number of conference rooms required.
+*/
+
+public int minMeetingRooms(Interval[] intervals) {
+  if (intervals == null || intervals.length == 0) return 0;
+
+  Arrays.sort(intervals, new Comparator<Interval>(){
+    public int compare(Interval i1, Interval i2) {
+      return i1.start - i2.start;
+    }
+  });
+  
+  
+  // Min heap
+  PriorityQueue<Interval> pq = new PriorityQueue<>(new Comparator<Interval>(){
+   public int compare(Interval i1, Interval i2) {
+    return i1.end - i2.end;
+   }
+  });
+  pq.push(intervals[0]);
+
+  for (int i = 1; i < intervals.length; i++) {
+    Interval interval = pq.poll();
+    // If the current event is greater thatn the earliest end time
+    // Means we can reuse the room, so we "extend" the event and
+    // later will be added again
+    if (intervals[i].start >= interval.end)
+      // Means we need another room
+      interval.end = intervals[i].end;
+    else
+      // There´s a conflict and we need to add them to the minheap
+      pq.add(intervals[i]);
+    pq.add(interval);
+}
+  
+  // The number of rooms will be the size of the heap
+return pq.size();
+}
+
+// Example = [[0, 30], [5, 10], [15, 20]];
+// Result = 2
