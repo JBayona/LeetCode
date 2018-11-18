2a2b81b6973b4b2b18199a1b427c8434b0425b07
JBayona <jordavids_22@hotmail.com>
2018-11-17 19:10:36 -0800

    Find Missing Number LinkedList.

---
+++ findMissingNumberLinkedList.js
@@ -0,0 +1,30 @@
+/*
+Given two linked lists, find the missing number between those linked lists. One lists contains the exact same numbers plus a different one. find which number
+is missing
+*/
+
+function ListNode(val, next) {
+  this.val = val,
+  this.next = next || null;
+}
+
+function findMissingNumberLinkedList(l1, l2) {
+  let sum1 = 0;
+  let sum2 = 0;
+
+  while(l1) {
+   sum1 += l1.val;
+   l1 = l1.next;
+  }
+
+  while(l2) {
+   sum2 += l2.val;
+   l2 = l2.next;
+  }
+
+  return Math.abs(sum1 - sum2);
+}
+
+l1 = new ListNode(7, new ListNode(3, new ListNode(5, new ListNode(8, new ListNode(9, new ListNode(6))))));
+l2 = new ListNode(8, new ListNode(3, new ListNode(5, new ListNode(9, new ListNode(7)))));
+console.log(findMissingNumberLinkedList(l1,l2));
