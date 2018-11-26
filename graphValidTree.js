9ab4763eb70f7cc7ca9ef2a94da5185d44c2cb76
JBayona <jordavids_22@hotmail.com>
2018-11-25 20:06:26 -0800

    Graph Valid Tree.

--- graphValidTree.js
+++ graphValidTree.js
@@ -2,7 +2,7 @@
 Given n nodes labeled from 0 to n-1 and a list of undirected edges (each edge is a pair of nodes), write
 a function to check whether these edges make up a valid tree.
 
-For example
+For example:
 
 Given n = 5 and edges = [[0, 1], [0, 2], [0, 3], [1, 4]], return true.
 Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.
@@ -34,7 +34,7 @@ var validTree = function(n, edges) {
 
   // Create the graph
   for(let i = 0; i < n; i++) {
-    graph[i] = []
+    graph[i] = [];
   }
 
   // Fill the undirected graph
