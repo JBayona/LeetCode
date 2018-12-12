018cec9fd0d82d8140abbf17830ef73a4e901a13
JBayona <jordavids_22@hotmail.com>
2018-12-11 23:13:32 -0800

    Stack With Two Queues Op1 O(N) push and O(1) pop.

---
+++ stackWithTwoQueues.js
@@ -0,0 +1,95 @@
+/*
+Implement the following operations of a stack using queues.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+empty() -- Return whether the stack is empty.
+Example:
+
+MyStack stack = new MyStack();
+
+stack.push(1);
+stack.push(2);  
+stack.top();   // returns 2
+stack.pop();   // returns 2
+stack.empty(); // returns false
+Notes:
+
+You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), 
+as long as you use only standard operations of a queue.
+You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+
+https://leetcode.com/problems/implement-stack-using-queues/description/
+*/
+
+// Push O(N) y pop()/top() O(1)
+/**
+ * Initialize your data structure here.
+ */
+var MyStack = function() {
+  this.queue1 = [];
+  this.queue2 = [];
+};
+
+// basically the idea is to change everytime the queues as stack and queue are the 
+// same but follow an opposite order
+
+/**
+ * Push element x onto stack. 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+  // Insert in queue2
+  // This queue at this point will always be empty
+  this.queue2.push(x);
+  
+  while(this.queue1.length) {
+      this.queue2.push(this.queue1.shift());
+  }
+  
+  let tmp = this.queue1;
+  this.queue1 = this.queue2;
+  this.queue2 = tmp;
+};
+
+/**
+ * Removes the element on top of the stack and returns that element.
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+  if(!this.queue1.length) {
+      return;
+  }
+  return this.queue1.shift();
+};
+
+/**
+ * Get the top element.
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+  if(!this.queue1.length) {
+      return;
+  }
+  return this.queue1[0];
+};
+
+/**
+ * Returns whether the stack is empty.
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+  return this.queue1.length === 0;
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = Object.create(MyStack).createNew()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
