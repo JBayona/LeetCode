5789a786203cb2cd552694236f6b550835194bc0
JBayona <jordavids_22@hotmail.com>
2018-12-28 15:22:03 -0600

    Search in sorted and rotated array.

--- searchInSortedArray.js
+++ searchInSortedArray.js
@@ -22,22 +22,26 @@ var search = function(nums, target){
           return mid;
     } else if (nums[start] <= nums[mid]) {// left side sorted
       if (nums[mid] > target && nums[start] <= target) {
-          // nums[mid] !== target, can safely do end = mid - 1
-          end = mid - 1;
+        /*Si el subarray izquierdo esta ordenado, es facil saber
+        si el elemento se encuentra en esa mitad*/
+        // nums[mid] !== target, can safely do end = mid - 1
+        end = mid - 1;
       } else {
         //Recorta la seccion a analizar
-          start = mid + 1;
+        start = mid + 1;
       }
     } else { // right side sorted
-        if (nums[mid] < target && nums[end] >= target) {
-            // nums[mid] !== target, can safely do start = mid + 1
-            start = mid + 1;
-        } else {
-            end = mid - 1;
-        }
+      /*Si no esta ordenado la parte izquierda, la derecha
+      debe de estar ordenada*/
+      if (nums[mid] < target && nums[end] >= target) {
+        // nums[mid] !== target, can safely do start = mid + 1
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
     }
   }
-  return -1
+  return -1;
 }
 
 array = [4,5,6,7,0,1,2];
