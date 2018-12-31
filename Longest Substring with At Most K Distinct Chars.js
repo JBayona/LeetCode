910c7d18ec0299f5beed70d76c1fd9c04b835d40
JBayona <jordavids_22@hotmail.com>
2018-12-31 00:06:09 -0600

    Longest substring lineal complexity.

--- Longest Substring with At Most K Distinct Chars.js
+++ Longest Substring with At Most K Distinct Chars.js
@@ -38,3 +38,39 @@ str = "karappa";
 k = 2; // appa - result = 4
 k = 3; // arappa - result = 6
 console.log(lengthOfLongestSubstringKDistinct(str, k));
+
+// Time O(N)
+// Space O(N)
+// https://www.youtube.com/watch?v=8AQra0p_HmI
+// https://www.programcreek.com/2013/02/longest-substring-which-contains-2-unique-characters/
+var lengthOfLongestSubstringKDistinct = function(str, k) {
+  let hash = {};
+  let result = 0;
+  let start = 0;
+
+  for(let i = 0; i < str.length; i++) {
+    let c = str[i];
+    if(c in hash) {
+      hash[c]++;
+    } else {
+      hash[c] = 1;
+    }
+
+    if(Object.keys(hash).length <= k) {
+      result = Math.max(result, i - start + 1);
+    } else {
+      // If we have rebased the limit, remove the most left element
+      while(Object.keys(hash).length > k) {
+        let l = str[start];
+        let count = hash[l];
+        if(count === 1) {
+          delete hash[l];
+        } else {
+          hash[l]--;
+        }
+        start++;
+      }
+    }
+  }
+  return result;
+}
