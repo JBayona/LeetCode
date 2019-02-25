b7bb50008b6cc940a624183343056620466b26ac
JBayona <jordavids_22@hotmail.com>
2019-02-24 19:18:05 -0800

    Longest Consecutive Numbers.

--- longestConsecutive.js
+++ longestConsecutive.js
@@ -10,6 +10,40 @@ Your algorithm should run in O(n) complexity.
 https://leetcode.com/problems/longest-consecutive-sequence/description/
 */
 
+// Time complexity O(N)
+// Space complexity O(N)
+var longestConsecutive = function(nums) {
+    
+  // If there's no number in the array, return 0
+  if(!nums.length) {
+      return 0;
+  }
+  
+  let hash = {};
+  // Store all the elements in the hash
+  for(let i = 0; i < nums.length; i++) {
+      hash[nums[i]] = true;
+  }
+  
+  // Minimum longest should be 1
+  let count = 1;
+  let result = 1;
+  let current = null;
+  for(let prop in hash) {
+    let num = parseInt(prop);
+    count = 1;
+    current = num;
+    while(current + 1 in hash) {
+      current++;
+      count++;
+      result = Math.max(count, result);
+    }
+  }
+  
+  return result;
+};
+
+// Option 2
 var longestConsecutive = function(nums) {
     var map = {};
     var value = 0;
