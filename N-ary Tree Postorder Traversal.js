cc32ef510df0bfa071e67247f753a23e90d3645e
JBayona <jordavids_22@hotmail.com>
2019-02-27 23:14:16 -0800

    N-ary Tree Postorder traversal.

---
+++ N-ary Tree Postorder Traversal.js
@@ -0,0 +1,68 @@
+/*
+Given an n-ary tree, return the postorder traversal of its nodes' values.
+
+For example, given a 3-ary tree:
+
+ 
+
+
+
+ 
+
+Return its postorder traversal as: [5,6,3,2,4,1].
+
+ 
+Note:
+
+Recursive solution is trivial, could you do it iteratively?
+
+
+https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+
+*/
+
+// Recursive
+var postorder = function(root) {
+
+  if(!root){
+    return [];
+  } 
+
+  let result = [];
+  helper(root, result);
+  return result;
+};
+
+function helper(node, result) {
+  if(!node) {
+    return;
+  }
+  
+  // Visit all children
+  for(let i = 0; i < node.children.length; i++) {
+    helper(node.children[i], result);
+  }
+  
+  result.push(node.val);
+}
+
+// Iterativo
+var postorder = function(root) {
+  if(!root) {
+    return [];
+  }
+  
+  let stack = [];
+  let result = [];
+  stack.push(root);
+  
+  while(stack.length) {
+    let node = stack.pop();
+    result.push(node.val);
+    for(let i = 0; i < node.children.length; i++) {
+      stack.push(node.children[i]);
+    }
+  }
+  
+  return result.reverse();
+};
