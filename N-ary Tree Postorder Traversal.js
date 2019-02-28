5d2a7aa4fa276e2f5b81d0b844865ac9bf28bc96
JBayona <jordavids_22@hotmail.com>
2019-02-27 23:17:26 -0800

    N-ary Tree Postorder Traversal.

--- N-ary Tree Postorder Traversal.js
+++ N-ary Tree Postorder Traversal.js
@@ -3,19 +3,11 @@ Given an n-ary tree, return the postorder traversal of its nodes' values.
 
 For example, given a 3-ary tree:
 
- 
-
-
-
- 
-
 Return its postorder traversal as: [5,6,3,2,4,1].
 
- 
 Note:
 
 Recursive solution is trivial, could you do it iteratively?
-
 
 https://leetcode.com/problems/n-ary-tree-postorder-traversal/
 
