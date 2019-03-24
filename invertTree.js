4c2547ad65efd567048a770d8c8b992ed3ad453d
JBayona <jordavids_22@hotmail.com>
2019-03-24 09:23:19 -0600

    Invert Tree.

--- invertTree.js
+++ invertTree.js
@@ -5,12 +5,15 @@ Invert a binary tree.
   2     7
  / \   / \
 1   3 6   9
+
 to
+
      4
    /   \
   7     2
  / \   / \
 9   6 3   1
+
 https://leetcode.com/problems/invert-binary-tree/description/
 */
 
