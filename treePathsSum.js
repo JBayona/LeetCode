339ce38a11983882712718052b08228b14b47f1a
JBayona <jordavids_22@hotmail.com>
2019-04-07 21:16:23 -0700

    Tree Path Sum.

--- treePathsSum.js
+++ treePathsSum.js
@@ -1,4 +1,5 @@
 /*
+
 Given a binary tree, where every node value is a Digit from 1-9 .Find the sum of all the numbers which are formed from root to leaf paths.
 For example consider the following Binary Tree.
 
@@ -18,6 +19,7 @@ For example consider the following Binary Tree.
 Answer = 632 + 6357 + 6354 + 654 = 13997 
 
 https://www.geeksforgeeks.org/sum-numbers-formed-root-leaf-paths/
+
 */
 
 function TreeNode(val, left, right) {
