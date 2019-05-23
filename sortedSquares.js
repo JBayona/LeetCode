b7fca86cce7066934ad0bffda1c2e3525d961221
JBayona <jordavids_22@hotmail.com>
2019-05-22 21:39:40 -0500

    Sorted Squares.

---
+++ sortedSquares.js
@@ -0,0 +1,52 @@
+/*
+Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Example 2:
+
+Input: [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+
+https://leetcode.com/problems/squares-of-a-sorted-array/
+
+*/
+
+var sortedSquares = function(A) {
+    let result = [];
+    let j = 0;
+    
+    while(j < A.length && A[j] < 0) {
+          j++;
+    }
+    
+    let i = j - 1;
+    let index = 0;
+    
+    // Two pointers
+    while(i >= 0 && j < A.length) {
+        if(A[i] * A[i] < A[j] * A[j]) {
+            result[index++] = A[i] * A[i];
+            i--;
+        } else {
+            result[index++] = A[j] * A[j];
+            j++;
+        }
+    }
+    
+    // If we have missing values
+    while (i >= 0) {
+        result[index++] = A[i] * A[i];
+        i--;
+    }
+
+    while (j < A.length) {
+        result[index++] = A[j] * A[j];
+        j++;
+    }
+
+    return result;
+    
+};
