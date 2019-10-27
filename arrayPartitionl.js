088f265e3d294c11d487991f91a931e1d67d8f61
JBayona <jordavids_22@hotmail.com>
2019-10-26 18:32:22 -0700

    Partition.

--- arrayPartitionl.js
+++ arrayPartitionl.js
@@ -7,6 +7,7 @@ Input: [1,4,3,2]
 
 Output: 4
 Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+
 Note:
 n is a positive integer, which is in the range of [1, 10000].
 All the integers in the array will be in the range of [-10000, 10000].
