e7c0b9e7bee4c80ad83d575997e5095905f677f0
JBayona <jordavids_22@hotmail.com>
2019-11-30 23:08:48 -0800

    Judge Circle.

--- judgeCircle.js
+++ judgeCircle.js
@@ -1,5 +1,4 @@
 /*
-
 Initially, there is a Robot at position (0, 0). Given a sequence of its moves, judge if
 this robot makes a circle, which means it moves back to the original place.
 
@@ -15,7 +14,6 @@ Input: "LL"
 Output: false
 
 https://leetcode.com/problems/judge-route-circle/description/
-
 */
 
 /**
