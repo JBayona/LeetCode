fdc3ce0999beb0d4458eb6a39a62c76ea3529b11
Jorge Bayona <jbayonaromo@lyft.com>
2019-12-21 11:24:07 -0800

    Valid Parenthesis.

--- validPalindrome.js
+++ validPalindrome.js
@@ -1,4 +1,5 @@
 /*
+
 Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 
 Note: For the purpose of this problem, we define empty string as valid palindrome.
@@ -13,6 +14,7 @@ Input: "race a car"
 Output: false
 
 https://leetcode.com/problems/valid-palindrome/description/
+
 */
 
 /**
