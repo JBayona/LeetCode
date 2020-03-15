1f0824033d20306a425ed565bd4b751e12f8ec10
Jorge Bayona <jbayonaromo@lyft.com>
2020-03-14 21:16:47 -0700

    Longest Substring With At Least K Repeting Characters.

--- longestSubstringWithAtLeastKRepeatingCharacters.js
+++ longestSubstringWithAtLeastKRepeatingCharacters.js
@@ -1,5 +1,5 @@
 /*
-ind the length of the longest substring T of a given string (consists of lowercase letters only) such that
+Find the length of the longest substring T of a given string (consists of lowercase letters only) such that
 every character in T appears no less than k times.
 
 Example 1:
