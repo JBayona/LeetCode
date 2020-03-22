248ec58e2713667b227783de64c55e9b79511f17
Jorge Bayona <jbayonaromo@lyft.com>
2020-03-21 20:22:47 -0700

    House Robber.

--- houseRobber.js
+++ houseRobber.js
@@ -9,14 +9,12 @@ Given a list of non-negative integers representing the amount of money of each h
 determine the maximum amount of money you can rob tonight without alerting the police.
 
 Example 1:
-
 Input: [1,2,3,1]
 Output: 4
 
 Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
              Total amount you can rob = 1 + 3 = 4.
 Example 2:
-
 Input: [2,7,9,3,1]
 Output: 12
 
