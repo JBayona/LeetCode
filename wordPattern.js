3ec971dc7d40df7522e930f6f5a515298f381a9a
Jorge Bayona <jbayonaromo@lyft.com>
2020-04-07 00:35:32 -0700

    Word Pattern.

---
+++ wordPattern.js
@@ -0,0 +1,51 @@
+/*
+Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+
+Example 1:
+
+Input: pattern = "abba", str = "dog cat cat dog"
+Output: true
+Example 2:
+
+Input:pattern = "abba", str = "dog cat cat fish"
+Output: false
+Example 3:
+
+Input: pattern = "aaaa", str = "dog cat cat dog"
+Output: false
+Example 4:
+
+Input: pattern = "abba", str = "dog dog dog dog"
+Output: false
+Notes:
+You may assume pattern contains only lowercase letters, and str contains lowercase letters that may be separated by a single space.
+
+https://leetcode.com/problems/word-pattern/
+*/
+
+var wordPattern = function(pattern, str) {
+    let strArray = str.split(' ');
+    
+    if(pattern.length  !== strArray.length) {
+        return false;
+    }
+    
+    let hash = {};
+    for(let i = 0; i < pattern.length; i++) {
+        let patternChar = pattern[i];
+        if(!(patternChar in hash)){
+            if(Object.values(hash).includes(strArray[i])) {
+                return false;
+            } else {
+                hash[patternChar] = strArray[i];
+            }
+        } else {
+            if(hash[patternChar] !== strArray[i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
