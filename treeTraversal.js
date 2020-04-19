78d8d48781708959020e1b40ae343977676e5fc5
Jorge Bayona <jbayonaromo@lyft.com>
2020-04-19 14:45:00 -0700

    Inorder Traversal iterative.

--- treeTraversal.js
+++ treeTraversal.js
@@ -2,6 +2,11 @@
 
 // Preorder
 
+/*
+It is O (n) where n is the number of nodes in the tree. It is simply because
+the algorithm will need to visit every node exactly once.
+*/
+
 // Iterative
 // https://www.youtube.com/watch?v=elQcrJrfObg&t=213s
 var preorderTraversal = function(root) {
@@ -43,4 +48,52 @@ function preorder(node, result) {
     result.push(node.val);
     preorder(node.left, result);
     preorder(node.right, result);
+}
+
+// Inorder
+
+// Iterative
+var inorderTraversal = function(root) {
+    if(!root) {
+        return [];
+    }
+    let result = [];
+    let stack = [];
+    let current = root;
+    
+    while(true) {
+        // reach the left most node of the current node 
+        if(current) {
+            // Place pointer to a tree node on the stack  
+            // before traversing the node's left subtree
+            stack.push(current);
+            current = current.left;
+        } else {
+            // Backtrack the empty subtree and visit the top node, if the stack
+            // is empty we  are done
+            if(!stack.length) {
+                break;
+            }
+            current = stack.pop();
+            result.push(current.val);
+            current = current.right;
+        }
+    }
+    return result;
+};
+
+// Recursive
+var inorderTraversal = function(root) {
+    let result = [];
+    inorderTraversalHelper(root, result);
+    return result;
+};
+
+function inorderTraversalHelper(root, result) {
+    if(!root) {
+        return;
+    }
+    inorderTraversalHelper(root.left, result);
+    result.push(root.val);
+    inorderTraversalHelper(root.right, result);
 }
