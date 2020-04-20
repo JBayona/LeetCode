2108429c8220c72ba49fe12e982a028b2d85664a
Jorge Bayona <jbayonaromo@lyft.com>
2020-04-20 00:03:19 -0700

    Postorder Traversal Iterative.

--- treeTraversal.js
+++ treeTraversal.js
@@ -97,3 +97,70 @@ function inorderTraversalHelper(root, result) {
     result.push(root.val);
     inorderTraversalHelper(root.right, result);
 }
+
+// Postorder
+
+// Iterative
+/*
+The idea is to move down to leftmost node using left pointer.
+While moving down, push root and root’s right child to stack. Once we reach leftmost node
+print it if it doesn’t have a right child. If it has a right child, then change root so that the right child is processed before.
+*/
+var postorderTraversal = function(root) {
+    if(!root) {
+        return [];
+    }
+    let current = root;
+    let stack = [];
+    let result = [];
+    
+    while(true) {
+        while(current) {
+            // Push root's right child and then root to stack
+            if(current.right) {
+                stack.push(current.right);
+            }
+            stack.push(current);
+            
+            // Set root as root's left child 
+            current = current.left;
+        }
+        // Pop item and set if as root
+        current = stack.pop();
+
+        // If the popped item has a right child and the 
+        // right child is not processed yet, then make sure
+        // right child is processed before root
+        if(current.right && stack[stack.length-1] === current.right) {
+            stack.pop(); // Remove the right child from the stack
+            stack.push(current); // Push root back to stack
+            current = current.right; // Change root so that the right child is processed next
+        } else {
+            // print root's data and set root as null
+            result.push(current.val);
+            current = null;
+        }
+        // We are done
+        if(stack.length <= 0) {
+            break;
+        }
+    }
+    return result;
+};
+
+
+// Recursive
+var postorderTraversal = function(root) {
+    let result = [];
+    postorderTraversalHelper(root, result);
+    return result;
+};
+
+function postorderTraversalHelper(root, result) {
+    if(!root) {
+        return [];
+    }
+    postorderTraversalHelper(root.left, result);
+    postorderTraversalHelper(root.right, result);
+    result.push(root.val);
+}
