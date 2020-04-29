433b304ea566c91bae279d124afcf2f75700db0b
Jorge Bayona <jbayonaromo@lyft.com>
2020-04-28 22:16:01 -0700

    Reorder Log Files.

--- reorderLogFiles.js
+++ reorderLogFiles.js
@@ -71,3 +71,25 @@ var reorderLogFiles = function(logs) {
     }
     return result;
 };
+
+// Option 2
+
+var reorderLogFiles = (logs) => {
+  const body = s => s.slice(s.indexOf(' ') + 1); // get the body after identifier
+  const isNum = c => /\d/.test(c);
+
+  // if body same then compare identifier
+  const compare = (a, b) => {
+    const n = body(a).localeCompare(body(b));
+    if (n !== 0) return n;
+    return a.localeCompare(b);
+  };
+
+  const digitLogs = [];
+  const letterLogs = [];
+  for (const log of logs) {
+    if (isNum(body(log))) digitLogs.push(log);
+    else letterLogs.push(log);
+  }
+  return [...letterLogs.sort(compare), ...digitLogs];
+};
