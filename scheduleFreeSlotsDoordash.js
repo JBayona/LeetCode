f3936b7addc3e8d7a8d1c05cc9b858a85f441ad8
JBayona <jordavids_22@hotmail.com>
2020-05-07 00:05:21 -0700

    Schedule Free Slots Doordash.

--- scheduleFreeSlotsDoordash.js
+++ scheduleFreeSlotsDoordash.js
@@ -1,3 +1,18 @@
+/*
+Google calendar, Outlookm Ical, has been banned from your company! So an intrepid engineer has decided to roll their own
+implementation. Unfourtunately one mayor missing feature is the ability to find out what time slots are free for a particular
+individual.
+
+Given a list of time blocks where a particular person is already booked/busy, a start and end time to seatch between, a minimum
+durationt to search for find all the blocks of time that a person is free for a potential meeting that will last the
+aforementioned duration.
+
+Given start_time, end_time, duration, meeting_list -> suggested_meetings
+
+Let´s assume we abstract the representation of time as simple integers, so a valid time is any valid integer supported by
+your environment. Here is an example input [[3,20], [-2, 0], [0,2], [16,17], [19,23], [30,40], [27,233]]
+*/
+
 function bookTime(intervals, start, end, duration) {
   if(!intervals || !intervals.length) {
     return [];
