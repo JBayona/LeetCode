2f5653944a60248b7c789e5a6519b6047f197792
JBayona <jordavids_22@hotmail.com>
2020-05-09 00:27:46 -0700

    Is Perfect Square.

---
+++ isPerfectSquare.js
@@ -0,0 +1,21 @@
+/*
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Note: Do not use any built-in library function such as sqrt.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 14
+Output: false
+
+https://leetcode.com/problems/valid-perfect-square/
+*/
+
+var isPerfectSquare = function(num) {
+    let sqr = Math.sqrt(num);
+    return (sqr - Math.floor(sqr) === 0);
+};
