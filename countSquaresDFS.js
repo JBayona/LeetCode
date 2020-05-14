f5ebe72d2802d0766c572d265a5b34d2c245c996
JBayona <jordavids_22@hotmail.com>
2020-05-13 20:18:32 -0700

    Count Squares DFS.

---
+++ countSquaresDFS.js
@@ -0,0 +1,53 @@
+/*
+Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
+
+Example 1:
+
+Input: matrix =
+[
+  [0,1,1,1],
+  [1,1,1,1],
+  [0,1,1,1]
+]
+Output: 15
+Explanation: 
+There are 10 squares of side 1.
+There are 4 squares of side 2.
+There is  1 square of side 3.
+Total number of squares = 10 + 4 + 1 = 15.
+Example 2:
+
+Input: matrix = 
+[
+  [1,0,1],
+  [1,1,0],
+  [1,1,0]
+]
+Output: 7
+Explanation: 
+There are 6 squares of side 1.  
+There is 1 square of side 2. 
+Total number of squares = 6 + 1 = 7.
+
+https://leetcode.com/problems/count-square-submatrices-with-all-ones/
+*/
+
+ * @return {number}
+ */
+// DP
+// O(M X N)
+var countSquares = function(matrix) {
+    let count = 0;
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[0].length; j++) {
+            if(matrix[i][j] === 0) {
+                continue;
+            }
+            if(i > 0 && j > 0) {
+                matrix[i][j] += Math.min(matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1]);
+            }
+            count += matrix[i][j];
+        }
+    }
+    return count;
+};
