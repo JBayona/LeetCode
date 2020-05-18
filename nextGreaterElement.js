46019f64ba86a36c12ed4667e7c39d4b4daa05bf
JBayona <jordavids_22@hotmail.com>
2020-05-18 00:53:38 -0700

    Next Greater Element.

--- nextGreaterElement.js
+++ nextGreaterElement.js
@@ -1,7 +1,9 @@
 /*
-You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2.
+Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
 
-The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2.
+If it does not exist, output -1 for this number.
 
 Example 1:
 Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
