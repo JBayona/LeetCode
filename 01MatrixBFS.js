307f0b22f24afc8481170859d19bc69627a64f96
JBayona <jordavids_22@hotmail.com>
2020-05-21 00:09:55 -0700

    01 Matrix BFS.

--- 01MatrixBFS.js
+++ 01MatrixBFS.js
@@ -2,9 +2,6 @@
 Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
 
 The distance between two adjacent cells is 1.
-
- 
-
 Example 1:
 
 Input:
@@ -27,7 +24,6 @@ Output:
 [[0,0,0],
  [0,1,0],
  [1,2,1]]
- 
 
 Note:
 
