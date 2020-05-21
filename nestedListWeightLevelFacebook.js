8817ef5bd26861e8eb550d0d180cf0a17693db74
JBayona <jordavids_22@hotmail.com>
2020-05-21 00:51:33 -0700

    Nested List Weight Level Facebook.

---
+++ nestedListWeightLevelFacebook.js
@@ -0,0 +1,42 @@
+/*
+Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Example 1:
+
+Input: [[1,1],2,[1,1]]
+Output: 10 
+Explanation: Four 1's at depth 2, one 2 at depth 1.
+Example 2:
+
+Input: [1,[4,[6]]]
+Output: 27 
+Explanation: One 1 at depth 1, one 4 at depth 2, and one 6 at depth 3; 1 + 4*2 + 6*3 = 27.
+*/
+function nestedListWeightLevel(list) {
+  // Send the list and the first level
+  return helper(list, 1);
+}
+
+function helper(nestedList, depth) {
+  // Base case
+  if(!nestedList) {
+    return 0;
+  }
+  let sum = 0;
+  for(let val of nestedList) {
+    // If is an array, return the sum with the new depth
+    if(Array.isArray(val)) {
+      sum += helper(val, depth + 1);
+    } else {
+      sum += val * depth;
+    }
+  }
+  // Return accunulate sum
+  return sum;
+}
+
+// array = [[1,1],2,[1,1]];
+array = [1,[4,[6]]];
+console.log(nestedListWeightLevel(array));
