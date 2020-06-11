90301bce32915bc8a9242e014fcdb3ceb7de1cf1
JBayona <jordavids_22@hotmail.com>
2020-06-10 20:45:09 -0700

    Verify Alien Dictionary.

---
+++ verifyingAlienDictionary.js
@@ -0,0 +1,63 @@
+/*
+In an alien language, surprisingly they also use english lowercase letters, but possibly
+in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet
+return true if and only if the given words are sorted lexicographicaly in this alien language.
+
+Example 1:
+
+Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+Example 2:
+
+Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+Output: false
+Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1]
+hence the sequence is unsorted.
+Example 3:
+
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is
+shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as
+the blank character which is less than any other character (More info).
+
+https://leetcode.com/problems/verifying-an-alien-dictionary/
+*/
+
+var isAlienSorted = function(words, order) {
+    let map = {};
+    // Set map
+    for(let i = 0; i < order.length; i++) {
+        let c = order[i];
+        map[c] = i;
+    }
+    
+    for(let i = 1; i < words.length; i++) {
+        let word1 = words[i-1];
+        let word2 = words[i];
+        if(!compareStrings(word1, word2, map)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+function compareStrings(word1, word2, map) {
+    let i = 0;
+    let j = 0;
+    while(i < word1.length && j < word2.length && word1[i] === word2[j]) {
+        i++;
+        j++;
+    }
+    // The first one is shorter
+    if(i === word1.length) {
+        return true;
+    } else if(j === word2.length) { // The second string is larger
+        return false;
+    } else {
+        return map[word1[i]] < map[word2[j]];
+    }
+}
