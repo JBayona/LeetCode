52ffffb3196dc4d8bd7d05acaacce940348b837c
JBayona <jordavids_22@hotmail.com>
2020-06-17 12:34:10 -0700

    Remove Nth From End.

--- removeNthFromEnd.js
+++ removeNthFromEnd.js
@@ -1,6 +1,8 @@
 /*
 Given linked list: 1->2->3->4->5, and n = 2.
 After removing the second node from the end, the linked list becomes 1->2->3->5.
+
+https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 */
 
 function ListNode(val, node){
