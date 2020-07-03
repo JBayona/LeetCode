f75fe189f738136c83cd162bf784a36b73a90078
JBayona <jordavids_22@hotmail.com>
2020-07-03 14:18:25 -0700

    Maximum Number Of Events That can Be Attended.

--- maximumNumberOfEventsThatCanBeAttended.js
+++ maximumNumberOfEventsThatCanBeAttended.js
@@ -33,3 +33,26 @@ var maxEvents = function(events) {
     }
     return answer;
 };
+
+// Option 2 - better performance
+var maxEvents = function(events) {
+    // Sort based on end time, from greater to less value
+    events.sort((a, b) => a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]);
+    let attended = new Array(100001).fill(false); // 10^5
+    
+    let answer = 0;
+    let lastFound = 0;
+    for(let i = 0; i < events.length; i++) {
+        let [start, end] = events[i];
+        let init = (i > 0 && events[i-1][0] == start) ? lastFound : start;
+        for(let j = init; j <= end; j++) {
+            if(attended[j] === false) {
+                attended[j] = true;
+                lastFound = j;
+                answer++;
+                break;
+            }
+        }
+    }
+    return answer;
+};
