50b2ec543e0f0866f16ed5ee940ce4c16b94133e
JBayona <jordavids_22@hotmail.com>
2020-07-06 23:37:06 -0700

    Minimum Possible Integer Swaps.

--- minimumPossibleIntegerAfterAtMostKAdjacentSwapsOnDigit.js
+++ minimumPossibleIntegerAfterAtMostKAdjacentSwapsOnDigit.js
@@ -37,7 +37,7 @@ var minInteger = function(num, k) {
     let n = arr.length;
     for (let i = 0; i < n-1 && k > 0; i++) {
         let pos = i; 
-        for (let j = i + 1; j < n ; ++j) {
+        for (let j = i + 1; j < n ; j++) {
             // No more swaps available
             if (j - i > k) {
                 break;
