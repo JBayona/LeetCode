1991622c4ba4c10779a14ff5c0777c3480a0ef31
JBayona <jordavids_22@hotmail.com>
2020-07-13 13:08:32 -0700

    Is Same Tree.

--- isSameTree.js
+++ isSameTree.js
@@ -20,7 +20,22 @@ https://leetcode.com/problems/same-tree/description/
  * @return {boolean}
  */
 
- var isSameTree = function(p, q) {
+ // Opción 1
+var isSameTree = function(p, q) {
+    if(p === null && q === null) {
+        return true;
+    }
+    if(p === null || q === null) {
+        return false;
+    }
+    if(p.val !== q.val) {
+        return false;
+    }
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+ // Opción 2
+var isSameTree = function(p, q) {
     return checkTree(p,q);
 };
 
