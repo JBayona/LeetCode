06179d039d5a2783638646d4cd6425c003ad97da
JBayona <jordavids_22@hotmail.com>
2020-08-05 13:28:10 -0700

    Maximum Product Of Words Lengths.

--- maximumProductOfWordLengths.js
+++ maximumProductOfWordLengths.js
@@ -1,6 +1,7 @@
 /*
-Given a string array words, find the maximum value of length(word[i]) * length(word[j]) where the two words
-do not share common letters. You may assume that each word will contain only lower case letters. If no such two words exist, return 0.
+Given a string array words, find the maximum value of length(word[i]) * length(word[j]) 
+where the two words do not share common letters. You may assume that each word will
+contain only lower case letters. If no such two words exist, return 0.
 
 Example 1:
 
