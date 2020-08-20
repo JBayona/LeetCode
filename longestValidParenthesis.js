7f0d5f79ab9de9c33dd084c6356a29b52ff7e21c
JBayona <jordavids_22@hotmail.com>
2020-08-20 17:19:38 -0500

    Longest Valid Parenthesis.

--- longestValidParenthesis.js
+++ longestValidParenthesis.js
@@ -3,12 +3,11 @@ Given a string containing just the characters '(' and ')', find the length of th
 longest valid (well-formed) parentheses substring.
 
 Example 1:
-
 Input: "(()"
 Output: 2
 Explanation: The longest valid parentheses substring is "()"
-Example 2:
 
+Example 2:
 Input: ")()())"
 Output: 4
 Explanation: The longest valid parentheses substring is "()()"
