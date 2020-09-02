897afde8b3c4b1e709c2a2e9c1f28bcfeed74086
JBayona <jordavids_22@hotmail.com>
2020-09-02 17:14:30 -0500

    Word Search V2.

--- wordSearchV2.js
+++ wordSearchV2.js
@@ -1,5 +1,4 @@
 /*
-
 Given a 2D board and a word, find if the word exists in the grid.
 
 The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
@@ -16,6 +15,7 @@ word = "ABCCED", -> returns true,
 word = "SEE", -> returns true,
 word = "ABCB", -> returns false.
 
+https://leetcode.com/problems/word-search/
 */
 
 var exist = function(board, word) {
