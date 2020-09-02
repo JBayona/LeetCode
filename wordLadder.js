3624899fd5ab5fc12cfe3d0784aac8b5fee3706a
JBayona <jordavids_22@hotmail.com>
2020-09-01 20:11:19 -0500

    Word Ladder.

--- wordLadder.js
+++ wordLadder.js
@@ -19,18 +19,17 @@ Input:
 beginWord = "hit",
 endWord = "cog",
 wordList = ["hot","dot","dog","lot","log","cog"]
-
 Output: 5
 
 Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
 return its length 5.
+
 Example 2:
 
 Input:
 beginWord = "hit"
 endWord = "cog"
 wordList = ["hot","dot","dog","lot","log"]
-
 Output: 0
 
 Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
