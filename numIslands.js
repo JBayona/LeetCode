c9df5c4abb6775ff612a640939a04bbb7d7f54ad
JBayona <jordavids_22@hotmail.com>
2020-09-07 00:06:56 -0400

    Num Islands.

--- numIslands.js
+++ numIslands.js
@@ -1,5 +1,6 @@
 /*
 Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+
 An island is surrounded by water and is formed by connecting adjacent lands
 horizontally or vertically. 
 
