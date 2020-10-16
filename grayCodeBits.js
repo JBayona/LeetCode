dd9bf03b9aae8943bdc758859bce35542ae585ee
JBayona <jordavids_22@hotmail.com>
2020-10-15 20:07:51 -0400

    Gray Code bits.

--- grayCodeBits.js
+++ grayCodeBits.js
@@ -4,7 +4,6 @@ The gray code is a binary numeral system where two successive values differ in o
 Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
 
 Example 1:
-
 Input: 2
 Output: [0,1,3,2]
 Explanation:
@@ -20,8 +19,8 @@ For example, [0,2,3,1] is also a valid gray code sequence.
 10 - 2
 11 - 3
 01 - 1
-Example 2:
 
+Example 2:
 Input: 0
 Output: [0]
 Explanation: We define the gray code sequence to begin with 0.
