95e756fcece9312183fdddcd296c2340272ad43e
JBayona <jordavids_22@hotmail.com>
2020-10-17 18:08:42 -0400

    Is Palindrome Number.

--- isPalindromeNumber.js
+++ isPalindromeNumber.js
@@ -8,16 +8,20 @@ Example 1:
 
 Input: x = 121
 Output: true
+
 Example 2:
 
 Input: x = -121
 Output: false
-Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Explanation: From left to right, it reads -121.
+From right to left, it becomes 121-. Therefore it is not a palindrome.
+
 Example 3:
 
 Input: x = 10
 Output: false
 Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
 Example 4:
 
 Input: x = -101
