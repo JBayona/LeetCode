68393ce5da9668edf83a8efa3fe312a752173756
JBayona <jordavids_22@hotmail.com>
2020-10-31 22:14:08 -0400

    Graph Find Parents Roblox.

--- graphFindParentsRoblox.js
+++ graphFindParentsRoblox.js
@@ -1,5 +1,7 @@
 /*
-// Suppose we have some input data describing a graph of relationships between parents and children over multiple generations. The data is formatted as a list of (parent, child) pairs, where each individual is assigned a unique integer identifier.
+// Suppose we have some input data describing a graph of relationships between
+// parents and children over multiple generations. The data is formatted as a list
+// of (parent, child) pairs, where each individual is assigned a unique integer identifier.
 
 // For example, in this diagram, 3 is a child of 1 and 2, and 5 is a child of 4
 
@@ -14,7 +16,8 @@
 //     (5, 7), (4, 5), (4, 8), (4, 9), (9, 11)
 // ]
 
-// Write a function that takes this data as input and return two collections: one containing all individuals with zero know parents, and one containing
+// Write a function that takes this data as input and return two collections: one containing
+// all individuals with zero know parents, and one containing
 // all individuals with exactly one parent
 
 // Sample input and output:
