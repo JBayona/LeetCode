fb7c87e6c20fcbe906c9f38e2fa7e3f7a685cc2e
JBayona <jordavids_22@hotmail.com>
2020-11-01 21:13:00 -0500

    Bad Version.

--- badVersion.js
+++ badVersion.js
@@ -1,6 +1,4 @@
 /*
-so bad.
-
 Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
 
 You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
@@ -16,7 +14,6 @@ call isBadVersion(4) -> true
 Then 4 is the first bad version.
 
 https://leetcode.com/problems/first-bad-version/description/
-
 /*
 
 /**
