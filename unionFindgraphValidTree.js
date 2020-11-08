d6a3d65070b67ee816d9ee4ed89ea842424715a8
JBayona <jordavids_22@hotmail.com>
2020-11-08 17:20:49 -0500

    Union Find Graph Valid Tree.

--- unionFindgraphValidTree.js
+++ unionFindgraphValidTree.js
@@ -1,5 +1,6 @@
 /*
-Given n nodes labeled from 0 to n-1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
+Given n nodes labeled from 0 to n-1 and a list of undirected edges (each edge is a pair of nodes)
+write a function to check whether these edges make up a valid tree.
 
 For example:
 
