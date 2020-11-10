3dc8d173741d548712845f34d72d1627add68f88
JBayona <jordavids_22@hotmail.com>
2020-11-09 22:46:11 -0500

    Search 2D Matrix.

--- search2DMatrix.js
+++ search2DMatrix.js
@@ -1,12 +1,12 @@
 /*
-Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+Write an efficient algorithm that searches for a value in an m x n matrix.
+This matrix has the following properties:
 
 Integers in each row are sorted in ascending from left to right.
 Integers in each column are sorted in ascending from top to bottom.
+
 Example:
-
 Consider the following matrix:
-
 [
   [1,   4,  7, 11, 15],
   [2,   5,  8, 12, 19],
