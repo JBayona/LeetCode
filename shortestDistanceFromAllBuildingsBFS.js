dd5149236b504c9235af75238849176cd8564ab5
JBayona <jordavids_22@hotmail.com>
2020-11-11 01:18:53 -0500

    Shortest Distance From All Buildings BFS.

--- shortestDistanceFromAllBuildingsBFS.js
+++ shortestDistanceFromAllBuildingsBFS.js
@@ -15,6 +15,7 @@ For example, given three buildings at (0,0), (0,4), (2,2), and an obstacle at (0
 The point (1,2) is an ideal empty land to build a house, as the total travel distance of 3+3+1=7 is minimal. So return 7.
 Note: There will be at least one building. If it is not possible to build such house according to the above rules, return -1.
 https://tenderleo.gitbooks.io/leetcode-solutions-/GoogleHard/317.html
+https://medium.com/@null00/leetcode-shortest-distance-from-all-buildings-ff07c5bea11c
 */
 
 function shortestDistanceFromAllBuildings(grid) {
@@ -23,9 +24,10 @@ function shortestDistanceFromAllBuildings(grid) {
 
   let buildings = 0;
   // Min distance sum of all buildings to dix[x][y]
-  let dis = new Array(ROW);
+  // To all of the buildings
+  let distance = new Array(ROW);
   for(let i = 0; i < ROW; i++) {
-    dis[i] = new Array(COL).fill(0);
+    distance[i] = new Array(COL).fill(0);
   }
 
   // How many buildings we can reach num[x][y]
@@ -36,25 +38,26 @@ function shortestDistanceFromAllBuildings(grid) {
 
   for(let i = 0; i < ROW; i++) {
       for(let j = 0; j < COL; j++) {
+          // If we found a 1, it means we have found a 
           if(grid[i][j] === 1) {
             // Check how many buildings we have in the grid
             buildings++;
-            bfs(grid, dis, num, i, j);
+            bfs(grid, distance, num, i, j);
           }
       }
   }
 
-  console.log(dis);
+  console.log(distance);
   console.log(num);
   console.log(buildings);
 
   let min = Number.MAX_SAFE_INTEGER;
   for(let i = 0; i < ROW; i++) {
       for(let j = 0; j < COL; j++) {
-        // Chech the space is empty, the min distance from the building is not zero
+        // Check if the space is empty, the min distance from the building is not zero
         // and we can reach of buildings based on neighbors
-        if(grid[i][j] === 0 && dis[i][j] !== 0 && num[i][j] === buildings) {
-          min = Math.min(min, dis[i][j]);
+        if(grid[i][j] === 0 && distance[i][j] !== 0 && num[i][j] === buildings) {
+          min = Math.min(min, distance[i][j]);
         }
       }
   }
@@ -65,6 +68,7 @@ function bfs(grid, dis, num, row, col) {
   let ROW = grid.length;
   let COL = grid[0].length;
   
+  // Directions from left, right, top and bottom
   let rowk = [-1,0,0,1];
   let colk = [0,-1,1,0];
 
@@ -74,17 +78,22 @@ function bfs(grid, dis, num, row, col) {
   }
 
   let queue = [];
+  // These row and col are the coordinates we have a building
   queue.push([row, col]);
   let distance = 0;
   while(queue.length) {
+    // Increase the number of units for every "step", "seconds", etc
     distance++;
     let size = queue.length;
     for(let i = 0; i < size; i++) {
       let [x, y] = queue.shift();
+      // Move 4 directions, left, right, top and bottom
       for(let j = 0; j < 4; j++) {
         let newRow = x + rowk[j];
         let newCol = y + colk[j];
 
+        // Grid 0 means that we can go tru the grid so we can move forward
+        // Check that the move is valid so we can go forward
         if(isSafe(grid, newRow, newCol) && grid[newRow][newCol] === 0 && visited[newRow][newCol] === 0) {
           visited[newRow][newCol] = 1;
           dis[newRow][newCol] += distance;
