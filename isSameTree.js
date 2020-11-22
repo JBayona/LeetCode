e7800212a005fa0b3ea21fc56c5c65071e942132
JBayona <jordavids_22@hotmail.com>
2020-11-22 17:27:46 -0500

    Is Same Tree.

--- isSameTree.js
+++ isSameTree.js
@@ -1,7 +1,8 @@
 /*
 Given two binary trees, write a function to check if they are equal or not.
 
-Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+Two binary trees are considered equal if they are structurally
+identical and the nodes have the same value.
 
 https://leetcode.com/problems/same-tree/description/
 */
