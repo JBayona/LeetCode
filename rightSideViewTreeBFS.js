53f1266607f9404a901e586aac621d6dde3fea3c
JBayona <jordavids_22@hotmail.com>
2020-11-23 21:13:41 -0500

    Right Side View Tree BFS.

--- rightSideViewTreeBFS.js
+++ rightSideViewTreeBFS.js
@@ -1,5 +1,6 @@
 /*
-Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+Given a binary tree, imagine yourself standing on the right side of it, return the
+values of the nodes you can see ordered from top to bottom.
 
 Example:
 
