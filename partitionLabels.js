99c4cdd2b01a822ed767a4800472758ccd2003aa
JBayona <jordavids_22@hotmail.com>
2020-11-27 06:22:39 -0600

    Partition Labels.

--- partitionLabels.js
+++ partitionLabels.js
@@ -10,7 +10,8 @@ Output: [9,7,8]
 Explanation:
 The partition is "ababcbaca", "defegde", "hijhklij".
 This is a partition so that each letter appears in at most one part.
-A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it
+splits S into less parts.
 
 https://leetcode.com/problems/partition-labels/
 https://www.youtube.com/watch?v=ED4ateJu86I
