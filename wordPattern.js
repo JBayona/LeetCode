031ea3399c4911c8d130e4942f6576dda907726f
JBayona <jordavids_22@hotmail.com>
2020-11-28 18:57:12 -0600

    Word Pattern.

--- wordPattern.js
+++ wordPattern.js
@@ -5,21 +5,21 @@ Here follow means a full match, such that there is a bijection between a letter
 in pattern and a non-empty word in str.
 
 Example 1:
-
 Input: pattern = "abba", str = "dog cat cat dog"
 Output: true
-Example 2:
 
+Example 2:
 Input:pattern = "abba", str = "dog cat cat fish"
 Output: false
-Example 3:
 
+Example 3:
 Input: pattern = "aaaa", str = "dog cat cat dog"
 Output: false
-Example 4:
 
+Example 4:
 Input: pattern = "abba", str = "dog dog dog dog"
 Output: false
+
 Notes:
 You may assume pattern contains only lowercase letters, and str contains lowercase letters that may be separated by a single space.
 
