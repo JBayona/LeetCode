e5652138bd318579620864f3adee6ccfcc964be5
JBayona <jordavids_22@hotmail.com>
2020-12-20 17:22:48 -0600

    Flood Fill.

--- floodFill.js
+++ floodFill.js
@@ -1,13 +1,14 @@
 /*
-An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+An image is represented by a 2-D array of integers, each integer representing
+the pixel value of the image (from 0 to 65535).
 
-Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel
-value newColor, "flood fill" the image.
+Given a coordinate (sr, sc) representing the starting pixel (row and column) of
+the flood fill, and a pixel value newColor, "flood fill" the image.
 
-To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the
-starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those
-pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels
-with the newColor.
+To perform a "flood fill", consider the starting pixel, plus any pixels
+connected 4-directionally to the starting pixel of the same color as the
+starting pixel, plus any pixels connected 4-directionally to those pixels
+(also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.)
 
 At the end, return the modified image.
 
@@ -16,6 +17,7 @@ Input:
 image = [[1,1,1],[1,1,0],[1,0,1]]
 sr = 1, sc = 1, newColor = 2
 Output: [[2,2,2],[2,2,0],[2,0,1]]
+
 Explanation: 
 From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
 by a path of the same color as the starting pixel are colored with the new color.
