677dd24eac7e63ef90122ef15a77327a18674751
JBayona <jordavids_22@hotmail.com>
2021-01-03 13:51:07 -0600

    Find K Lenght Substrings With No Repeated Characters.

--- findKLengthSubstringsWithNoRepeatedCharactersSlidingWindow.js
+++ findKLengthSubstringsWithNoRepeatedCharactersSlidingWindow.js
@@ -4,12 +4,14 @@ Given a string S, return the number of substrings of length K with no repeated c
 Example 1:
 Input: S = "havefunonleetcode", K = 5
 Output: 6
+
 Explanation: 
 There are 6 substrings they are : 'havef','avefu','vefun','efuno','etcod','tcode'.
 
 Example 2:
 Input: S = "home", K = 5
 Output: 0
+
 Explanation: 
 Notice K can be larger than the length of S. In this case is not possible to find any substring.
 */
