70108307453ca77b0f81607ccbc86b016285e2af
JBayona <jordavids_22@hotmail.com>
2021-01-08 17:31:51 -0600

    Delete Node In BST.

--- deleteNodeInBST.js
+++ deleteNodeInBST.js
@@ -5,6 +5,7 @@ Return the root node reference (possibly updated) of the BST.
 Basically, the deletion can be divided into two stages:
 
 Search for a node to remove.
+
 If the node is found, delete the node.
 
 https://leetcode.com/problems/delete-node-in-a-bst/
