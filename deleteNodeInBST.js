af6a3096dff1edc1342f6f692b27e178382c3f37
JBayona <jordavids_22@hotmail.com>
2021-01-12 21:37:37 -0600

    Delete Node In BST.

--- deleteNodeInBST.js
+++ deleteNodeInBST.js
@@ -3,6 +3,7 @@ Given a root node reference of a BST and a key, delete the node with the given k
 Return the root node reference (possibly updated) of the BST.
 
 Basically, the deletion can be divided into two stages:
+
 Search for a node to remove.
 
 If the node is found, delete the node.
