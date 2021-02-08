b0e66bc9b94039c963815d05d0ff797a3be74b09
JBayona <jordavids_22@hotmail.com>
2021-02-07 19:57:15 -0600

    Leaf Similar Tree.

---
+++ leafSimilarTree.js
@@ -0,0 +1,39 @@
+/*
+Consider all the leaves of a binary tree, from left to right order, the values of
+those leaves form a leaf value sequence.
+
+https://leetcode.com/problems/leaf-similar-trees/
+*/
+
+var leafSimilar = function(root1, root2) {
+    let array = [];
+    let arr = [];
+    
+    // Iterate both trees
+    preorder(root1, array);
+    preorder(root2, arr);
+    
+    // Check result
+    if(array.length !== arr.length) {
+        return false;
+    }
+    
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] !== arr[i]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+function preorder(node, array) {
+    if(!node) {
+        return;
+    }
+    // Find if is a leaf node
+    if(!node.left && !node.right) {
+        array.push(node.val);
+    }
+    preorder(node.left, array);
+    preorder(node.right, array);
+}
