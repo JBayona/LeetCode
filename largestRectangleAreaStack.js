8fb8396866d1a471742f9f9d1702a07c412c1f54
JBayona <jordavids_22@hotmail.com>
2021-02-16 17:29:47 -0600

    Largest Rectangle Area Stack.

--- largestRectangleAreaStack.js
+++ largestRectangleAreaStack.js
@@ -5,7 +5,6 @@ Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
 The largest rectangle is shown in the shaded area, which has area = 10 unit.
 
 Example:
-
 Input: [2,1,5,6,2,3]
 Output: 10
 
