2b13343188022f52635ef962ea4db0c8441040e0
JBayona <jordavids_22@hotmail.com>
2021-02-25 22:01:54 -0600

    Missing Range.

--- missingRange.js
+++ missingRange.js
@@ -4,8 +4,9 @@ all elements are in the inclusive range.
 
 A number x is considered missing if x is in the range [lower, upper] and x is not in nums.
 
-Return the smallest sorted list of ranges that cover every missing number exactly. That is, no element
-of nums is in any of the ranges, and each missing number is in one of the ranges.
+Return the smallest sorted list of ranges that cover every missing number exactly.
+That is, no element of nums is in any of the ranges, and each missing number is
+in one of the ranges.
 
 Each range [a,b] in the list should be output as:
     "a->b" if a != b
