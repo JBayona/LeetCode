5773570ff0a3ed300a86243f5b1e01bdcf03996b
JBayona <jordavids_22@hotmail.com>
2021-02-28 21:36:47 -0600

    Missing Range.

--- missingRange.js
+++ missingRange.js
@@ -13,8 +13,10 @@ Each range [a,b] in the list should be output as:
     "a" if a == b
 
 Example 1:
+
 Input: nums = [0,1,3,50,75], lower = 0, upper = 99
 Output: ["2","4->49","51->74","76->99"]
+
 Explanation: The ranges are:
 [2,2] --> "2"
 [4,49] --> "4->49"
