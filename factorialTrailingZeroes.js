8c6870fcc840b790a2deb81dc85adc5276c6cc77
JBayona <jordavids_22@hotmail.com>
2021-03-15 22:12:27 -0600

    Factorial Trailing Zeroes.

--- factorialTrailingZeroes.js
+++ factorialTrailingZeroes.js
@@ -1,6 +1,5 @@
 /*
 Given an integer n, return the number of trailing zeroes in n!.
-
 Follow up: Could you write a solution that works in logarithmic time complexity?
 
 https://leetcode.com/problems/factorial-trailing-zeroes/
