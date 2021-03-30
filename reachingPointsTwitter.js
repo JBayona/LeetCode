b3427e7ef10c64ae9d5e1205a4d0b67660f26dfb
JBayona <jordavids_22@hotmail.com>
2021-03-29 22:39:44 -0600

    Reaching Points Twitter.

--- reachingPointsTwitter.js
+++ reachingPointsTwitter.js
@@ -1,5 +1,6 @@
 /*
-A move consists of taking a point (x, y) and transforming it to either (x, x+y) or (x+y, y).
+A move consists of taking a point (x, y) and transforming it to
+either (x, x+y) or (x+y, y).
 
 Given a starting point (sx, sy) and a target point (tx, ty), return True if and only if a sequence
 of moves exists to transform the point (sx, sy) to (tx, ty). Otherwise, return False.
