3785a37513dae292ddd54fb8d73c0b9941b4b4b9
JBayona <jordavids_22@hotmail.com>
2021-03-30 21:17:20 -0600

    Accounts Merge Union Find.

--- accountMergeUnionFind.js
+++ accountMergeUnionFind.js
@@ -31,79 +31,89 @@ https://leetcode.com/problems/accounts-merge/
 // Space O(k)
 // Where n is the number of rows, m is the maximum number of columns and k the total size
 // of the text
+// Union Find
 var accountsMerge = function(accounts) {
   if(!accounts.length) {
       return [];
   }
-  let parent = {}
+  
+  // Init parent
+  let parent = {};
   for(let i = 0; i < accounts.length; i++) {
       parent[i] = i;
   }
-  // {0:0, 1:1, 2:2, 3:3..}
+  
+  // parent = {0:0, 1:1, 2:2, 3:3..}
   
   // Union find
-  let emailToIdx = {}; // {john_newyork@mail.com: 0, johnsmith@mail.com: 0....}
+  let emailToIndex = {}; // {johnsmith@mail.com: 0, john_newyork@mail.com: 0,...}
   for(let i = 0; i < accounts.length; i++) {
-    let account = accounts[i];
-    let name = account[0];
-    let emails = account.slice(1);
-    for(let email of emails) {
-      if(email in emailToIdx) {
-        let givenIndex = emailToIdx[email];
-        let parent1 = findParent(givenIndex, parent);
-        let parent2 = findParent(i, parent);
-        parent[parent2] = parent1;
-      } else {
-        // As we are iterating over of all our emails, the i position help us
-        // to mark a distinction of the owner of the email, so of all the emails
-        // will have the same index as parent, every time we find an email already
-        // registered, we update the parent to be the parent one to make union find
-        emailToIdx[email] = i;
+      let account = accounts[i];
+      let name = account[0];
+      // Get the list of emails
+      let emails = account.slice(1);
+      // Iterate over the emails
+      for(let email of emails) {
+          if(email in emailToIndex) {
+              // Union find
+              let givenIndex = emailToIndex[email];
+              let parent1 = findParent(givenIndex, parent);
+              let parent2 = findParent(i, parent);
+              parent[parent2] = parent1;
+          } else {
+              // As we are iterating over of all our emails, the i position help us
+              // to mark a distinction of the owner of the email, so of all the emails
+              // will have the same index as parent, every time we find an email already
+              // registered, we update the parent to be the parent one to make union find
+              emailToIndex[email] = i;
+          }
       }
-    }
   }
-  // console.log(parent); // {0:0, 1:0, 2:2, 3:3..}
-  // {node: parent, node: parent....}
   
+  // console.log(parent); // {0:0, 1:0, 2:2, 3:3..}
   // Merge accounts
   let indexToEmail = {};
   for(let i = 0; i < accounts.length; i++) {
-    let account = accounts[i];
-    //console.log(account.length);
-    for(let j = 1; j < account.length; j++) {
-      let p = findParent(i, parent);
-      if(p in indexToEmail) {
-          indexToEmail[p].add(account[j]);
-      } else {
-          indexToEmail[p] = new Set([account[j]]);
+      let account = accounts[i];
+      // Iterate only for emails
+      for(let j = 1; j < account.length; j++) {
+          let p = findParent(i, parent);
+          if(p in indexToEmail) {
+              indexToEmail[p].add(account[j]);
+          } else {
+              indexToEmail[p] = new Set([account[j]]);
+          }
       }
-    }
   }
   
   // console.log(indexToEmail);
-  /* {
-      '0': Set(3) { 'johnsmith@mail.com', 'john_newyork@mail.com', 'john00@mail.com'},
-      '2': Set(1) { 'mary@mail.com' },
-      '3': Set(1) { 'johnnybravo@mail.com' 
-  } */
+  /*
+      {
+        '0': Set(3) {
+          'johnsmith@mail.com',
+          'john_newyork@mail.com',
+          'john00@mail.com'
+        },
+        '2': Set(1) { 'mary@mail.com' },
+        '3': Set(1) { 'johnnybravo@mail.com' }
+      }
+  */
   
-  // Format the output
+  // Format the result
   let result = [];
   for(let index in indexToEmail) {
-    let account = accounts[index];
-    let name = account[0];
-    let tmp = [name];
-    // Output is required to sort the elements
-    tmp.push(...Array.from(indexToEmail[index]).sort());
-    result.push(tmp);
+      let account = accounts[index];
+      let name = account[0];
+      let tmp = [name];
+      tmp.push(...Array.from(indexToEmail[index]).sort());
+      result.push(tmp);
   }
   return result;
-
 };
 
 function findParent(node, parent) {
   if(parent[node] === node) {
-    return node;
+      return node;
   }
   return findParent(parent[node], parent);
 }
