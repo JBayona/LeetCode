a3f8d9c0a37bf10a645c001b885c182a6195c95f
JBayona <jordavids_22@hotmail.com>
2021-04-08 21:11:02 -0500

    Single Element In Sorted Array.

--- singleElementInSortedArrayNoDuplicateTwitter.js
+++ singleElementInSortedArrayNoDuplicateTwitter.js
@@ -4,11 +4,10 @@ appears exactly twice, except for one element which appears exactly once.
 Find this single element that appears only once.
 
 Example 1:
-
 Input: [1,1,2,3,3,4,4,8,8]
 Output: 2
-Example 2:
 
+Example 2:
 Input: [3,3,7,7,10,11,11]
 Output: 10
 
