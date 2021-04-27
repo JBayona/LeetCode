e6ba0435f5fa2308d682d8e1919279544d153b1b
JBayona <jordavids_22@hotmail.com>
2021-04-26 23:42:45 -0500

    Shortest Path from Source to Destination BFS.

--- shortestPathSourceToDestinationBFS.js
+++ shortestPathSourceToDestinationBFS.js
@@ -2,6 +2,8 @@
 Given a MxN matrix where each element can either be 0 or 1. We need to find the shortest path between a given source
 cell to a destination cell. The path can only be created out of a cell if its value is 1.
 
+We can only move between 1, for 0 it´s not allowed.
+
 Expected time complexity is O(MN).
 
 https://www.geeksforgeeks.org/shortest-path-in-a-binary-maze/
