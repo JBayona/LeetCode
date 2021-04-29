a75729137ad21721e782324680709d3c87af7d5b
JBayona <jordavids_22@hotmail.com>
2021-04-29 01:12:53 -0500

    Oranges Rotting BFS.

--- orangesRottingBFS.js
+++ orangesRottingBFS.js
@@ -11,16 +11,15 @@ Return the minimum number of minutes that must elapse until no cell has a fresh 
 If this is impossible, return -1 instead.
 
 Example 1:
-
 Input: [[2,1,1],[1,1,0],[0,1,1]]
 Output: 4
-Example 2:
 
+Example 2:
 Input: [[2,1,1],[0,1,1],[1,0,1]]
 Output: -1
 Explanation:  The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
-Example 3:
 
+Example 3:
 Input: [[0,2]]
 Output: 0
 Explanation:  Since there are already no fresh oranges at minute 0, the answer is just 0.
