855feb562dd111ba373b50492e441219e6d96a9b
JBayona <jordavids_22@hotmail.com>
2021-05-19 21:55:28 -0500

    Daily Temperatures Next Larger Element.

--- dailyTemperaturesNextLargerElement.js
+++ dailyTemperaturesNextLargerElement.js
@@ -1,7 +1,7 @@
 /*
 Given a list of daily temperatures temperatures, return a list such that, for each
-day in the input, tells you how many days you would have to wait until a warmer temperature. If there
-is no future day for which this is possible, put 0 instead.
+day in the input, tells you how many days you would have to wait until a warmer
+temperature. If there is no future day for which this is possible, put 0 instead.
 
 For example, given the list of temperatures temperatures = [73, 74, 75, 71, 69, 72, 76, 73], your
 output should be [1, 1, 4, 2, 1, 1, 0, 0].
