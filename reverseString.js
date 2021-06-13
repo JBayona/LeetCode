214a1a9f2d6ab6c2257f7dbb236aa1b6ca4f2c81
JBayona <jordavids_22@hotmail.com>
2021-06-13 16:54:52 -0500

    Reverse String.

--- reverseString.js
+++ reverseString.js
@@ -7,23 +7,22 @@ Given s = "hello", return "olleh".
 https://leetcode.com/problems/reverse-string/description/
 */
 
+// Option 1
+// Time O(N)
 var reverseString = function(s) {
-    return s.split('').reverse().join('')
-};
-
-/**
- * @param {string} s
- * @return {string}
- */
-var reverseString = function(s) {
-    let reversed = [];
-    let index = 0;
-    for(let i = s.length-1; i >= 0; i--) {
-        reversed[index++] = s[i];
+    let left = 0;
+    let right = s.length - 1;
+    while(left < right) {
+        let tmp = s[left];
+        s[left] = s[right];
+        s[right] = tmp;
+        left++;
+        right--;
     }
-    return reversed.join('');
+    return s;
 };
 
+// Option 2
 // Recursion
 var reverseString = function(array) {
     return reverseStringHelper(array, 0, array.length - 1);
@@ -40,3 +39,21 @@ function reverseStringHelper(array, start, end) {
     array[end] = tmp;
     return reverseStringHelper(array, start + 1, end - 1);
 }
+
+// Option 3
+var reverseString = function(s) {
+    return s.split('').reverse().join('')
+};
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseString = function(s) {
+    let reversed = [];
+    let index = 0;
+    for(let i = s.length-1; i >= 0; i--) {
+        reversed[index++] = s[i];
+    }
+    return reversed.join('');
+};
