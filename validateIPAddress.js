973165cf33aecc125a3c02c66df0a8c467d578ed
JBayona <jordavids_22@hotmail.com>
2021-06-27 18:54:18 -0500

    Validate IP Address.

--- validateIPAddress.js
+++ validateIPAddress.js
@@ -22,21 +22,17 @@ in the input string.
 
 Example 1:
 Input: "172.16.254.1"
-
 Output: "IPv4"
-
 Explanation: This is a valid IPv4 address, return "IPv4".
+
 Example 2:
 Input: "2001:0db8:85a3:0:0:8A2E:0370:7334"
-
 Output: "IPv6"
-
 Explanation: This is a valid IPv6 address, return "IPv6".
+
 Example 3:
 Input: "256.256.256.256"
-
 Output: "Neither"
-
 Explanation: This is neither a IPv4 address nor a IPv6 address.
 
 https://leetcode.com/problems/validate-ip-address/
