dab716c949047a286a308532787e32cd72778bdd
JBayona <jordavids_22@hotmail.com>
2021-06-30 23:37:40 -0500

    Max Consecutive Ones Sliding Window.

--- maxConsecutiveOnesSlidingWindow.js
+++ maxConsecutiveOnesSlidingWindow.js
@@ -1,6 +1,6 @@
 /*
-Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array
-if you can flip at most k 0's.
+Given a binary array nums and an integer k, return the maximum number
+of consecutive 1's in the array if you can flip at most k 0's.
 
 Example 1:
 Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
