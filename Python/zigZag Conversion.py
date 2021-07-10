d2590962dffbd25408ea36fd38d94b20fc99213f
JBayona <jordavids_22@hotmail.com>
2021-07-10 12:03:38 -0500

    Zig Zig Conversion.

--- Python/zigZag Conversion.py
+++ Python/zigZag Conversion.py
@@ -7,7 +7,6 @@ And then read line by line: "PAHNAPLSIIGYIR"
 Write the code that will take a string and make this conversion given a number of rows:
 string convert(string text, int nRows);
 convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
-
 [ 'P', '', '' ]
 [ 'P', 'A', '' ]
 [ 'P', 'A', 'Y' ]
