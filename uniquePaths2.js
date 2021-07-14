265e43ce4078745444c896633e27a761854df622
JBayona <jordavids_22@hotmail.com>
2021-07-13 23:53:20 -0500

    Unique Paths 2.

--- uniquePaths2.js
+++ uniquePaths2.js
@@ -36,3 +36,41 @@ var uniquePathsWithObstacles = function(obstacleGrid) {
     }
     return obstacleGrid[m-1][n-1];
 };
+
+// DP
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    let m = obstacleGrid.length;
+    let n = obstacleGrid[0].length;
+    let dp = new Array(m);
+    for(let i = 0; i < m; i++) {
+        dp[i] = new Array(n).fill(0);
+    }
+    
+    // Row
+    for(let i = 0; i < m; i++) {
+        if(obstacleGrid[i][0] === 0) {
+            dp[i][0] = 1;
+        } else {
+            break;
+        }
+    }
+    
+    // Column
+    for(let i = 0; i < n; i++) {
+        if(obstacleGrid[0][i] === 0) {
+            dp[0][i] = 1;
+        } else {
+            break;
+        }
+    }
+    
+    for(let i = 1; i < m; i++) {
+        for(let j = 1; j < n; j++) {
+            if(obstacleGrid[i][j] === 0) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+    }
+    
+    return dp[m-1][n-1];
+};
