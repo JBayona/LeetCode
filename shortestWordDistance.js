6971985e7bd8f97fe9fe5ce4f9b8b7d2705495f6
JBayona <jordavids_22@hotmail.com>
2021-10-20 21:47:40 -0400

    Shortest Word Distance.

--- shortestWordDistance.js
+++ shortestWordDistance.js
@@ -3,6 +3,7 @@ Given a list of words and two words word1 and word2, return the shortest distanc
 these two words in the list.
 
 Example:
+
 Assume that words =["practice", "makes", "perfect", "coding", "makes"].
 Input: word1 = “coding”, word2 = “practice”
 Output: 3
