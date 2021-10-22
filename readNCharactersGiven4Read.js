85cf98cf6170acc7cd0562bc582060a52cc87559
JBayona <jordavids_22@hotmail.com>
2021-10-21 23:22:24 -0400

    Read N Characters Given 4 Read.

--- readNCharactersGiven4Read.js
+++ readNCharactersGiven4Read.js
@@ -28,3 +28,32 @@ const read = (buffer, n) => {
         buffer[total++] = tmp[i];
     }
 }
+
+// Follow-up: The read function may be called multiple times.
+let queue = [];
+const read = (buffer, n) => {
+    let index = 0
+    // If the queue is not empty first read from the queue
+    while(queue.length && i < n) {
+        buffer[index++] = queue.shift();
+    }
+    
+    if(index == n) return n;
+
+    tmp = new Array(4);
+    while(index < n) {
+        let count = read4(buffer);
+        let i = 0;
+        for(; i < count && index < n; i++) {
+            buffer[index++] = tmp[i];
+        }
+        while(i < count) {
+            queue.push(tmp[i++]);
+        }
+        // Nothing left to read from the file
+        if(count < 4) {
+            break;
+        }
+    }
+    return index;
+}
