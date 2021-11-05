ab5d4fdd12faff89cac6958582d82ec5991427cf
JBayona <jordavids_22@hotmail.com>
2021-11-04 23:47:18 -0600

    Longest Substring With At Most Two Distinct Characters.

--- longestSubstringWithAtMostKDistinctChars.js
+++ longestSubstringWithAtMostKDistinctChars.js
@@ -5,60 +5,33 @@ T is "ece" which length is 3
 
 https://www.youtube.com/watch?v=RHFrVNmlyA8
 https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
-
-Time Complexity O(N^2)
-Space Complexity O(N^2)
 */
-var lengthOfLongestSubstringKDistinct = function(str, k) {
-  let hash = {};
-  let maxLength = 0;
-  let result = 0;
-
-  for(let i = 0; i < str.length; i++) {
-    // Clean hash and maxLenght
-    hash = {};
-    maxLength = 0;
-    for(let j = i; j < str.length; j++) {
-      // Override hash with new indexes
-      hash[str[j]] = j;
-      // While our hash stores less than k characteres we should update our values
-      if(Object.keys(hash).length <= k) {
-        maxLength++;
-        result = Math.max(maxLength, result);
-      } else {
-        // If we find more than k, break the loop and check the next iteration
-        break;
-      }
-    }
-  }
-  return result;
-}
 
 // Time O(N)
 // Space O(N)
 // https://www.youtube.com/watch?v=8AQra0p_HmI
 // https://www.programcreek.com/2013/02/longest-substring-which-contains-2-unique-characters/
-var lengthOfLongestSubstringKDistinct = function(str, k) {
+var lengthOfLongestSubstringKDistinct = function (str, k) {
   let hash = {};
   let result = 0;
   let start = 0;
 
-  for(let i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
     let c = str[i];
-    if(c in hash) {
+    if (c in hash) {
       hash[c]++;
     } else {
       hash[c] = 1;
     }
 
-    if(Object.keys(hash).length <= k) {
+    if (Object.keys(hash).length <= k) {
       result = Math.max(result, i - start + 1);
     } else {
       // If we have rebased the limit, remove the most left element
-      while(Object.keys(hash).length > k) {
+      while (Object.keys(hash).length > k) {
         let l = str[start];
         let count = hash[l];
-        if(count === 1) {
+        if (count === 1) {
           delete hash[l];
         } else {
           hash[l]--;
@@ -68,7 +41,34 @@ var lengthOfLongestSubstringKDistinct = function(str, k) {
     }
   }
   return result;
-}
+};
+
+// Time Complexity O(N^2)
+// Space Complexity O(N^2)
+var lengthOfLongestSubstringKDistinct = function (str, k) {
+  let hash = {};
+  let maxLength = 0;
+  let result = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    // Clean hash and maxLenght
+    hash = {};
+    maxLength = 0;
+    for (let j = i; j < str.length; j++) {
+      // Override hash with new indexes
+      hash[str[j]] = j;
+      // While our hash stores less than k characteres we should update our values
+      if (Object.keys(hash).length <= k) {
+        maxLength++;
+        result = Math.max(maxLength, result);
+      } else {
+        // If we find more than k, break the loop and check the next iteration
+        break;
+      }
+    }
+  }
+  return result;
+};
 
 str = "karappa";
 k = 2; // appa - result = 4
