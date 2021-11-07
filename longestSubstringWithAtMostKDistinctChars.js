54a739fbe7811afd79e7c5585b3ff66ab2c64bfb
JBayona <jordavids_22@hotmail.com>
2021-11-07 17:29:32 -0600

    Longest Substring With At Most K Distinct Chars.

--- longestSubstringWithAtMostKDistinctChars.js
+++ longestSubstringWithAtMostKDistinctChars.js
@@ -1,7 +1,7 @@
 /*
 Given a string, find the length of the longest substring T that contains at most k distinct characteres.
 
-For example, Given s = "eceba" and k = 2, is "ece" which length is 3
+For example, Given s = "eceba" and k = 2, is "ece" which length is 3.
 
 https://www.youtube.com/watch?v=RHFrVNmlyA8
 https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
