e2534c50a66c43eeed252ae3229d7b568e9355d7
JBayona <jordavids_22@hotmail.com>
2021-11-10 21:13:59 -0600

    Insert Interval.

--- insertInterval.js
+++ insertInterval.js
@@ -16,6 +16,34 @@ Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
 https://leetcode.com/problems/insert-interval/
 */
 
+// Time O(N)
+var insert = function(intervals, newInterval) {
+  let result = [];
+  let i = 0;
+
+  // Here we handle all of the non overlapping cases that we are sure
+  while(i < intervals.length && intervals[i][1] < newInterval[0]) {
+    result.push(intervals[i++]);
+  }
+
+  while(i < intervals.length && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+    newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+    i++;
+  }
+
+  //Add the result of the overlapping 
+  result.push(newInterval);
+
+  // Add the rest of the cases we don´t match the conditions above
+  while(i < intervals.length) {
+    result.push(intervals[i++]);
+  }
+
+  return result;
+};
+
+// Interval class
 function Interval(start, end) {
   this.start = start;
   this.end = end;
