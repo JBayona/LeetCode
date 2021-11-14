2191526f3f04b19d7198f9d1bf6db3073cc74252
JBayona <jordavids_22@hotmail.com>
2021-11-13 19:32:34 -0600

    Non Overlapping Intervals.

--- nonOverlappingIntervals.js
+++ nonOverlappingIntervals.js
@@ -4,6 +4,7 @@ Given an array of intervals intervals where intervals[i] = [starti, endi], retur
 Example 1:
 Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
 Output: 1
+
 Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
 
 Example 2:
