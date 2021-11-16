49471abccf639cca265e9cb8b5ed188dc93556e1
JBayona <jordavids_22@hotmail.com>
2021-11-15 23:19:30 -0600

    Meeting Rooms.

---
+++ Python/meetingRooms.py
@@ -0,0 +1,29 @@
+"""
+Given an array of meeting time intervalsof start and end times [[s1,ed], [s2, e2]..] determine if a
+person coudld attend al meetings
+
+Description: https://aaronice.gitbook.io/lintcode/sweep-line/meeting-rooms
+
+https://leetcode.com/problems/meeting-rooms/
+https://www.youtube.com/watch?v=i2bBG7CaVxs
+https://github.com/JBayona/Facebook-Interview-Coding-1/blob/master/253.%20Meeting%20Rooms%20II.java
+"""
+
+# Greedy algorithm
+def canAttendMeetings(intervals):
+    intervals = sorted(intervals, key = lambda x: [x[0], x[1]])
+    
+    prev = intervals[0]
+    print(intervals)
+    for i in range(1, len(intervals)):
+        # If there's an overlap, it's not possible to attend the meeetings
+        if intervals[i][0] < prev[1]:
+            return False
+        prev = intervals[i]
+        
+    return True
+        
+
+# meetings = [[0,30],[5,10],[15,20]] # Output: False
+meetings = [[7,10],[2,4]] # Output: True
+print(canAttendMeetings(meetings))
