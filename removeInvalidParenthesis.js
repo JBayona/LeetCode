0a72299c433bc80499059bd837634c6127842d32
JBayona <jordavids_22@hotmail.com>
2021-11-20 19:41:50 -0600

    Remove Invalid Parenthesis.

--- removeInvalidParenthesis.js
+++ removeInvalidParenthesis.js
@@ -1,7 +1,6 @@
 /*
 Remove the minimum number of invalid parentheses in order to make the input string valid.
 Return all possible results.
-
 Note: The input string may contain letters other than the parentheses ( and ).
 
 Example 1:
