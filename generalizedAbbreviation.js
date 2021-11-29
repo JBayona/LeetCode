b0a1aab4679558ce3b389a3ddf577dd993487a46
JBayona <jordavids_22@hotmail.com>
2021-11-28 19:02:46 -0600

    Generalized Abbreviation.

--- generalizedAbbreviation.js
+++ generalizedAbbreviation.js
@@ -6,6 +6,7 @@ into "a3e" ("bcd" turned into "3"), "1bcd1" ("a" and "e" both turned into "1"), 
 
 Given a string word, return a list of all the possible generalized abbreviations of word.
 Return the answer in any order.
+
 Example 1:
 Input: word = "word"
 Output: ["4","3d","2r1","2rd","1o2","1o1d","1or1","1ord","w3","w2d","w1r1","w1rd","wo2","wo1d","wor1","word"]
