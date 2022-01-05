31f8bc6eb1641f61258885f8f62b3f9e4cb29c05
JBayona <jordavids_22@hotmail.com>
2022-01-05 01:04:19 -0600

    Binary Search.

--- binarySearch.js
+++ binarySearch.js
@@ -2,13 +2,11 @@
 Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. If target exists, then return its index, otherwise return -1.
 
 Example 1:
-
 Input: nums = [-1,0,3,5,9,12], target = 9
 Output: 4
 Explanation: 9 exists in nums and its index is 4
 
 Example 2:
-
 Input: nums = [-1,0,3,5,9,12], target = 2
 Output: -1
 Explanation: 2 does not exist in nums so return -1
@@ -24,8 +22,7 @@ Time Complexity O(log(N))
 var search = function(nums, target) {
     let mid = 0;
     let start = 0;
-    let end = nums.length-1;
-    
+    let end = nums.length-1;    
     while(start <= end) {
         mid = Math.floor((start+end)/2);
         if(target === nums[mid]) {
@@ -37,7 +34,6 @@ var search = function(nums, target) {
             start = mid + 1;
         }
     }
-    
     return -1;
 };
 
@@ -52,7 +48,6 @@ var search = function(nums, target) {
 };
 
 function binarySearch(start, end, nums, target) {
-    
     // Base Case
     if(start <= end) {
         mid = Math.floor((start+end)/2);
@@ -65,7 +60,6 @@ function binarySearch(start, end, nums, target) {
             return binarySearch(mid+1, end, nums, target);
         }
     }
-    
     return -1
 }
 
