d8a51793c42fad91e44e227a3d0f2367c057fe65
JBayona <jordavids_22@hotmail.com>
2022-01-05 01:07:03 -0600

    Bad Version.

--- badVersion.js
+++ badVersion.js
@@ -4,13 +4,10 @@ Suppose you have n versions [1, 2, ..., n] and you want to find out the first ba
 You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
 
 Example:
-
 Given n = 5, and version = 4 is the first bad version.
-
 call isBadVersion(3) -> false
 call isBadVersion(5) -> true
 call isBadVersion(4) -> true
-
 Then 4 is the first bad version.
 
 https://leetcode.com/problems/first-bad-version/description/
@@ -46,8 +43,7 @@ var solution = function(isBadVersion) {
       } else {
         left = middle + 1;
       }
-    }
-    
+    }    
     return left;
   };
 };
