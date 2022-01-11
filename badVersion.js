d748f0d54d3ec9c7bb8df67c42769721b514f540
JBayona <jordavids_22@hotmail.com>
2022-01-10 19:21:25 -0600

    Bad Version Binary Search.

--- badVersion.js
+++ badVersion.js
@@ -1,7 +1,9 @@
 /*
-Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one
+which causes all the following ones to be bad.
 
-You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+You are given an API bool isBadVersion(version) which will return whether version is bad.
+Implement a function to find the first bad version. You should minimize the number of calls to the API.
 
 Example:
 Given n = 5, and version = 4 is the first bad version.
