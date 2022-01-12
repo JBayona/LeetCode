2c227ea8a37ad24b9745b30ff5086716188bc366
JBayona <jordavids_22@hotmail.com>
2022-01-11 22:52:42 -0600

    Merge Trees.

--- mergeTrees.js
+++ mergeTrees.js
@@ -1,7 +1,8 @@
 /*
-Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
-
-You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the
+two trees are overlapped while the others are not.
+You need to merge them into a new binary tree. The merge rule is that if two nodes overlap
+then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
 
 Example 1:
 
@@ -19,7 +20,6 @@ Merged tree:
        4   5
       / \   \ 
      5   4   7
- 
 
 Note: The merging process must start from the root nodes of both trees.
 
