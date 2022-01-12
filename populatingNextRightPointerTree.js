37658a9e9b47ca830df529c115d748c5a4f7156f
JBayona <jordavids_22@hotmail.com>
2022-01-11 23:09:02 -0600

    Populating Next Right Node Pointer Tree.

--- populatingNextRightPointerTree.js
+++ populatingNextRightPointerTree.js
@@ -1,5 +1,6 @@
 /*
-You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+You are given a perfect binary tree where all leaves are on the same level, and every parent
+has two children. The binary tree has the following definition:
 
 struct Node {
   int val;
@@ -7,14 +8,11 @@ struct Node {
   Node *right;
   Node *next;
 }
-Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
-
+Populate each next pointer to point to its next right node. If there is no next right node, the next
+pointer should be set to NULL.
 Initially, all next pointers are set to NULL.
 
- 
-
 Follow up:
-
 You may only use constant extra space.
 Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
 
@@ -39,10 +37,8 @@ var connect = function(root) {
     if(!root) {
         return null;
     }
-    
     let queue = [];
     queue.push(root);
-    
     while(queue.length) {
         let size = queue.length;
         let prev = null;
@@ -78,7 +74,6 @@ function inorder(node, level) {
     if(!node) {
         return;
     }
-    
     inorder(node.left, level + 1);
     node.next = null;
     if(level in visited) {
