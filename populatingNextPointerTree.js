d514912e2c2b9cb7593609c884f96ef0f58a58e6
JBayona <jordavids_22@hotmail.com>
2022-01-12 18:26:29 -0600

    Populating Next Pointer Tree.

--- populatingNextPointerTree.js
+++ populatingNextPointerTree.js
@@ -10,7 +10,6 @@ struct Node {
 Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
 
 Initially, all next pointers are set to NULL.
-
 Follow up:
 
 You may only use constant extra space.
