fa932003f3d02da6842cf9897fabde93e7d50ff2
JBayona <jordavids_22@hotmail.com>
2022-01-16 13:58:16 -0600

    House Robber.

--- houseRobber.js
+++ houseRobber.js
@@ -30,6 +30,29 @@ https://leetcode.com/problems/house-robber/description/
  * @param {number[]} nums
  * @return {number}
  */
+
+// DP
+var rob = function(nums) {
+    if(!nums.length) {
+        return 0;
+    }
+    if(nums.length === 1) {
+        return nums[0];
+    }
+    if(nums.length === 2) {
+        return Math.max(nums[0], nums[1]);
+    }
+    
+    let dp = new Array(nums.length). fill(0);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    for(let i = 2; i < nums.length; i++) {
+        dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+    }
+    return dp[nums.length - 1];
+};
+
+// Option 2
 var rob = function(nums) {
     if(nums.length === 0) {
         return 0;
