c945b5155e39a3d6f7dbfa64be584bc500b15f5c
JBayona <jordavids_22@hotmail.com>
2022-01-16 14:27:53 -0600

    Triangle Min Path DP.

--- triangleMinPathDP.js
+++ triangleMinPathDP.js
@@ -36,3 +36,31 @@ var minimumTotal = function(triangle) {
   // Top will have the maximum value
   return triangle[0][0];
 };
+
+// DP
+var minimumTotal = function(triangle) {
+  // Copy from the array
+  const arr = [...triangle];
+  // DP Array with N columns filled with 0s
+  const dp = Array.from(Array(triangle.length), () => Array(triangle.length).fill(0));
+  dp[0][0] = arr[0][0];
+  
+  for (let i = 1; i < arr.length; i++) {
+      for (let j = 0; j < arr[i].length; j++) {
+          // We can´t move the i index as we are only allowed to
+          // move i and i-1
+          if (j === 0) {
+              dp[i][j] = dp[i - 1][j] + arr[i][j];
+              continue;
+          }
+          // Avoid out of index, we are in the last element
+          if (j === arr[i].length - 1) {
+              dp[i][j] = dp[i - 1][j - 1] + arr[i][j];
+              continue;
+          }
+        
+          dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
+      }
+  }   
+  return Math.min(...dp[dp.length - 1]);    
+};
