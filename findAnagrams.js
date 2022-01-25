a9a648577c24cacf49272e5de42c156796eb4166
JBayona <jordavids_22@hotmail.com>
2022-01-24 19:58:45 -0600

    Find Anagrams.

--- findAnagrams.js
+++ findAnagrams.js
@@ -37,13 +37,84 @@ leetcode.com
 
 */
 
-/**
- * @param {string} s
- * @param {string} p
- * @return {number[]}
- */
+// Sliding Window
+// Time O(N)
+// Space O(N)
 var findAnagrams = function(s, p) {
-    
+  // p is greater then s
+  if(s.length < p.length) {
+      return [];
+  }
+  
+  let mapS = {};
+  // We want a window of size p
+  for(let i = 0; i < p.length; i++) {
+      let elem = s[i];
+      if(elem in mapS) {
+          mapS[elem]++;
+      } else {
+          mapS[elem] = 1;
+      }
+  }
+  
+  // This won't change
+  let mapP = {}
+  for(let i = 0; i < p.length; i++) {
+      let elem = p[i];
+      if(elem in mapP) {
+          mapP[elem]++;
+      } else {
+          mapP[elem] = 1;
+      }
+  }
+  
+  let result = [];
+  for(let i = p.length; i < s.length; i++) {
+      if(isMapEquals(mapS, mapP)) {
+          result.push(i - p.length);
+      }
+      
+      // Expand the window
+      let c = s[i];
+      // Add the new char coming into the window
+      if(c in mapS) {
+          mapS[c]++;
+      } else {
+          mapS[c] = 1;
+      }
+      
+      // Remove the left char of the window
+      c = s[i - p.length];
+      if(mapS[c] === 1) {
+          delete mapS[c];
+      } else if(mapS[c] > 1) {
+          mapS[c]--;
+      }
+  }
+  
+  // The last window
+  if(isMapEquals(mapS, mapP)) {
+      result.push(s.length - p.length);
+  }
+  return result;
+};
+
+function isMapEquals(map1, map2) {
+  if(Object.keys(map1).length !== Object.keys(map2).length) {
+      return false;
+  }
+  
+  for(let prop in map1) {
+      if(!(prop in map2) || map1[prop] !== map2[prop]) {
+          return false;
+      }
+  }
+  return true;
+}
+
+/*
+var findAnagrams = function(s, p) {
+  
   let pCount = countWord(p);
   let result = [];
   
@@ -57,9 +128,9 @@ var findAnagrams = function(s, p) {
   
   return result;
 };
-    
+  
 function countWord(word) {
-    
+  
   let count = new Array(26).fill(0);
   for(let i = 0; i < word.length; i++) {
       let index = word[i].charCodeAt(0) - 'a'.charCodeAt(0);
@@ -68,3 +139,4 @@ function countWord(word) {
   
   return count;
 }
+*/
