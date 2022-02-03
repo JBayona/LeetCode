3bac99627bd994d117071d0f6bd92d6e16686d7b
JBayona <jordavids_22@hotmail.com>
2022-02-02 21:12:02 -0600

    Word Break.

--- wordBreak.js
+++ wordBreak.js
@@ -19,6 +19,89 @@ https://www.programcreek.com/2012/12/leetcode-solution-word-break/
 https://youtu.be/WepWFGxiwRs
 */
 
+// BFS
+// Time O(n^3)
+// Space O(n^3)
+var wordBreak = function(s, wordDict) {
+  let set = new Set(wordDict);
+  let queue = [];
+  let visited = new Set();
+  
+  queue.push(0);
+  while(queue.length) {
+      let start = queue.shift();
+      if(visited.has(start)) {
+          continue;
+      }
+      // + 1 because it's exclusive
+      for(let i = start + 1; i < s.length + 1; i++) {
+          if(set.has(s.substring(start, i))) {
+              queue.push(i);
+              if(i === s.length) {
+                  return true;
+              }
+          }
+      }
+      visited.add(start);
+  }
+  return false;
+};
+
+/*
+function wordBreak(str, array) {
+  let dp = new Array(str.length + 1).fill(false);
+  // Initial state
+  dp[0] = true;
+  for(let i = 0; i < str.length; i++) {
+      for(let j = 0; j <= i; j++) {
+          if(!dp[j]) {
+              continue;
+          }
+          // if i = 3, j will be 0, 1, 2, 3
+          if(array.includes(str.substring(j, i + 1))) {
+              dp[i + 1] = true;
+              break;
+          }
+      }
+  }
+  // The last element will be the result
+  return dp[str.length];
+}
+*/
+
+/*
+// Recursion approach
+/*
+                    WB[code]
+  c && WB[ode]     co && WB[de]       cod && WB[e]         code && WB[] = true
+// O(2^n)
+https://www.youtube.com/watch?v=hLQYQ4zj0qg
+*/
+/*
+var wordBreak = function(s, wordDict) {
+  const map = {}
+  function run(s) {
+      if (map.hasOwnProperty(s)) {
+          return map[s]
+      } else if (s=='') {
+          return true
+      }
+      for (let i=0;i<=s.length;i++) {
+          if (
+              wordDict.includes(s.slice(i)) &&
+              run(s.slice(0, i))
+          ){
+              map[s] = true
+              return true
+          }
+      }
+      map[s] = false
+      return false
+  }
+  return run(s)
+};
+*/
+
 // Time O(n^2)
 function wordBreak(str, array) {
   return helper(str, array, 0);
