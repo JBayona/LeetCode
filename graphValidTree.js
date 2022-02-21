bb8aefe3ce8af28743e487e38f90670e466f8f32
JBayona <jordavids_22@hotmail.com>
2022-02-20 22:39:07 -0600

    Graph Valid Tree.

--- graphValidTree.js
+++ graphValidTree.js
@@ -4,7 +4,6 @@ edges (each edge is a pair of nodes), write
 a function to check whether these edges make up a valid tree.
 
 For example:
-
 Given n = 5 and edges = [[0, 1], [0, 2], [0, 3], [1, 4]], return true.
 Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.
 
