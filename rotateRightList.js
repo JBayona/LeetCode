08f7d2fb741855b6b246fc3e3d62e08625a12197
JBayona <jordavids_22@hotmail.com>
2022-03-10 22:53:20 -0600

    Rotate Right List.

--- rotateRightList.js
+++ rotateRightList.js
@@ -8,118 +8,54 @@ return 4->5->1->2->3->NULL.
 https://leetcode.com/problems/rotate-list/description/
 */
 
-//Option 1 
+//Option 1
 
-function ListNode(val, next){
+function ListNode(val, next) {
   this.val = val;
   this.next = next || null;
 }
 
-function getLength(head){
-  let list = head;
-  let len = 0;
-  while(list){
-    len++;
-    list = list.next;
+var rotateRight = function (head, k) {
+  if (!head) {
+    return head;
   }
-  return len;
-}
+  // Step 1 - Get length of the list
+  let len = 0;
+  let current = head;
+  while (current) {
+    len++;
+    current = current.next;
+  }
 
-var rotateRight = function(head, k) {
-    let len = getLength(head);
-    //Get the k element in the list
-    let index = ((len - k)%len + len)%len;
-    //console.log(index);
-    //Find the first element of the tree
-    let list = head;
-    let tmp = 0;
-    let count = 0;
-    let resultHead = null;
-    let result = null;
-    let flag = false;
-    //Adding element while reach the number of lenght
-    while(count < len){
-      if(flag){
-        //If we reach the last element lest start from 0
-        if(!list){
-          list = head;
-        }
-        if(list){
-          result.next = new ListNode(list.val);
-          result = result.next;
-          count++;
-        }
-      }
-      //Find the option of the first element
-      if(tmp === index){
-        resultHead = new ListNode(list.val);
-        result = resultHead;
-        count++;
-        flag = true;
-      }
-      list = list.next;
-      tmp++;
-    }
-    return resultHead;
+  // Step 2 - Number of rotations, we need to use module in case we overflow
+  k = k % len;
+
+  // Step 3 - Move pointer k positions ahead
+  current = head;
+  while (k) {
+    k--;
+    current = current.next;
+  }
+
+  // Step 4, since the current is k steps ahead, then it will tell us
+  // the kth position from the end, then current.next == null it means that we reach the last node
+  // our prev pointer will have kth position from the end, here the is the number of rotations
+  let prev = head;
+  while (current.next) {
+    current = current.next;
+    prev = prev.next;
+  }
+
+  // Step 5 - Modify the head and last node
+  current.next = head;
+  head = prev.next;
+  prev.next = null;
+  return head;
 };
 
-list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+list = new ListNode(
+  1,
+  new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))
+);
 k = 2;
-console.log(rotateRight(list,k));
-
-//Option 2
-
-function ListNode(val, next){
-  this.val = val;
-  this.next = next || null;
-}
-
-var rotateRight = function(head, k) {
-    var pointerA = head,
-        pointerB = head,
-        len = 0,
-        newHead,
-        i;
-    
-    if (head === null) {
-        return null;
-    }
-    
-    //Contamos el length de la lista
-    while (pointerA) {
-        pointerA = pointerA.next;
-        len++;
-    }
-    
-    //En caso de desbordar
-    k = k % len;
-    
-    if (k === 0 || len === 1) {
-        return head;
-    }
-    
-    //Recorremos A k posiciones
-    pointerA = head;
-    for (i = 0; i < k; i++) {
-        pointerA = pointerA.next;
-    }
-    
-    /*Mientras tengamos A, posicionamos A en el 
-    último elemento y vamos recorriendo B, estarán a
-    k posiciones de distancia A de B*/
-    while (pointerA && pointerA.next) {
-        pointerA = pointerA.next;
-        pointerB = pointerB.next;
-    }
-    
-    //Formamos el resultado
-    newHead = pointerB.next;
-    pointerB.next = null;
-    pointerA.next = head;
-    
-    return newHead;
-};
-
-list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-k = 2;
-console.log(rotateRight(list,k));
+console.log(rotateRight(list, k));
