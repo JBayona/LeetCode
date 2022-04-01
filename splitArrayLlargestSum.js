cd65714d09bf03063147e6a983861e43f7d2cac7
JBayona <jordavids_22@hotmail.com>
2022-03-31 20:08:36 -0400

    Split Array Largest Sum.

--- splitArrayLlargestSum.js
+++ splitArrayLlargestSum.js
@@ -1,7 +1,6 @@
 /*
 Given an array nums which consists of non-negative integers and an integer m, you can
 split the array into m non-empty continuous subarrays.
-
 Write an algorithm to minimize the largest sum among these m subarrays.
 Example 1:
 Input: nums = [7,2,5,10,8], m = 2
