2afb73110c25f303e0dd5c005847d034df33710a
JBayona <jordavids_22@hotmail.com>
2022-04-05 22:29:23 -0400

    Path Sum 2.

--- pathSumll.js
+++ pathSumll.js
@@ -1,5 +1,6 @@
 /*
-Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+Given a binary tree and a sum, find all root-to-leaf paths where each path's
+sum equals the given sum.
 Note: A leaf is a node with no children.
 Example:
 
