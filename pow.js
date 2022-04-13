a704d733fc66da0a56bde75b09b807de0976aa9e
JBayona <jordavids_22@hotmail.com>
2022-04-12 23:31:38 -0400

    Pow.

--- pow.js
+++ pow.js
@@ -4,11 +4,14 @@ Implement pow(x, n).
 https://leetcode.com/problems/powx-n/description/
 */
 
+// 1. If n is negative, we still have to continue the process that we would for a positive n, and simply divide the result by 1.
+// 2. 2^10 is equivalent to (2^5) * (2^5). And (2^5) is equivalent to (2^2) * (2^2) * 2. Depending on whether n is odd or even.
+// 3. So the problem can be continously divided. And results from one calculation, can simply be reused.
+// 4. Finally, anything to the power of 0 is always 1. That is, if n is '0', the answer is '1'.
 var myPow = function(x, n) {
     if (n < 0) {
         return 1 / power(x, n);
     }
-    
     return power(x, n);
 };
 
@@ -16,16 +19,11 @@ function power(x, n) {
     if (n === 0) {
         return 1;
     }
-    
     var v = power(x, parseInt(n/2));
-    
+    // Even number (par)
     if (n % 2 === 0) {
         return v * v;
     }
-    
+    // Odd number (impar)
     return v * v * x;
 }
-
-x = 4;
-n = 2;
-console.log(myPow(x,n));
