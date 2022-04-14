4db566f90d5aa380ef8aa585e3c4de425c33aa95
JBayona <jordavids_22@hotmail.com>
2022-04-13 22:06:41 -0400

    Min Mooves To Equal Array.

--- minMovesToEqualArray2Twitter.js
+++ minMovesToEqualArray2Twitter.js
@@ -1,5 +1,6 @@
 /*
-Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
+Given an integer array nums of size n, return the minimum number of moves required
+to make all array elements equal.
 In one move, you can increment or decrement an element of the array by 1.
 Test cases are designed so that the answer will fit in a 32-bit integer.
 
