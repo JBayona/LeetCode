5f68fe2a36ad6ce68d13999291316ed3aa37c955
JBayona <jordavids_22@hotmail.com>
2022-04-16 13:23:04 -0400

    Upside Down Binary Tree.

--- binaryTreeUpsideDownYoutube.js
+++ binaryTreeUpsideDownYoutube.js
@@ -1,6 +1,8 @@
 /*
-Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node) or empty,
-flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
+Given a binary tree where all the right nodes are either leaf nodes
+with a sibling (a left node that shares the same parent node) or empty,
+flip it upside down and turn it into a tree where the original right nodes
+turned into left leaf nodes. Return the new root.
 
 Given a binary tree, the task is to flip the binary tree towards right direction that is clockwise.
 See below examples to see the transformation.
