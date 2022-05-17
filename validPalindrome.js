21eb1e38477352e8c3aec8133d604ae7d204a703
JBayona <jordavids_22@hotmail.com>
2022-05-16 23:09:28 -0500

    Valid Palindrome.

--- validPalindrome.js
+++ validPalindrome.js
@@ -2,7 +2,6 @@
 Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 
 Note: For the purpose of this problem, we define empty string as valid palindrome.
-
 Example 1:
 
 Input: "A man, a plan, a canal: Panama"
