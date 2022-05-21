eb985c835e6c27296591f99614ac85160540363e
JBayona <jordavids_22@hotmail.com>
2022-05-21 16:51:23 -0500

    Valid Number.

--- validNumber.js
+++ validNumber.js
@@ -7,6 +7,7 @@ Some examples:
 "1 a" => false
 "2e10" => true
 Note: It is intended for the problem statement to be ambiguous.
+
 You should gather all requirements up front before implementing one.
 
 https://leetcode.com/problems/valid-number/description/
