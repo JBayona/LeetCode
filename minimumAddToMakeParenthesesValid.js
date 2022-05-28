b4f1bfc0d8c8ee0365b4d44466c43e3747900a0b
JBayona <jordavids_22@hotmail.com>
2022-05-28 13:30:07 -0500

    Minimum Add To Make Parenthesis Valid.

--- minimumAddToMakeParenthesesValid.js
+++ minimumAddToMakeParenthesesValid.js
@@ -1,6 +1,6 @@
 /*
-A parentheses string is valid if and only if:
 
+A parentheses string is valid if and only if:
 It is the empty string,
 It can be written as AB (A concatenated with B), where A and B are valid strings, or
 It can be written as (A), where A is a valid string.
