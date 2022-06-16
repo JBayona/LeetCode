09e0d749653999cc93511077e659634557d68e11
JBayona <jordavids_22@hotmail.com>
2022-06-15 21:54:53 -0500

    Maximum Number Of Events That Can Be Attended.

--- maximumNumberOfEventsThatCanBeAttended.js
+++ maximumNumberOfEventsThatCanBeAttended.js
@@ -16,39 +16,22 @@ https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/
 // For each event, if we can attend a event, we increment the result count.
 // Time Complexity: O(NK) = O(N^2) = O(10^5 * 10^5)
 var maxEvents = function(events) {
-    // Sort based on end time, from greater to less value
-    events.sort((a, b) => a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]);
-    let attended = new Array(100001).fill(false); // 10^5
-    
-    let answer = 0;
-    for(let event of events) {
-        let [start, end] = event;
-        for(let i = start; i <= end; i++) {
-            if(attended[i] === false) {
-                attended[i] = true;
-                answer++;
-                break;
-            }
-        }
-    }
-    return answer;
-};
-
-// Option 2 - better performance
-// Better time complexity.
-var maxEvents = function(events) {
-    // Sort based on end time, from greater to less value
+    // Sort based on end time, from less to greater value
     events.sort((a, b) => a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]);
     let attended = new Array(100001).fill(false); // 10^5
     
     let answer = 0;
     let lastFound = 0;
+    // Iterate through the events
     for(let i = 0; i < events.length; i++) {
         let [start, end] = events[i];
+        // Check if the current event starts at the same time of the previous event
         let init = (i > 0 && events[i-1][0] == start) ? lastFound : start;
         for(let j = init; j <= end; j++) {
+            // Try to acomodate to the soonest available spot
             if(attended[j] === false) {
                 attended[j] = true;
+                // Track where we were able to accommodate the last event.
                 lastFound = j;
                 answer++;
                 break;
