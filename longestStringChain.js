c61cbaaf1393383fa86d8e4dacf74cb07bb0533e
JBayona <jordavids_22@hotmail.com>
2022-06-17 21:47:07 -0500

    Longest String Chain.

--- longestStringChain.js
+++ longestStringChain.js
@@ -8,7 +8,6 @@ For example, "abc" is a predecessor of "abac", while "cba" is not a predecessor 
 A word chain is a sequence of words [word1, word2, ..., wordk] with k >= 1, where word1 is
 a predecessor of word2, word2 is a predecessor of word3, and so on.
 A single word is trivially a word chain with k == 1.
-
 Return the length of the longest possible word chain with words chosen from the given list of words.
 
 Example 1:
