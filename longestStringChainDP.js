238e0c35574d5226c96dc56be3d97eb077a10730
JBayona <jordavids_22@hotmail.com>
2022-06-19 19:42:03 -0500

    Lonngest Strnig Chain DP.

--- longestStringChainDP.js
+++ longestStringChainDP.js
@@ -1,6 +1,5 @@
 /*
 You are given an array of words where each word consists of lowercase English letters.
-
 wordA is a predecessor of wordB if and only if we can insert exactly one letter anywhere
 in wordA without changing the order of the other characters to make it equal to wordB.
 
