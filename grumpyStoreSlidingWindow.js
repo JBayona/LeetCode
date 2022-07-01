b785a214eba94deef51987b67faf7489b9a16d54
JBayona <jordavids_22@hotmail.com>
2022-06-30 22:00:53 -0500

    Grumpy Store Sliding Wiindow.

--- grumpyStoreSlidingWindow.js
+++ grumpyStoreSlidingWindow.js
@@ -1,5 +1,6 @@
 /*
-There is a bookstore owner that has a store open for n minutes. Every minute, some number of customers enter the store. You are given an integer array customers of length n where customers[i] is the number of the customer that enters the store at the start of the ith minute and all those customers leave after the end of that minute.
+There is a bookstore owner that has a store open for n minutes. Every minute, some number
+of customers enter the store. You are given an integer array customers of length n where customers[i] is the number of the customer that enters the store at the start of the ith minute and all those customers leave after the end of that minute.
 
 On some minutes, the bookstore owner is grumpy. You are given a binary array grumpy where grumpy[i] is 1
 if the bookstore owner is grumpy during the ith minute, and is 0 otherwise.
