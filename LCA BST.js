2881c991fd3f1cb4a80d46d66e51996794e26054
JBayona <jordavids_22@hotmail.com>
2022-07-05 22:10:02 -0500

    Lowest Common Ancestor BST.

--- LCA BST.js
+++ LCA BST.js
@@ -1,7 +1,8 @@
 /*
 Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
 
-According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between
+two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 
 Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
 
@@ -55,14 +56,23 @@ var lowestCommonAncestor = function(root, p, q) {
 // Time O(h) with O(1)
 // Iterative
 var lowestCommonAncestor = function(root, p, q) {
+  // We use a while loop to traverse in the BST
   while(root) {
-    if(root.val < p.val && root.val < q.val) {
-        root = root.right;
-    } else if(root.val > p.val && root.val > q.val) {
-        root = root.left;
-    } else {
-        break;
-    }
+      // If the value of current node is lesser than both
+      // the value then we go on the right side
+      if(root.val < p.val && root.val < q.val) {
+          root = root.right;
+          // If the value of current node is greater than both
+          // the value then we go on the left side
+      } else if(root.val > p.val && root.val > q.val) {
+          root = root.left;
+      } else {
+          // IIf both the condition fails then it means that one node
+          // is on the left and the other node is on the right side, so in
+          // this case LCA will be the current node itself.
+          // So we break the loop and return the current node as LCA  
+          break;
+      }
   }
   return root;
 };
