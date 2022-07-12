fca413e546ea80c862e5bc613534df711f85c967
JBayona <jordavids_22@hotmail.com>
2022-07-11 23:50:03 -0500

    Decode String Stack.

--- decodeStringStack.js
+++ decodeStringStack.js
@@ -19,32 +19,58 @@ https://leetcode.com/problems/decode-string/description/
 
 // Using stacks
 
-var decodeString = function(s) {
-    let regex = /\d/;
-    let stack = [];
-    let num = '';
-    let char;
-    let currentStr = '';
-    for(let i = 0; i < s.length; i++) {
-      char = s[i];
-      if(regex.test(char)) {
-        num += char;
-      } else if (char === '[') {
-        stack.push(parseInt(num))
-        // Push the string we could find in nested levels
-        stack.push(currentStr);
-        num = '';
-        currentStr = '';
-      } else if (char === ']') {
-        prevStr = stack.pop();
-        times = stack.pop()
-        currentStr = prevStr + currentStr.repeat(times);
-      } else {
-        currentStr += char;
-      }
+/*var decodeString = function (s) {
+  let regex = /\d/;
+  let stack = [];
+  let num = "";
+  let char;
+  let currentStr = "";
+  for (let i = 0; i < s.length; i++) {
+    char = s[i];
+    if (regex.test(char)) {
+      num += char;
+    } else if (char === "[") {
+      stack.push(parseInt(num));
+      // Push the string we could find in nested levels
+      stack.push(currentStr);
+      num = "";
+      currentStr = "";
+    } else if (char === "]") {
+      prevStr = stack.pop();
+      times = stack.pop();
+      currentStr = prevStr + currentStr.repeat(times);
+    } else {
+      currentStr += char;
     }
-    return currentStr;
+  }
+  return currentStr;
+};*/
+
+// Recursionn
+var decodeString = function (s) {
+  let index = {i: 0};
+  return helper(s, index);
 };
+
+function helper(s, index) {
+  let result = "";
+  while (index.i < s.length && s[index.i] !== "]") {
+    if (!/\d/.test(s[index.i])) {
+      result += s[index.i++];
+    } else {
+      let n = 0;
+      while (index.i < s.length &&  /\d/.test(s[index.i])) {
+        n = n * 10 + Number(s[index.i++]);
+      }
+      index.i++; // Skip [
+      let answer = helper(s, index);
+      index.i++; // Skip ]
+      
+      result += answer.repeat(n);
+    }
+  }
+  return result;
+}
 
 s = "3[a]2[bc]"; // return "aaabcbc".
 // s = "3[a2[c]]" // "accaccacc"
