d5682cf4979e2da75267f27f441e737e7ede78bd
JBayona <jordavids_22@hotmail.com>
2022-07-12 23:49:50 -0500

    Decoode String Stack.

--- decodeStringStack.js
+++ decodeStringStack.js
@@ -1,8 +1,9 @@
 /*
 Given an encoded string, return it's decoded string.
 
-The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being
-repeated exactly k times. Note that k is guaranteed to be a positive integer.
+The encoding rule is: k[encoded_string], where the encoded_string
+inside the square brackets is being repeated exactly k times.
+Note that k is guaranteed to be a positive integer.
 
 You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
 
