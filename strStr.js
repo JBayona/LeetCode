bbc3c9af8ad0e96e695102ef9b55049c2704fc69
JBayona <jordavids_22@hotmail.com>
2022-08-04 11:17:31 -0500

    Impleement strStr.

--- strStr.js
+++ strStr.js
@@ -1,15 +1,14 @@
 /*
-
 Implement strStr().
 
-Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+Return the index of the first occurrence of needle in haystack, or -1 if needle is
+not part of haystack.
 
 Example 1:
-
 Input: haystack = "hello", needle = "ll"
 Output: 2
-Example 2:
 
+Example 2:
 Input: haystack = "aaaaa", needle = "bba"
 Output: -1
 Clarification:
@@ -17,7 +16,6 @@ Clarification:
 What should we return when needle is an empty string? This is a great question to ask during an interview.
 
 For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
-
 
 https://leetcode.com/problems/implement-strstr/description/
 */
