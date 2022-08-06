f83f62de3217b8449761aca80d41892a27ddaaf8
JBayona <jordavids_22@hotmail.com>
2022-08-06 13:48:31 -0400

    Accounts Union Find.

--- accountsMergeUnionFind.js
+++ accountsMergeUnionFind.js
@@ -6,7 +6,8 @@ emails representing emails of the account.
 Now, we would like to merge these accounts. Two accounts definitely belong to the same person
 if there is some common email to both accounts. Note that even if two accounts have the same name
 they may belong to different people as people could have the same name.
-A person can have any number of accounts initially, but all of their accounts definitely have the same name.
+A person can have any number of accounts initially, but all of their accounts definitely
+have the same name.
 
 After merging the accounts, return the accounts in the following format: the first element of each account is the name
 and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
