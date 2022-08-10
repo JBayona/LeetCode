2e753210679244848e8476af0ba5bee64d76d433
JBayona <jordavids_22@hotmail.com>
2022-08-09 21:21:43 -0400

    Max Path Sum Binary Tree.

--- maxPathSumBinaryTree.js
+++ maxPathSumBinaryTree.js
@@ -2,7 +2,8 @@
 Given a non-empty binary tree, find the maximum path sum.
 
 For this problem, a path is defined as any sequence of nodes from some starting node to any node
-in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
+in the tree along the parent-child connections.
+The path must contain at least one node and does not need to go through the root.
 
 Example 1:
 Input: [1,2,3]
