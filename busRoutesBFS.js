3fd90f734cc7941a5e8995d2a72803f7fb23d3e3
JBayona <jordavids_22@hotmail.com>
2022-08-15 21:57:52 -0400

    Bus routes BFS.

--- busRoutesBFS.js
+++ busRoutesBFS.js
@@ -8,7 +8,8 @@ the sequence 1 -> 5 -> 7 -> 1 -> 5 -> 7 -> 1 -> ... forever.
 You will start at the bus stop source (You are not on any bus initially), and you want to
 go to the bus stop target. You can travel between bus stops by buses only.
 
-Return the least number of buses you must take to travel from source to target. Return -1 if it is not possible.
+Return the least number of buses you must take to travel from source to target.
+Return -1 if it is not possible.
 
 Example 1:
 Input: routes = [[1,2,7],[3,6,7]], source = 1, target = 6
