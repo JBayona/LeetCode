fdb518cdf101ffc5d0da61b4ce3e36200e018432
JBayona <jordavids_22@hotmail.com>
2022-08-24 23:50:11 -0400

    Is Valid Sudoku.

--- isValidSudoku.js
+++ isValidSudoku.js
@@ -1,6 +1,5 @@
 /*
 Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
-
 The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
 
 https://leetcode.com/problems/valid-sudoku/description/
