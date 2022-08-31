ffca51125088c9422b2229c5981e9712eb1c339d
JBayona <jordavids_22@hotmail.com>
2022-08-30 22:09:50 -0400

    Search Suggestion System Trie Improved.

--- searchSuggestionSystemTrieImproved.js
+++ searchSuggestionSystemTrieImproved.js
@@ -1,8 +1,9 @@
 /*
 You are given an array of strings products and a string searchWord.
 Design a system that suggests at most three product names from products after each character
-of searchWord is typed. Suggested products should have common prefix with searchWord. If there are more
-than three products with a common prefix return the three lexicographically minimums products.
+of searchWord is typed. Suggested products should have common prefix with searchWord.
+If there are more than three products with a common prefix return the three lexicographically
+minimums products.
 
 Return a list of lists of the suggested products after each character of searchWord is typed.
 
