56c90e4d728c4a4c6901cf76bf15853062ca23ed
JBayona <jordavids_22@hotmail.com>
2022-09-07 23:10:01 -0400

    Prune Binary Tree.

---
+++ pruneTreeBinaryTree.js
@@ -0,0 +1,37 @@
+/*
+Given the root of a binary tree, return the same tree where every subtree
+(of the given tree) not containing a 1 has been removed.
+
+A subtree of a node node is node plus every node that is a descendant of node.
+
+Example 1:
+Input: root = [1,null,0,0,1]
+Output: [1,null,0,null,1]
+Explanation: 
+Only the red nodes satisfy the property "every subtree not containing a 1".
+The diagram on the right represents the answer.
+
+Example 2:
+Input: root = [1,0,1,0,0,0,1]
+Output: [1,null,1,null,1]
+
+Example 3:
+Input: root = [1,1,0,1,1,0,1,0]
+Output: [1,1,0,1,1,null,1]
+
+https://leetcode.com/problems/binary-tree-pruning/
+*/
+
+// Option 1
+var pruneTree = function (root) {
+  if (!root) {
+    return null;
+  }
+  root.left = pruneTree(root.left);
+  root.right = pruneTree(root.right);
+  // Node leaf, last value is 0 and it has no children
+  if (root.val === 0 && !root.left && !root.right) {
+    return null;
+  }
+  return root;
+};
