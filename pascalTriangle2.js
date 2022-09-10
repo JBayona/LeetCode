847c43f9bb6762d90550de69b8cca046688767a3
JBayona <jordavids_22@hotmail.com>
2022-09-10 15:59:32 -0400

    Pascal Triangle 2.

--- pascalTriangle2.js
+++ pascalTriangle2.js
@@ -8,7 +8,21 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
 
 https://leetcode.com/problems/pascals-triangle-ii/
 */
+// Option 1
+var getRow = function(rowIndex) {
+    var row = [1];
+    for(var i = 1 ; i <= rowIndex ; i++) {
+        for(var j = i; j > 0; j--) {
+            if(j === i)
+                row[j] = 1;
+            else
+                row[j] = row[j - 1] + row[j];
+        }
+    }
+    return row;
+};
 
+// Option 2
 var getRow = function(rowIndex) {
     let res = new Array(rowIndex + 1).fill(0);
     res[0] = 1;
