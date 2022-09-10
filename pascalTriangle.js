961fb9b6968c874e5f842441f09d5a8cf07e5aa3
JBayona <jordavids_22@hotmail.com>
2022-09-10 13:46:38 -0400

    Pascal Triangle.

--- pascalTriangle.js
+++ pascalTriangle.js
@@ -15,23 +15,40 @@ Return
 https://leetcode.com/problems/pascals-triangle/description/
 */
 
-// O(n^2) time and O(1)
-var pascalTriangle = function(n) {
-    let result = [];
-    let tmp = [];
-    //Iterate through every line and add it into the array
-    for(let line = 1; line <= n; line++) {
-        /*Every line has number of integers equal to line number*/
-        let c = 1;
-        for(let j = 1; j <= line; j++) {
-            tmp.push(c);
-            c = c * (line - j) / j;
-        }
-        result.push(tmp);
-        tmp = [];
+// DP
+// Option 1
+var generate = function (numRows) {
+  let result = [];
+  for (let i = 0; i < numRows; i++) {
+    result[i] = [];
+    // Left most 1
+    result[i][0] = 1;
+    for (let j = 1; j < i; j++) {
+      result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
     }
-    return result;
+    result[i][i] = 1;
+  }
+  return result;
 };
- 
- n = 5;
- console.log(pascalTriangle(n));
+
+// Option 2
+// O(n^2) time and O(1)
+var pascalTriangle = function (n) {
+  let result = [];
+  let tmp = [];
+  //Iterate through every line and add it into the array
+  for (let line = 1; line <= n; line++) {
+    /*Every line has number of integers equal to line number*/
+    let c = 1;
+    for (let j = 1; j <= line; j++) {
+      tmp.push(c);
+      c = (c * (line - j)) / j;
+    }
+    result.push(tmp);
+    tmp = [];
+  }
+  return result;
+};
+
+n = 5;
+console.log(pascalTriangle(n));
