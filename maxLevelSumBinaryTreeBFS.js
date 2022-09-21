13b7fbad9a5bd922257c78636e29e8cb655a7776
JBayona <jordavids_22@hotmail.com>
2022-09-20 21:33:22 -0400

    Max Level Sum Binary Tree BFS.

--- maxLevelSumBinaryTreeBFS.js
+++ maxLevelSumBinaryTreeBFS.js
@@ -4,7 +4,6 @@ Given the root of a binary tree, the level of its root is 1, the level of its ch
 Return the smallest level X such that the sum of all the values of nodes at level X is maximal.
 
 Example 1:
-
 Input: root = [1,7,0,7,-8,null,null]
 Output: 2
 Explanation: 
@@ -12,8 +11,8 @@ Level 1 sum = 1.
 Level 2 sum = 7 + 0 = 7.
 Level 3 sum = 7 + -8 = -1.
 So we return the level with the maximum sum which is level 2.
-Example 2:
 
+Example 2:
 Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
 Output: 2
 
