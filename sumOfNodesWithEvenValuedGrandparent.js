92bfb7febaedcedf8a217f5e22c92ddeb711f403
JBayona <jordavids_22@hotmail.com>
2022-09-24 10:57:29 -0400

    Sum Of Nodes With Even Valued Grand parent.

--- sumOfNodesWithEvenValuedGrandparent.js
+++ sumOfNodesWithEvenValuedGrandparent.js
@@ -1,7 +1,6 @@
 /*
 Given a binary tree, return the sum of values of nodes with even-valued
 grandparent.(A grandparent of a node is the parent of its parent, if it exists.)
-
 If there are no nodes with an even-valued grandparent, return 0.
 
 https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/
