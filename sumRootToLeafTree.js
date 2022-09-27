cd02ae0c0ae6eb9b0e36a390e637ffdd12f90560
JBayona <jordavids_22@hotmail.com>
2022-09-26 22:18:17 -0500

    Sum Root Leaf Tree.

--- sumRootToLeafTree.js
+++ sumRootToLeafTree.js
@@ -3,7 +3,6 @@ You are given the root of a binary tree where each node has a value 0 or 1.  Eac
 starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
 
 For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
-
 Return the sum of these numbers. The answer is guaranteed to fit in a 32-bits integer.
 
 https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
