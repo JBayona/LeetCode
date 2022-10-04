16d1c6bc3b80fc4b555c2b875c436bcb009e26c3
JBayona <jordavids_22@hotmail.com>
2022-10-03 19:44:43 -0500

    Find K Closest Element Two Pointers.

--- findKClosestElementTwoPointers.js
+++ findKClosestElementTwoPointers.js
@@ -3,6 +3,7 @@ Given a sorted integer array arr, two integers k and x, return the k closest int
 to x in the array.
 The result should also be sorted in ascending order.
 An integer a is closer to x than an integer b if:
+
 |a - x| < |b - x|, or
 |a - x| == |b - x| and a < b
  
