9606170da75d897d2dc451d058d7f496a7a18af3
JBayona <jordavids_22@hotmail.com>
2022-10-06 21:03:24 -0500

    Tree Path Sums.

--- treePathsSum.js
+++ treePathsSum.js
@@ -12,8 +12,8 @@ For example consider the following Binary Tree.
                                       /   \
                                      7     4
 
-  There are 4 leaves, hence 4 root to leaf paths:
-  Path                    Number
+ There are 4 leaves, hence 4 root to leaf paths:
+ Path                    Number
   6->3->2                   632
   6->3->5->7               6357
   6->3->5->4               6354
