bad5001c9fd0d31963b48078819c16dd97fd0d5a
JBayona <jordavids_22@hotmail.com>
2022-10-25 22:54:46 -0500

    Check If Two Strings Are Equivalent.

--- checkIfTwoStringsAreEquivalent.js
+++ checkIfTwoStringsAreEquivalent.js
@@ -1,7 +1,6 @@
 /*
 Given two string arrays word1 and word2, return true if the two arrays represent the same string
 and false otherwise.
-
 A string is represented by an array if the array elements concatenated in order forms the string.
 
 Example 1:
