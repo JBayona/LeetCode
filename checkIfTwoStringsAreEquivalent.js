0f0eb868c6c01c52f2557432f7545d1d08db427a
JBayona <jordavids_22@hotmail.com>
2022-10-27 22:53:06 -0500

    Check If Two Strings are equiivalent.

--- checkIfTwoStringsAreEquivalent.js
+++ checkIfTwoStringsAreEquivalent.js
@@ -2,7 +2,6 @@
 Given two string arrays word1 and word2, return true if the two arrays represent the same string
 and false otherwise.
 A string is represented by an array if the array elements concatenated in order forms the string.
-
 Example 1:
 Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
 Output: true
