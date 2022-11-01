148ba5dafbc9c89019731f6a337d11d18fcc38d8
JBayona <jordavids_22@hotmail.com>
2022-10-31 21:23:28 -0600

    Shortest Path With Obstacle.

--- shortestPathWithObstaclesBFSMaze.js
+++ shortestPathWithObstaclesBFSMaze.js
@@ -1,7 +1,7 @@
 /*
 You are given an m x n integer matrix grid where each cell is either
-0 (empty) or 1 (obstacle). You can move up, down, left, or right from and
-to an empty cell in one step.
+0 (empty) or 1 (obstacle). You can move up, down, left, or right from and to an empty cell
+in one step.
 
 Return the minimum number of steps to walk from the upper left corner (0, 0) to the
 lower right corner (m - 1, n - 1) given that you can eliminate at most k obstacles.
