f5543ccad5c57e92756a19ed53fc8451e473fbf4
JBayona <jordavids_22@hotmail.com>
2022-11-02 20:34:23 -0600

    Minimum Genetic Mutation BFS.

--- minimumGeneticMutationBFS.js
+++ minimumGeneticMutationBFS.js
@@ -1,14 +1,12 @@
 /*
-A gene string can be represented by an 8-character long string, with choices from 'A', 'C', 'G', and 'T'.
-Suppose we need to investigate a mutation from a gene string start to a gene string end where one mutation is
-defined as one single character changed in the gene string.
+A gene string can be represented by an 8-character long string, with choices
+from 'A', 'C', 'G', and 'T'.
+Suppose we need to investigate a mutation from a gene string start to a gene string end where one mutation is defined as one single character changed in the gene string.
 
 For example, "AACCGGTT" --> "AACCGGTA" is one mutation.
-There is also a gene bank bank that records all the valid gene mutations. A gene must be in bank to make it
-a valid gene string.
+There is also a gene bank bank that records all the valid gene mutations. A gene must be in bank to make it a valid gene string.
 
-Given the two gene strings start and end and the gene bank bank, return the minimum number of mutations
-needed to mutate from start to end. If there is no such a mutation, return -1.
+Given the two gene strings start and end and the gene bank bank, return the minimum number of mutations needed to mutate from start to end. If there is no such a mutation, return -1.
 
 Note that the starting point is assumed to be valid, so it might not be included in the bank.
 
