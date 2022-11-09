19c4623ee8d41a2483e076b16bb8e273b80b4a29
JBayona <jordavids_22@hotmail.com>
2022-11-08 18:07:05 -0600

    Make The String Great Stack.

--- makeTheStringGreatStack.js
+++ makeTheStringGreatStack.js
@@ -1,6 +1,5 @@
 /*
 Given a string s of lower and upper case English letters.
-
 A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
 - 0 <= i <= s.length - 2
 - s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
