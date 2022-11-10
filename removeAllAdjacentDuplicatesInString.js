050b05f7c9dbbe01be4add986c66dde3c8fa9dea
JBayona <jordavids_22@hotmail.com>
2022-11-09 21:30:32 -0600

    Remove All Adjacent Duplicates In String.

---
+++ removeAllAdjacentDuplicatesInString.js
@@ -0,0 +1,31 @@
+/*
+You are given a string s consisting of lowercase English letters. A duplicate removal
+consists of choosing two adjacent and equal letters and removing them.
+
+We repeatedly make duplicate removals on s until we no longer can.
+Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+Example 1:
+Input: s = "abbaca"
+Output: "ca"
+Explanation: 
+For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+
+Example 2:
+Input: s = "azxxzy"
+Output: "ay"
+
+https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/description/https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/description/
+*/
+var removeDuplicates = function(s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+      let c = s[i];
+      if (stack.length && stack.at(-1) === c) {
+          stack.pop();            
+      } else {
+          stack.push(c);
+      }
+  }
+  return stack.join('');
+};
