b131d95b397d43fc0a357760ba1b962571d11eda
JBayona <jordavids_22@hotmail.com>
2022-11-10 21:06:28 -0600

    Remove All Adjacent Duplicates.

--- removeAllAdjacentDuplicatesInString.js
+++ removeAllAdjacentDuplicatesInString.js
@@ -3,7 +3,8 @@ You are given a string s consisting of lowercase English letters. A duplicate re
 consists of choosing two adjacent and equal letters and removing them.
 
 We repeatedly make duplicate removals on s until we no longer can.
-Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+Return the final string after all such duplicate removals have been made.
+It can be proven that the answer is unique.
 
 Example 1:
 Input: s = "abbaca"
