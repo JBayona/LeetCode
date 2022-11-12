364eead901baa4bdef2215ba6a43877b58c3c4b6
JBayona <jordavids_22@hotmail.com>
2022-11-11 20:41:11 -0600

    Remove All Adjacent Duplicates In String.

--- removeAllAdjacentDuplicatesInString.js
+++ removeAllAdjacentDuplicatesInString.js
@@ -9,6 +9,7 @@ It can be proven that the answer is unique.
 Example 1:
 Input: s = "abbaca"
 Output: "ca"
+
 Explanation: 
 For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
 
