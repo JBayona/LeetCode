518fd7e2036a8415e19742210e65959740503002
JBayona <jordavids_22@hotmail.com>
2022-11-14 19:34:45 -0600

    Remove Leaf Nodes.

--- removeLeafNodes.js
+++ removeLeafNodes.js
@@ -14,23 +14,21 @@ After removing, new nodes become leaf nodes with value (target = 2) (Picture in 
 Example 2:
 Input: root = [1,3,3,3,2], target = 3
 Output: [1,3,null,null,2]
-Example 3:
 
+Example 3:
 Input: root = [1,2,null,2,null,2], target = 2
 Output: [1]
 Explanation: Leaf nodes in green with value (target = 2) are removed at each step.
-Example 4:
 
+Example 4:
 Input: root = [1,1,1], target = 1
 Output: []
-Example 5:
 
+Example 5:
 Input: root = [1,2,3], target = 1
 Output: [1,2,3]
  
-
 Constraints:
-
 1 <= target <= 1000
 Each tree has at most 3000 nodes.
 Each node's value is between [1, 1000].
