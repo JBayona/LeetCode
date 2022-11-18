b076f33738742fc7a144e5b2820d0e7f16580748
JBayona <jordavids_22@hotmail.com>
2022-11-17 21:47:48 -0600

    Ugly Number Prime Factors.

---
+++ uglyNumberPrimeFactors.js
@@ -0,0 +1,26 @@
+/*
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+Given an integer n, return true if n is an ugly number.
+
+https://leetcode.com/problems/ugly-number/description/
+*/
+
+var isUgly = function (n) {
+  if (n <= 0) {
+    return false;
+  }
+
+  // These aree the factors available to use
+  let factors = new Set([2, 3, 5]);
+  // Use them until wee can either have 1 or not
+  // That way we´ll know if those are the only
+  // factors available
+  for (let factor of factors) {
+    while (n % factor === 0) {
+      n /= factor;
+    }
+  }
+
+  return n === 1;
+};
