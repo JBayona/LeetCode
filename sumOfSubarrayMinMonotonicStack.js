8a29cb549f1b42eb6a52d3202fffab43360bf9a7
JBayona <jordavids_22@hotmail.com>
2022-11-26 19:53:24 -0600

    Min of Sum Of Subarray Monotonic Stack.

--- sumOfSubarrayMinMonotonicStack.js
+++ sumOfSubarrayMinMonotonicStack.js
@@ -1,5 +1,6 @@
 /*
-Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr.
+Given an array of integers arr, find the sum of min(b), where b ranges over
+every (contiguous) subarray of arr.
 Since the answer may be large, return the answer modulo 109 + 7.
 
 Example 1:
