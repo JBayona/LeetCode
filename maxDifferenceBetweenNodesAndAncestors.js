a49402817bca85c73e8b5bf7e78ddd8adcf6be4c
JBayona <jordavids_22@hotmail.com>
2022-12-09 00:25:10 -0600

    Max Difference Between Nodes And Ancestors.

--- maxDifferenceBetweenNodesAndAncestors.js
+++ maxDifferenceBetweenNodesAndAncestors.js
@@ -1,7 +1,6 @@
 /*
 Given the root of a binary tree, find the maximum value V for which there exists
 different nodes A and B where V = |A.val - B.val| and A is an ancestor of B.
-
 (A node A is an ancestor of B if either: any child of A is equal to B
 or any child of A is an ancestor of B.)
 
