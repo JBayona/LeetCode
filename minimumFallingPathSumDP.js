765590a8411bb6c23a5a6d108856b4017d097439
JBayona <jordavids_22@hotmail.com>
2022-12-14 18:16:24 -0600

    Miinimum Falling Path DP.

--- minimumFallingPathSumDP.js
+++ minimumFallingPathSumDP.js
@@ -1,9 +1,10 @@
 /*
 Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
 
-A falling path starts at any element in the first row and chooses the element in the next row that is
-either directly below or diagonally left/right. Specifically, the next element from
-position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+A falling path starts at any element in the first row and chooses the element in the
+next row that is either directly below or diagonally left/right. Specifically, the next
+element from position (row, col) will be (row + 1, col - 1), (row + 1, col)
+or (row + 1, col + 1).
 
 https://leetcode.com/problems/minimum-falling-path-sum/description/
 */
