24b78bfaaae9b24b084db09328ad71bfead7b176
JBayona <jordavids_22@hotmail.com>
2022-12-16 22:39:48 -0600

    Largest Number.

--- largestNumber.js
+++ largestNumber.js
@@ -8,16 +8,16 @@ instead of an integer.
 Example 1:
 Input: nums = [10,2]
 Output: "210"
-Example 2:
 
+Example 2:
 Input: nums = [3,30,34,5,9]
 Output: "9534330"
-Example 3:
 
+Example 3:
 Input: nums = [1]
 Output: "1"
-Example 4:
 
+Example 4:
 Input: nums = [10]
 Output: "10"
 
