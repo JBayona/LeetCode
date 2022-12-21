3cb2c7dde24e4c7abb0c3b55b8994eb38d8555b6
JBayona <jordavids_22@hotmail.com>
2022-12-20 21:17:16 -0600

    Find Path If Exists In Graph.

--- findPathIfExistsInGraph.js
+++ findPathIfExistsInGraph.js
@@ -1,12 +1,13 @@
 /*
-There is a bi-directional graph with n vertices, where each vertex is labeled from 0 to n - 1 (inclusive).
-The edges in the graph are represented as a 2D integer array edges, where each edges[i] = [ui, vi] denotes
-a bi-directional edge between vertex ui and vertex vi. Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
+There is a bi-directional graph with n vertices, where each vertex is labeled
+from 0 to n - 1 (inclusive).
+The edges in the graph are represented as a 2D integer array edges, where each
+edges[i] = [ui, vi] denotes a bi-directional edge between vertex ui and vertex vi.
+Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
 
-You want to determine if there is a valid path that exists from vertex source to vertex destination.
-
-Given edges and the integers n, source, and destination, return true if there is a valid path from source
-to destination, or false otherwise.
+You want to determine if there is a valid path that exists from vertex
+source to vertex destination.Given edges and the integers n, source, and destination
+return true if there is a valid path from source to destination, or false otherwise.
 
 https://leetcode.com/problems/find-if-path-exists-in-graph/description/
 */
