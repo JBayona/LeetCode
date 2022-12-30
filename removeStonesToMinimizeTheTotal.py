982d426991f32de0b67e1ee801189b0cb41824bd
JBayona <jordavids_22@hotmail.com>
2022-12-29 21:12:38 -0600

    Remove Stones To Minimiize The Total.

--- removeStonesToMinimizeTheTotal.py
+++ removeStonesToMinimizeTheTotal.py
@@ -1,117 +1,46 @@
-// Calculate distance from root to node
-/*
-           1
-        2       3
-      4   5   6   7
-               8
-*/
-function Node(val, left, right) {
-  this.val = val || 0;
-  this.left = left || null;
-  this.right = right || null;
-}
-/*
-Dist(n1, n2) = Dist(root, n1) + Dist(root, n2) - (2 * Dist(root, lca))
-'n1' and 'n2' are the two given keys
-'root' is root of given Binary Tree.
-'lca' is lowest common ancestor of n1 and n2
-Dist(n1, n2) is the distance between n1 and n2.
-*/
+"""
+    You are given a 0-indexed integer array piles, where piles[i] represents the number
+    of stones in the ith pile, and an integer k. You should apply the following operation exactly k times:
 
-// Global variable
-let d1 = -1;
-let d2 = -1;
-let dist = 0;
+  Choose any piles[i] and remove floor(piles[i] / 2) stones from it.
+  Notice that you can apply the operation on the same pile more than once.
 
-function findDistanceFromRoot(node, n, level) {
-  if (!node) {
-    return 0;
-  }
+  Return the minimum possible total number of stones remaining after applying the k operations.
 
-  if (node.val === n) {
-    return level;
-  }
+  floor(x) is the greatest integer that is smaller than or equal to x (i.e., rounds x down).
 
-  let left = findDistanceFromRoot(node.left, n, level + 1);
-  return left !== 0 ? left : findDistanceFromRoot(node.right, n, level + 1);
-}
+  Example 1:
+  Input: piles = [5,4,9], k = 2
+  Output: 12
+  Explanation: Steps of a possible scenario are:
+  - Apply the operation on pile 2. The resulting piles are [5,4,5].
+  - Apply the operation on pile 0. The resulting piles are [3,4,5].
+  The total number of stones in [3,4,5] is 12.
 
-function findDistUtil(root, n1, n2, lvl) {
-  // Base case
-  if (!root) {
-    return null;
-  }
+  Example 2:
+  Input: piles = [4,3,6,7], k = 3
+  Output: 12
+  Explanation: Steps of a possible scenario are:
+  - Apply the operation on pile 2. The resulting piles are [4,3,3,7].
+  - Apply the operation on pile 3. The resulting piles are [4,3,3,4].
+  - Apply the operation on pile 0. The resulting piles are [2,3,3,4].
+  The total number of stones in [2,3,3,4] is 12.
 
-  // If either n1 or n2 matches with root's
-  // key, report the presence by returning
-  // root (Note that if a key is ancestor of
-  // other, then the ancestor key becomes LCA
-  if (root.val == n1) {
-    d1 = lvl;
-    return root;
-  }
-  if (root.val == n2) {
-    d2 = lvl;
-    return root;
-  }
+  https://leetcode.com/problems/remove-stones-to-minimize-the-total/description/
+    """
 
-  // Look for n1 and n2 in left and right subtrees
-  let leftLCA = findDistUtil(root.left, n1, n2, lvl + 1);
-  let rightLCA = findDistUtil(root.right, n1, n2, lvl + 1);
+import heapq
 
-  // If both of the above calls return Non-null,
-  // then one key is present in once subtree and
-  // other is present in other, So this node is the LCA
-  if (leftLCA && rightLCA) {
-    // Lvl is the level of the LCA
-    dist = d1 + d2 - (2 * lvl);
-    return root;
-  }
 
-  // Otherwise check if left subtree
-  // or right subtree is LCA
-  return leftLCA ? leftLCA : rightLCA;
-}
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        # Max heap
+        maxHeap = [-x for x in piles]
+        heapq.heapify(maxHeap)
 
-function findDistance(root, n1, n2) {
-  d1 = -1;
-  d2 = -1;
-  dist = 0;
-  lca = findDistUtil(root, n1, n2, 1);
+        while k > 0:
+            maxVal = heapq.heappop(maxHeap)
+            heapq.heappush(maxHeap, maxVal // 2)
+            k -= 1
 
-  // If both n1 and n2 were present
-  // in Binary Tree, return dist
-  if (d1 != -1 && d2 != -1) {
-    return dist;
-  }
-
-  // If n1 is ancestor of n2, consider
-  // n1 as root and find level
-  // of n2 in subtree rooted with n1
-  if (d1 != -1) {
-    dist = findDistanceFromRoot(lca, n2, 0);
-    return dist;
-  }
-  s;
-
-  // If n2 is ancestor of n1, consider
-  // n2 as root and find level
-  // of n1 in subtree rooted with n2
-  if (d2 != -1) {
-    dist = findDistanceFromRoot(lca, n1, 0);
-    return dist;
-  }
-  return -1;
-}
-
-let root = new Node(1);
-root.left = new Node(2);
-root.right = new Node(3);
-root.left.left = new Node(4);
-root.left.right = new Node(5);
-root.right.left = new Node(6);
-root.right.right = new Node(7);
-root.right.left.right = new Node(8);
-let p = 4;
-let q = 8;
-console.log(findDistance(root, p, q));
+        return sum(-i for i in maxHeap)
