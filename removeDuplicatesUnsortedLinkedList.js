50590f385bacb15428a7bc3310387407901601b3
JBayona <jordavids_22@hotmail.com>
2023-01-02 12:45:15 -0600

    Remove Duplicates From Unsorted Linked List.

--- removeDuplicatesUnsortedLinkedList.js
+++ removeDuplicatesUnsortedLinkedList.js
@@ -29,3 +29,34 @@ let removeDuplicates = function (head) {
   }
   return head;
 };
+
+// Option 2
+// Time O(N^2)
+/*
+ * Function to remove duplicates from an unsorted linked list
+ */
+function remove_duplicates() {
+  let ptr1 = null;
+  let ptr2 = null;
+  let dup = null;
+  let ptr1 = head;
+
+  /* Pick elements one by one */
+  while (ptr1 != null && ptr1.next != null) {
+    ptr2 = ptr1;
+    /*
+     * Compare the picked element with rest of the elements
+     */
+    while (ptr2.next != null) {
+      /* If duplicate then delete it */
+      if (ptr1.data == ptr2.next.data) {
+        /* sequence of steps is important here */
+        dup = ptr2.next;
+        ptr2.next = ptr2.next.next;
+      } /* This is tricky */ else {
+        ptr2 = ptr2.next;
+      }
+    }
+    ptr1 = ptr1.next;
+  }
+}
