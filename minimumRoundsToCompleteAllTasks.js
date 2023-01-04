dc5cf317574ed5408607d41c831fff4bc476d0d6
JBayona <jordavids_22@hotmail.com>
2023-01-03 20:56:45 -0600

    Minimum Rounds To Complete All Tasks.

---
+++ minimumRoundsToCompleteAllTasks.js
@@ -0,0 +1,51 @@
+/*
+You are given a 0-indexed integer array tasks, where tasks[i] represents the
+difficulty level of a task. In each round, you can complete either 2 or 3 tasks
+of the same difficulty level.
+
+Return the minimum rounds required to complete all the tasks, or -1 if it is not
+possible to complete all the tasks.
+
+Example 1:
+Input: tasks = [2,2,3,3,2,4,4,4,4,4]
+Output: 4
+Explanation: To complete all the tasks, a possible plan is:
+- In the first round, you complete 3 tasks of difficulty level 2. 
+- In the second round, you complete 2 tasks of difficulty level 3. 
+- In the third round, you complete 3 tasks of difficulty level 4. 
+- In the fourth round, you complete 2 tasks of difficulty level 4.  
+It can be shown that all the tasks cannot be completed in fewer than 4 rounds, so the answer is 4.
+
+Example 2:
+Input: tasks = [2,3,3]
+Output: -1
+Explanation: There is only 1 task of difficulty level 2, but in each round, you can only
+complete either 2 or 3 tasks of the same difficulty level. Hence, you cannot complete all the tasks, and the answer is -1.
+
+https://leetcode.com/problems/minimum-rounds-to-complete-all-tasks/description/
+*/
+
+// Time O(N)
+// Space O(N)
+var minimumRounds = function (tasks) {
+  let hash = {};
+  for (let n of tasks) {
+    if (!(n in hash)) {
+      hash[n] = 0;
+    }
+    hash[n]++;
+  }
+
+  let result = 0;
+  for (let n in hash) {
+    // If frecuency is less than 2,
+    if (hash[n] < 2) {
+      return -1;
+    } else if (hash[n] === 2) {
+      result += 1;
+    } else {
+      result += Math.ceil(hash[n] / 3);
+    }
+  }
+  return result;
+};
