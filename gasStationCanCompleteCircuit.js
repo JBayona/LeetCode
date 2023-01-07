de3c187e54babf6b4e2960d68269862e57e1a297
JBayona <jordavids_22@hotmail.com>
2023-01-06 19:06:32 -0600

    Gas Station Can Complete Circuit.

--- gasStationCanCompleteCircuit.js
+++ gasStationCanCompleteCircuit.js
@@ -8,16 +8,14 @@ Return the starting gas station's index if you can travel around the circuit onc
 clockwise direction, otherwise return -1.
 
 Note:
-
 If there exists a solution, it is guaranteed to be unique.
 Both input arrays are non-empty and have the same length.
 Each element in the input arrays is a non-negative integer.
-Example 1:
 
+Example 1:
 Input: 
 gas  = [1,2,3,4,5]
 cost = [3,4,5,1,2]
-
 Output: 3
 
 Explanation:
@@ -28,14 +26,12 @@ Travel to station 1. Your tank = 7 - 3 + 2 = 6
 Travel to station 2. Your tank = 6 - 4 + 3 = 5
 Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
 Therefore, return 3 as the starting index.
-Example 2:
 
+Example 2:
 Input: 
 gas  = [2,3,4]
 cost = [3,4,3]
-
 Output: -1
-
 Explanation:
 You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
 Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
