5b255246563db0fd828982633d586768986e9683
JBayona <jordavids_22@hotmail.com>
2023-01-18 21:12:44 -0600

    Subarray Sum Div By K Module.

--- subArraysSumDivByKModule.js
+++ subArraysSumDivByKModule.js
@@ -1,8 +1,8 @@
 /*
-Given an array A of integers, return the number of (contiguous, non-empty) subarrays that have a sum divisible by K.
+Given an array A of integers, return the number of (contiguous, non-empty) subarrays
+that have a sum divisible by K.
 
 Example 1:
-
 Input: A = [4,5,0,-2,-3,1], K = 5
 Output: 7
 Explanation: There are 7 subarrays with a sum divisible by K = 5:
