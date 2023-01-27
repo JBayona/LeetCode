5e9f9b810747117f7afe601ae141f71331ef43b0
JBayona <jordavids_22@hotmail.com>
2023-01-27 16:30:46 -0500

    Cooncatenated Words.

--- concatenatedWords.js
+++ concatenatedWords.js
@@ -16,31 +16,36 @@ Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats";
 https://leetcode.com/problems/concatenated-words/
 */
 
-var findAllConcatenatedWordsInADict = function(words) {
-    let set = new Set(words);
-    let result = [];
-    
-    for(let word of words) {
-        let isCon = dfs(word, 0, set);
-        if(isCon) {
-            result.push(word);
-        }
+var findAllConcatenatedWordsInADict = function (words) {
+  let set = new Set(words);
+  let result = [];
+
+  // For each word run the DFS
+  for (let word of words) {
+    // Start 0 as count to be considered concatenation of > 1 words
+    let isCon = dfs(word, 0, set);
+    if (isCon) {
+      result.push(word);
     }
-    return result;
+  }
+  return result;
 };
 
 function dfs(word, count, set) {
-    if(word.length === 0 && count > 1) {
+  // Exhaust the current word
+  if (word.length === 0 && count > 1) {
+    return true;
+  }
+  // Check character by character
+  for (let i = 1; i <= word.length; i++) {
+    let str = word.substring(0, i);
+    // If the word has been seen, check if there are more concatenatios
+    if (set.has(str)) {
+      let isConf = dfs(word.substring(i), count + 1, set);
+      if (isConf) {
         return true;
+      }
     }
-    for(let i = 1; i <= word.length; i++) {
-        let str = word.substring(0, i);
-        if(set.has(str)) {
-            let isConf = dfs(word.substring(i), count + 1, set);
-            if(isConf) {
-                return true;
-            }
-        }
-    }
-    return false;
+  }
+  return false;
 }
