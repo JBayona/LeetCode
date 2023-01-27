8f8915237a7ff09513e03db5fd05df1b34cefc36
JBayona <jordavids_22@hotmail.com>
2023-01-27 16:23:22 -0500

    Concatenated Words.

--- concatenatedWords.js
+++ concatenatedWords.js
@@ -1,6 +1,6 @@
 /*
-Given a list of words (without duplicates), please write a program that returns all concatenated words
-in the given list of words.
+Given a list of words (without duplicates), please write a program that returns
+all concatenated words in the given list of words.
 A concatenated word is defined as a string that is comprised entirely of at least two shorter
 words in the given array.
 
