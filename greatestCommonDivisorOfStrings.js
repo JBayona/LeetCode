dc3e1c63bf7839e9b2bb5f810e1d0875e56c6e77
JBayona <jordavids_22@hotmail.com>
2023-02-04 15:33:31 -0500

    Greatest Common Divisor.

--- greatestCommonDivisorOfStrings.js
+++ greatestCommonDivisorOfStrings.js
@@ -2,7 +2,8 @@
 For two strings s and t, we say "t divides s" if and only if s = t + ... + t
 (i.e., t is concatenated with itself one or more times).
 
-Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+Given two strings str1 and str2, return the largest string x such that x
+divides both str1 and str2.
 Example 1:
 Input: str1 = "ABCABC", str2 = "ABC"
 Output: "ABC"
