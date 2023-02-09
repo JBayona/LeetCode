8318d8dff133eb94359e1c0ed8775f1344e0daf0
JBayona <jordavids_22@hotmail.com>
2023-02-08 21:39:56 -0500

    Longest Palindrome.

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -3,7 +3,6 @@ Given a string which consists of lowercase or uppercase letters, find the length
 of the longest palindromes that can be built with those letters.
 
 This is case sensitive, for example "Aa" is not considered a palindrome here.
-
 Note:
 Assume the length of given string will not exceed 1,010.
 
