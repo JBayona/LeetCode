304da10a49e1548fdc901b10800a9ceb0fba930d
JBayona <jordavids_22@hotmail.com>
2023-02-09 23:26:04 -0500

    Fruit Into Basket Sliding Window.

--- fruitIntoBasketSlidingWindow.js
+++ fruitIntoBasketSlidingWindow.js
@@ -1,5 +1,7 @@
 /*
-You are visiting a farm that has a single row of fruit trees arranged from left to right. The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+You are visiting a farm that has a single row of fruit trees arranged from left to right.
+The trees are represented by an integer array fruits where fruits[i] is the type of fruit the
+ith tree produces.
 
 You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
 
