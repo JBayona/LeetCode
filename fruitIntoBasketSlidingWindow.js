22aa1bfc015d8824f392a24f7391ffce8cbb017a
JBayona <jordavids_22@hotmail.com>
2023-02-11 17:36:18 -0500

    Fruit Into Baskt Sliding Window.

--- fruitIntoBasketSlidingWindow.js
+++ fruitIntoBasketSlidingWindow.js
@@ -9,7 +9,8 @@ rules that you must follow:
 You only have two baskets, and each basket can only hold a single type of fruit.
 There is no limit on the amount of fruit each basket can hold.
 Starting from any tree of your choice, you must pick exactly one fruit from every
-tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+tree (including the start tree) while moving to the right. The picked fruits must fit
+in one of your baskets.
 Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
 Given the integer array fruits, return the maximum number of fruits you can pick.
 
