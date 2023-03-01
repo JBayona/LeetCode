9da8dd872a3cd0333437d6d2224a3d5dbebc6c4b
JBayona <jordavids_22@hotmail.com>
2023-02-28 20:51:27 -0500

    Longest Palindrome.

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -5,7 +5,6 @@ of the longest palindromes that can be built with those letters.
 This is case sensitive, for example "Aa" is not considered a palindrome here.
 Note:
 Assume the length of given string will not exceed 1,010.
-
 Example:
 Input:
 "abccccdd"
