074fe5e061844563ba251bc5f54db7cb5ba66c3c
JBayona <jordavids_22@hotmail.com>
2023-03-08 21:59:46 -0500

    Find Kth Missing Possitiive.

--- findKthMissingPositive.js
+++ findKthMissingPositive.js
@@ -5,7 +5,8 @@ Return the kth positive integer that is missing from this array.
 Example 1:
 Input: arr = [2,3,4,7,11], k = 5
 Output: 9
-Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing
+positive integer is 9.
 
 Example 2:
 Input: arr = [1,2,3,4], k = 2
