ea13a45a401964c1a5e1b8caeb12d5786f712236
JBayona <jordavids_22@hotmail.com>
2023-03-15 23:02:33 -0400

    Construct Tree From Inorder and Postorder.

--- constructTreeFromInorderPostorder.js
+++ constructTreeFromInorderPostorder.js
@@ -1,6 +1,5 @@
 /*
 Given inorder and postorder traversal of a tree, construct the binary tree.
-
 Note:
 You may assume that duplicates do not exist in the tree.
 https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/
