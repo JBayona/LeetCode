8e3df13f5624e0d6dbc0e206c146d128470fd45a
JBayona <jordavids_22@hotmail.com>
2023-03-18 20:02:48 -0400

    Convert Sorted Array to BST.

--- convertSortedArrayToBSTTree.js
+++ convertSortedArrayToBSTTree.js
@@ -2,7 +2,6 @@
 Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
 
 For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
-
 Example:
 
 Given the sorted array: [-10,-3,0,5,9],
