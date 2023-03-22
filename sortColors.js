df093f1f814ebfb875a78c7a0d8dda2214204b74
JBayona <jordavids_22@hotmail.com>
2023-03-21 21:58:30 -0400

    Sort Colors.

--- sortColors.js
+++ sortColors.js
@@ -4,14 +4,12 @@ with the colors in the order red, white and blue.
 Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
 
 Note:
-
 You are not suppose to use the library's sort function for this problem.
 click to show follow up.
 Follow up:
 A rather straight forward solution is a two-pass algorithm using counting sort.
 First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
 Could you come up with an one-pass algorithm using only constant space?
-
 
 https://leetcode.com/problems/sort-colors/description/
 */
