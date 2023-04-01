ce54dff620f07f9394c1a5738eca962e4a39b967
JBayona <jordavids_22@hotmail.com>
2023-03-31 21:15:00 -0400

    Count Unreachable Pair of Nodes.

--- countUnreachablePairsOfNodeInUniderectedGraphsUnionFind.js
+++ countUnreachablePairsOfNodeInUniderectedGraphsUnionFind.js
@@ -1,7 +1,8 @@
 /*
 You are given an integer n. There is an undirected graph with n nodes
-numbered from 0 to n - 1. You are given a 2D integer array edges where edges[i] = [ai, bi] denotes
-that there exists an undirected edge connecting nodes ai and bi.
+numbered from 0 to n - 1. You are given a 2D integer array edges where
+edges[i] = [ai, bi] denotes that there exists an undirected edge
+connecting nodes ai and bi.
 
 Return the number of pairs of different nodes that are unreachable from each other.
 
