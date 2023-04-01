e477642416a9b24c385040d752e4284ce681ddb7
JBayona <jordavids_22@hotmail.com>
2023-04-01 14:26:56 -0400

    Minimum Height Trees.

--- minimumHeightTrees.js
+++ minimumHeightTrees.js
@@ -5,7 +5,6 @@ minimum height are called minimum height trees (MHTs). Given such a graph, write
 to find all the MHTs and return a list of their root labels.
 
 Format:
-
 The graph contains n nodes which are labeled from 0 to n - 1. You will be given the number
 n and a list of undirected edges (each edge is a pair of labels).
 
