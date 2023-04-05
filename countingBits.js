957bb40aa90f427005864a802750ccb04a0545d3
JBayona <jordavids_22@hotmail.com>
2023-04-04 23:31:41 -0400

    Counting Bits.

--- countingBits.js
+++ countingBits.js
@@ -2,11 +2,10 @@
 Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
 
 Example 1:
-
 Input: 2
 Output: [0,1,1]
-Example 2:
 
+Example 2:
 Input: 5
 Output: [0,1,1,2,1,2]
 
