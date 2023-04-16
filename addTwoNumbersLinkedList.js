14ee613b4487948a0072e9545619470f421a7bfe
JBayona <jordavids_22@hotmail.com>
2023-04-16 13:50:34 -0600

    Add Two numbers.

--- addTwoNumbersLinkedList.js
+++ addTwoNumbersLinkedList.js
@@ -4,9 +4,7 @@ stored in reverse order and each of their nodes contain a single digit. Add the 
 and return it as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-
 Example:
-
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
