68250d4c8d23311dcb93a7c7aca201f37aa2d53b
JBayona <jordavids_22@hotmail.com>
2023-05-04 17:31:11 -0600

    Reverse Pairs.

--- reversePairs.py
+++ reversePairs.py
@@ -5,10 +5,11 @@ You need to return the number of important reverse pairs in the given array.
 Example1:
 Input: [1,3,2,3,1]
 Output: 2
-Example2:
 
+Example2:
 Input: [2,4,3,5,1]
 Output: 3
+
 Note:
 The length of the given array will not exceed 50,000.
 All the numbers in the input array are in the range of 32-bit integer.
