4ad693c249bac6bd120e3652f6ad78dc8f41d2a4
JBayona <jordavids_22@hotmail.com>
2023-05-11 17:15:15 -0600

    Max Uncrossed Lines.

--- maxUncrossedLines.js
+++ maxUncrossedLines.js
@@ -2,7 +2,6 @@
 We write the integers of A and B (in the order they are given) on two separate horizontal lines.
 
 Now, we may draw connecting lines: a straight line connecting two numbers A[i] and B[j] such that:
-
 A[i] == B[j];
 The line we draw does not intersect any other connecting (non-horizontal) line.
 Note that a connecting lines cannot intersect even at the endpoints: each number can only belong to one connecting line.
