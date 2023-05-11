045c19cc1180eb417748d18421a844063a7eee72
JBayona <jordavids_22@hotmail.com>
2023-05-10 20:05:42 -0600

    Max Uncrossed Lines.

--- maxUncrossedLines.js
+++ maxUncrossedLines.js
@@ -10,17 +10,16 @@ Note that a connecting lines cannot intersect even at the endpoints: each number
 Return the maximum number of connecting lines we can draw in this way.
 
 Example 1:
-
 Input: A = [1,4,2], B = [1,2,4]
 Output: 2
 Explanation: We can draw 2 uncrossed lines as in the diagram.
 We cannot draw 3 uncrossed lines, because the line from A[1]=4 to B[2]=4 will intersect the line from A[2]=2 to B[1]=2.
-Example 2:
 
+Example 2:
 Input: A = [2,5,1,2,5], B = [10,5,2,1,5,2]
 Output: 3
-Example 3:
 
+Example 3:
 Input: A = [1,3,7,1,7,5], B = [1,9,2,5,1]
 Output: 2
 
