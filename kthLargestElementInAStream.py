7ff44ef50b4a56f9810a24a53c36e6eb10ccd62c
JBayona <jordavids_22@hotmail.com>
2023-05-23 23:27:20 -0600

    Subsequence.

--- kthLargestElementInAStream.py
+++ kthLargestElementInAStream.py
@@ -3,7 +3,9 @@ Design a class to find the kth largest element in a stream. Note that it is the 
 largest element in the sorted order, not the kth distinct element.
 
 Implement KthLargest class:
-KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the
+stream of integers nums.
+
 int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
  
 Example 1:
