e8beefd4df61787779ce6cd689498f4ee5cc1f92
JBayona <jordavids_22@hotmail.com>
2023-05-30 22:47:31 -0600

    Design HashSet.

--- designHashSet.js
+++ designHashSet.js
@@ -2,10 +2,9 @@
 Design a HashSet without using any built-in hash table libraries.
 
 Implement MyHashSet class:
-
-void add(key) Inserts the value key into the HashSet.
-bool contains(key) Returns whether the value key exists in the HashSet or not.
-void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+-void add(key) Inserts the value key into the HashSet.
+-bool contains(key) Returns whether the value key exists in the HashSet or not.
+-void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
  
 Example 1:
 Input
