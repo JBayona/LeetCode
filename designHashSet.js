a62058f7f5736f287093b5dc078c16241f3860a7
JBayona <jordavids_22@hotmail.com>
2023-06-11 21:14:31 -0600

    Desigh Hash Set.

--- designHashSet.js
+++ designHashSet.js
@@ -4,8 +4,8 @@ Implement MyHashSet class:
 -void add(key) Inserts the value key into the HashSet.
 -bool contains(key) Returns whether the value key exists in the HashSet or not.
 -void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
-Example 1:
 
+Example 1:
 Input
 ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
 [[], [1], [2], [1], [3], [2], [2], [2], [2]]
