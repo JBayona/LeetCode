6372c6cf1f09480f9604d299298fce488f7a05d4
JBayona <jordavids_22@hotmail.com>
2023-06-13 22:14:10 -0600

    Equal Row And Column Pair.

--- equalRowAndColumnPair.js
+++ equalRowAndColumnPair.js
@@ -1,7 +1,6 @@
 /*
 Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that
 row ri and column cj are equal.
-
 A row and column pair is considered equal if they contain the same elements
 in the same order (i.e., an equal array).
 
