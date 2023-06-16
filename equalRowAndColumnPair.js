6a1e4ddd588561aac4850fec8dcced4274809d04
JBayona <jordavids_22@hotmail.com>
2023-06-15 20:28:21 -0600

    Equal Row and column.

--- equalRowAndColumnPair.js
+++ equalRowAndColumnPair.js
@@ -20,6 +20,7 @@ Explanation: There are 3 equal row and column pairs:
 
 https://leetcode.com/problems/equal-row-and-column-pairs/description/
 */
+
 // Time: O(N^2)
 // Space: O(N)
 var equalPairs = function (grid) {
