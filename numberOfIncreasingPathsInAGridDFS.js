20a9b1c8d91f331169eb1147cd33d519b8092d51
JBayona <jordavids_22@hotmail.com>
2023-06-19 22:21:06 -0600

    Number Of Increasing Paths In A Grid DFS.

--- numberOfIncreasingPathsInAGridDFS.js
+++ numberOfIncreasingPathsInAGridDFS.js
@@ -1,8 +1,10 @@
 /*
-You are given an m x n integer matrix grid, where you can move from a cell to any adjacent cell in all 4 directions.
+You are given an m x n integer matrix grid, where you can move from a cell to
+any adjacent cell in all 4 directions.
 
 Return the number of strictly increasing paths in the grid such that you can
-start from any cell and end at any cell. Since the answer may be very large, return it modulo 109 + 7.
+start from any cell and end at any cell. Since the answer may be very large, return
+it modulo 109 + 7.
 
 Two paths are considered different if they do not have exactly the same sequence of visited cells.
 
