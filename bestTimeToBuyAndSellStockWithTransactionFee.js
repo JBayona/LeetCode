f704258bcffb0cfea3b46900f5bcaca63866823b
JBayona <jordavids_22@hotmail.com>
2023-06-22 17:33:03 -0600

    Best Time To Buy And Sell Stocks With Transaction Fee.

--- bestTimeToBuyAndSellStockWithTransactionFee.js
+++ bestTimeToBuyAndSellStockWithTransactionFee.js
@@ -5,7 +5,8 @@ stock on the ith day, and an integer fee representing a transaction fee.
 Find the maximum profit you can achieve. You may complete as many transactions as you like, but
 you need to pay the transaction fee for each transaction.
 
-Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell
+the stock before you buy again).
 
 Example 1:
 Input: prices = [1,3,2,8,4,9], fee = 2
