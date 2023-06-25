4af1b118e22bb079416746866961fb24e49ae161
JBayona <jordavids_22@hotmail.com>
2023-06-25 10:41:10 -0600

    Longest Arithmetic Subsequence.

--- longestArithmeticSubsequenceDP.js
+++ longestArithmeticSubsequenceDP.js
@@ -1,6 +1,5 @@
 /*
 Given an array nums of integers, return the length of the longest arithmetic subsequence in nums.
-
 Note that:
 A subsequence is an array that can be derived from another array by deleting some or no
 elements without changing the order of the remaining elements.
