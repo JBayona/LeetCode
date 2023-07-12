0c26ba1c4ae1009f43b8c247feddcd5fd9cef3df
JBayona <jordavids_22@hotmail.com>
2023-07-11 20:34:00 -0600

    All Nodes Distancee K In Binary Tree Graph.

--- allNodesDistanceKInBinaryTreeGraph.js
+++ allNodesDistanceKInBinaryTreeGraph.js
@@ -6,13 +6,10 @@ Return a list of the values of all nodes that have a distance K from the target 
 The answer can be returned in any order.
 
 Example 1:
-
 Input: root = [3,5,1,6,2,0,8,null,null,7,4], target = 5, K = 2
-
 Output: [7,4,1]
 
 Explanation: 
-
 The nodes that are a distance 2 from the target node (with value 5)
 have values 7, 4, and 1.
 
