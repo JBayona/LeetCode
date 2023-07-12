9e54c358ae88292735f54304ac59d6a095db20c6
JBayona <jordavids_22@hotmail.com>
2023-07-11 20:12:55 -0600

    Path Sum-

--- pathSum.js
+++ pathSum.js
@@ -5,6 +5,23 @@ a root-to-leaf path such that adding up all the values along the path equals tar
 https://leetcode.com/problems/path-sum/description/
 */
 
+// Option 1
+// DFS
+var hasPathSum = function(root, targetSum) {
+  if(!root) {
+      return 0;
+  }
+  
+  targetSum -= root.val;
+  // Check if the node is a leaf node
+  if(!root.left && !root.right && targetSum === 0) {
+      return true;
+  }
+  
+  return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+};
+
+// Option 2
 var hasPathSum = function(root, targetSum) {
   return helper(root, targetSum);
 };
