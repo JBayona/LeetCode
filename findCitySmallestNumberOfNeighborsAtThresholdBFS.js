da7d8247ff06bdd5306667e044450ce4d215ecb4
JBayona <jordavids_22@hotmail.com>
2023-07-13 15:50:32 -0600

    Find City Smallest Number Of Neiighbors At Threshold.

--- findCitySmallestNumberOfNeighborsAtThresholdBFS.js
+++ findCitySmallestNumberOfNeighborsAtThresholdBFS.js
@@ -4,7 +4,8 @@ edges[i] = [fromi, toi, weighti] represents a bidirectional and weighted edge be
 cities fromi and toi, and given the integer distanceThreshold.
 
 Return the city with the smallest number of cities that are reachable through some path and whose
-distance is at most distanceThreshold, If there are multiple such cities, return the city with the greatest number.
+distance is at most distanceThreshold, If there are multiple such cities, return the
+city with the greatest number.
 
 Notice that the distance of a path connecting cities i and j is equal to the sum of the
 edges' weights along that path.
