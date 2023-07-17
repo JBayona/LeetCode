2c255dd420795b0e183059620cf5dd56f6428923
JBayona <jordavids_22@hotmail.com>
2023-07-17 17:00:50 -0600

    Remove Element.

--- removeElement.js
+++ removeElement.js
@@ -1,9 +1,6 @@
 /*
-
 Given an array and a value, remove all instances of that value in place and return the new length.
-
 Do not allocate extra space for another array, you must do this in place with constant memory.
-
 The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
 Example:
