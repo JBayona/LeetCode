017e94c88fc4c482d9b68aaef53e50e6e6b1ee36
JBayona <jordavids_22@hotmail.com>
2023-07-18 19:19:16 -0600

    Remove Elements.

--- removeElement.js
+++ removeElement.js
@@ -11,7 +11,6 @@ Your function should return length = 2, with the first two elements of nums bein
 https://leetcode.com/problems/remove-element/description/
 
 */
-
 // Option 1
 // Time O(N)
 // Space O(1)
