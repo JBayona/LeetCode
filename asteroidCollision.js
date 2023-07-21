a82c0dde059fc76cd9e084ff0688a1c29bde417a
JBayona <jordavids_22@hotmail.com>
2023-07-20 20:07:04 -0600

    Asteroid Collision.

--- asteroidCollision.js
+++ asteroidCollision.js
@@ -5,7 +5,8 @@ For each asteroid, the absolute value represents its size, and the sign represen
 its direction (positive meaning right, negative meaning left).
 Each asteroid moves at the same speed.
 
-Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode.
+Find out the state of the asteroids after all collisions. If two asteroids meet, the
+smaller one will explode.
 If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
 
 https://leetcode.com/problems/asteroid-collision/description/
