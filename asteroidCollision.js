cf28656a381f889e6076153a62c93e1255919ee3
JBayona <jordavids_22@hotmail.com>
2023-07-21 12:05:21 -0600

    Asteroid Collision.

--- asteroidCollision.js
+++ asteroidCollision.js
@@ -11,7 +11,6 @@ If both are the same size, both will explode. Two asteroids moving in the same d
 
 https://leetcode.com/problems/asteroid-collision/description/
 */
-
 // Time O(N)
 // Space O(N)
 var asteroidCollision = function (asteroids) {
