1e537016e353c2de4e28cffa4390e1bdad4f94bb
JBayona <jordavids_22@hotmail.com>
2023-07-23 09:45:31 -0600

    Asteroid Collision.

--- asteroidCollision.js
+++ asteroidCollision.js
@@ -1,6 +1,5 @@
 /*
 We are given an array asteroids of integers representing asteroids in a row.
-
 For each asteroid, the absolute value represents its size, and the sign represents
 its direction (positive meaning right, negative meaning left).
 Each asteroid moves at the same speed.
