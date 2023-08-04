99169eb6081b75aba9113f915e04f5d1ba245832
JBayona <jordavids_22@hotmail.com>
2023-08-03 19:59:12 -0600

    Lexicographical Number DFS.

--- lexicographicalNumbersDFS.js
+++ lexicographicalNumbersDFS.js
@@ -1,6 +1,5 @@
 /*
 Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
-
 You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
 
 Example 1:
