651654ae300ec93ce19dd5efaffe9354ef884f91
JBayona <jordavids_22@hotmail.com>
2023-08-04 20:21:13 -0600

    Unique Binary Search Trees II.

--- uniqueBinarySearchTrees II.js
+++ uniqueBinarySearchTrees II.js
@@ -12,25 +12,39 @@ Given n = 3, your program should return all 5 unique BST's shown below.
 https://leetcode.com/problems/unique-binary-search-trees-ii/description/
 */
 
-function TreeNode(val, left, right){
-    this.val = val;
-    this.left = left || null;
-    this.right = right || null;
+function TreeNode(val, left, right) {
+  this.val = val;
+  this.left = left || null;
+  this.right = right || null;
+}
+
+var generateTrees = function (n) {
+  if (n === 0) {
+    return [];
   }
- 
- function constructTrees(start,end){
+  let memo = new Map();
+  return constructTrees(1, n, memo);
+};
+
+function constructTrees(start, end, memo) {
+  // Result has TreeNode
   let result = [];
-  if(start > end){
+  if (start > end) {
     return [null];
   }
+
+  if (memo.has(new TreeNode(start, end))) {
+    return memo.get(new TreeNoode(start, end));
+  }
+
   /*Recorremos todos los valores de 1 a N construyendo recursivamente
-  left y right subtrees*/
-  for(let i = start; i <=end; i++){
+    left y right subtrees*/
+  for (let i = start; i <= end; i++) {
     //left subtree
-    let left = constructTrees(start, i - 1);
+    let left = constructTrees(start, i - 1, memo);
     //right subtree
-    let right = constructTrees(i+1, end)
-    for(let j = 0; j < left.length; j++){
+    let right = constructTrees(i + 1, end, memo);
+    for (let j = 0; j < left.length; j++) {
       for (let k = 0; k < right.length; k++) {
         let node = new TreeNode(i);
         node.left = left[j];
@@ -39,15 +53,9 @@ function TreeNode(val, left, right){
       }
     }
   }
-  return result
- }
+  memo.set(new TreeNode(start, end), result);
+  return result;
+}
 
- function generateTrees(n){
-  if(n === 0){
-    return [];
-  }
-  return constructTrees(1,n);
- }
-
- n = 3;
- console.log(generateTrees(n));
+n = 3;
+console.log(generateTrees(n));
