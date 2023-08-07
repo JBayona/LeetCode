12d3b0e50fd1856cb573d14c9736abcc2a027827
JBayona <jordavids_22@hotmail.com>
2023-08-06 21:09:16 -0600

    Unique Binary Search Trees.

--- uniqueBinarySearchTrees.js
+++ uniqueBinarySearchTrees.js
@@ -1,7 +1,6 @@
 /*
 Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
 For example,
-
 Given n = 3, there are a total of 5 unique BST's.
 
 https://leetcode.com/problems/unique-binary-search-trees/description/
