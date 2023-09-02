f7df0931f3232466dfc16ac1b4dc879656fae941
JBayona <jordavids_22@hotmail.com>
2023-09-02 18:26:50 -0400

    Is Anagram.

--- isAnagram.js
+++ isAnagram.js
@@ -2,11 +2,10 @@
 Given two strings s and t , write a function to determine if t is an anagram of s.
 
 Example 1:
-
 Input: s = "anagram", t = "nagaram"
 Output: true
-Example 2:
 
+Example 2:
 Input: s = "rat", t = "car"
 Output: false
 Note:
