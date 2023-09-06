c46d7f3380ac37ddd43d8d6e31745e0cabcc4e33
JBayona <jordavids_22@hotmail.com>
2023-09-05 20:04:00 -0400

    Product Except Self.

--- productExceptSelf.js
+++ productExceptSelf.js
@@ -4,14 +4,12 @@ Solve it without division and in O(n).
 For example, given [1,2,3,4], return [24,12,8,6].
 
 Follow up:
-
 Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
 
 https://leetcode.com/problems/product-of-array-except-self/description/
 */
 
 //O(n)
-
 var productExceptSelf = function(nums) {
     let left = [];
     let right = [];
