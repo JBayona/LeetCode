18b72ae6d944891be60f74e7e48901985954b8ea
JBayona <jordavids_22@hotmail.com>
2023-09-13 22:39:35 -0400

    Two Sum Sorted Array.

--- twoSumSortedArray.js
+++ twoSumSortedArray.js
@@ -6,7 +6,6 @@ The function twoSum should return indices of the two numbers such that they
 add up to the target, where index1 must be less than index2.
 
 Note:
-
 Your returned answers (both index1 and index2) are not zero-based.
 You may assume that each input would have exactly one solution and you may
 not use the same element twice.
