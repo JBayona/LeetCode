7a7f7716134d3d60ccd75b46113296d60664d2d8
JBayona <jordavids_22@hotmail.com>
2023-09-14 21:56:40 -0400

    Two Sum.

--- twoSum.py
+++ twoSum.py
@@ -8,7 +8,6 @@ may not use the same element twice.
 You can return the answer in any order. 
 
 Example 1:
-
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Output: Because nums[0] + nums[1] == 9, we return [0, 1].
