d7bc0f8758502340f43bbc28bda859584d07300f
JBayona <jordavids_22@hotmail.com>
2023-09-24 14:09:46 -0400

    Length Of Longest Substring No Repeating Chars.

--- lengthOfLongestSubstringNoRepeatingChars.js
+++ lengthOfLongestSubstringNoRepeatingChars.js
@@ -27,130 +27,165 @@ https://leetcode.com/problems/longest-substring-without-repeating-characters/
 // Sliding window
 // Time O(N)
 // Space O(N) assuming all chars are different
-var lengthOfLongestSubstring = function(s) {
-    let result = 0;
-    let hash = {};
-    let start = 0;
-    let end = 0;
-    
-    // No string
-    if(!s.length) {
-        return 0;
+var lengthOfLongestSubstring = function (s) {
+  let result = 0;
+  let hash = {};
+  let start = 0;
+  let end = 0;
+
+  // No string
+  if (!s.length) {
+    return 0;
+  }
+
+  while (end < s.length) {
+    let c = s[end];
+    // We already found the character
+    if (c in hash) {
+      let tmp = s[start];
+      delete hash[tmp];
+      start++;
+    } else {
+      // No character is found so we should look for a max
+      result = Math.max(result, end - start + 1);
+      hash[c] = true;
+      end++;
     }
-    
-    while(end < s.length) {
-        let c = s[end];
-        // We already found the character
-        if(c in hash) {
-            let tmp = s[start];
-            delete hash[tmp];
-            start++;
-        } else {
-            // No character is found so we should look for a max
-            result = Math.max(result, end - start + 1);
-            hash[c] = true;
-            end++;
-        }
+  }
+  return result;
+};
+
+// Another option
+// Time O(N)
+var lengthOfLongestSubstring = function (s) {
+  if (!s.length) {
+    return 0;
+  }
+
+  let hash = {};
+  let result = 0;
+  // Sliding window
+  let left = 0,
+    right = 0;
+  while (right < s.length) {
+    // Get the first most character
+    let c = s[right];
+    // If the char is not in the hash, track it
+    if (!(c in hash)) {
+      hash[c] = 0;
     }
-    return result;
+    hash[c]++;
+    // Whenever we find repetitive characters
+    while (hash[c] > 1) {
+      let l = s[left++];
+      hash[l]--;
+      if (hash[l] === 0) {
+        delete hash[l];
+      }
+    }
+    // Try to maximize the results
+    result = Math.max(result, right - left + 1);
+    right++;
+  }
+  return result;
 };
 
 // Sliding window
 // Time O(N)
 // Space O(N) assuming all chars are different
-var lengthOfLongestSubstring = function(s) {
-    let start = 0;
-    let end = 0;
-    let max = 0;
-    let seen = {};
-    while(end < s.length) {
-        let c = s[end];
-        // If we already see the character
-        if(c in seen) {
-            // Remove it and move the left window
-            let left = s[start];
-            delete seen[left];
-            start++;
-        } else {
-            // Move right window
-            seen[c] = 1;
-            if(end - start + 1 > max) {
-                max = end - start + 1;
-            }
-            end++;
-        }
+var lengthOfLongestSubstring = function (s) {
+  let start = 0;
+  let end = 0;
+  let max = 0;
+  let seen = {};
+  while (end < s.length) {
+    let c = s[end];
+    // If we already see the character
+    if (c in seen) {
+      // Remove it and move the left window
+      let left = s[start];
+      delete seen[left];
+      start++;
+    } else {
+      // Move right window
+      seen[c] = 1;
+      if (end - start + 1 > max) {
+        max = end - start + 1;
+      }
+      end++;
     }
-    return max;
-  };
+  }
+  return max;
+};
 
 // Option 1
 // Sliding window
 // Time O(N)
 // Space O(N)
-var lengthOfLongestSubstring = function(s) {
-    let start = 0;
-    let end = 0;
-    let max = 0;
-    let seen = {};
-    while(end < s.length) {
-        let c = s[end];
-        // If we already see the character
-        if(c in seen) {
-            // Remove it and move the left window
-            while(seen[c] > 0) {
-                let left = s[start];
-                delete seen[left];
-                start++;
-            }
-        } else {
-            // Move right window
-            seen[c] = 1;
-            if(end - start + 1 > max) {
-                max = end - start + 1;
-            }
-            end++;
-        }
+var lengthOfLongestSubstring = function (s) {
+  let start = 0;
+  let end = 0;
+  let max = 0;
+  let seen = {};
+  while (end < s.length) {
+    let c = s[end];
+    // If we already see the character
+    if (c in seen) {
+      // Remove it and move the left window
+      while (seen[c] > 0) {
+        let left = s[start];
+        delete seen[left];
+        start++;
+      }
+    } else {
+      // Move right window
+      seen[c] = 1;
+      if (end - start + 1 > max) {
+        max = end - start + 1;
+      }
+      end++;
     }
-    return max;
-  };
+  }
+  return max;
+};
 
-// Option 2 
+// Option 2
 
 // Sliding window
 // Time O(N)
 // Space O(N)
-var lengthOfLongestSubstring = function(s) {
-    let start = 0;
-    let end = 0;
-    let map = {};
-    let result = 0;
-    let counter = 0;
-    
-    while(end < s.length) {
-        let current = s[end];
-        if(current in map) {
-            map[current]++;
-            // If we find a repeating character, count how many repeating
-            // characters we have seen
-            if(map[current] > 1) {
-                counter++;
-            }
-        } else {
-            map[current] = 1;
-        }
-        
-        // Move our window as we should not have repeating characters
-        while(counter > 0) {
-            let head = s[start];
-            if(map[head] > 1) {
-                counter--;
-            }
-            map[head]--;
-            start++;
-        }
-        end++;
-        
-        result = Math.max(result, end - start);
+var lengthOfLongestSubstring = function (s) {
+  let start = 0;
+  let end = 0;
+  let map = {};
+  let result = 0;
+  let counter = 0;
+
+  while (end < s.length) {
+    let current = s[end];
+    if (current in map) {
+      map[current]++;
+      // If we find a repeating character, count how many repeating
+      // characters we have seen
+      if (map[current] > 1) {
+        counter++;
+      }
+    } else {
+      map[current] = 1;
     }
-    return result;
+
+    // Move our window as we should not have repeating characters
+    while (counter > 0) {
+      let head = s[start];
+      if (map[head] > 1) {
+        counter--;
+      }
+      map[head]--;
+      start++;
+    }
+    end++;
+
+    result = Math.max(result, end - start);
+  }
+  return result;
 };
