e5d2bd6785b5a81faabd5b74321a1972e3f8acd9
JBayona <jordavids_22@hotmail.com>
2023-10-02 20:27:32 -0400

    Longest Substring Without Repeating Character Sliding Window.

--- longestSubstringWithoutRepeatingCharactersSlidingWindow.js
+++ longestSubstringWithoutRepeatingCharactersSlidingWindow.js
@@ -2,17 +2,16 @@
 Given a string, find the length of the longest substring without repeating characters.
 
 Example 1:
-
 Input: "abcabcbb"
 Output: 3 
 Explanation: The answer is "abc", with the length of 3. 
-Example 2:
 
+Example 2:
 Input: "bbbbb"
 Output: 1
 Explanation: The answer is "b", with the length of 1.
-Example 3:
 
+Example 3:
 Input: "pwwkew"
 Output: 3
 Explanation: The answer is "wke", with the length of 3. 
