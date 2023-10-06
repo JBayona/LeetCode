e7830d1278ab6028392bbdff35401a2fd7477070
JBayona <jordavids_22@hotmail.com>
2023-10-06 19:06:49 -0400

    Minimum Window Substring Sliding Window.

--- minimumWindowSubstringSlidingWiindow.js
+++ minimumWindowSubstringSlidingWiindow.js
@@ -6,7 +6,6 @@ Example:
 Input: S = "ADOBECODEBANC", T = "ABC"
 Output: "BANC"
 Note:
-
 If there is no such window in S that covers all characters in T, return the empty string "".
 If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
 
