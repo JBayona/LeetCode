7863e32e49520b77c4b116a8a9bc64867d0789fb
JBayona <jordavids_22@hotmail.com>
2023-11-06 23:30:52 -0500

    Implement Queue With Stacks.

--- implementQueueWithStacks.js
+++ implementQueueWithStacks.js
@@ -23,67 +23,127 @@ You may assume that all operations are valid (for example, no pop or peek operat
 https://leetcode.com/problems/implement-queue-using-stacks/description/
 */
 
-// Option 2
-/**
- * Initialize your data structure here.
- */
-var MyQueue = function() {
-    this.enqueue = [];
-    this.dequeue = [];
+// Option 1
+var MyQueue = function () {
+  this.enqueue = [];
+  this.dequeue = [];
 };
 
 /**
- * Push element x to the back of queue. 
+ * Push element x to the back of queue.
  * @param {number} x
  * @return {void}
  */
-MyQueue.prototype.push = function(x) {
-    this.enqueue.push(x);
+MyQueue.prototype.push = function (x) {
+  this.enqueue.push(x);
 };
 
 /**
  * Removes the element from in front of queue and returns that element.
  * @return {number}
  */
-MyQueue.prototype.pop = function() {
-    if(this.dequeue.length) {
-        return this.dequeue.pop();
-    } else {
-        while(this.enqueue.length) {
-            this.dequeue.push(this.enqueue.pop());
-        }
-        return this.dequeue.pop();
-    }
+MyQueue.prototype.pop = function () {
+  this.peek();
+  return this.dequeue.pop();
 };
 
 /**
  * Get the front element.
  * @return {number}
  */
-MyQueue.prototype.peek = function() {
-    if(this.dequeue.length) {
-        return this.dequeue[this.dequeue.length-1];
-    } else {
-        while(this.enqueue.length) {
-            this.dequeue.push(this.enqueue.pop());
-        }
-        return this.dequeue[this.dequeue.length-1];
-    }
+MyQueue.prototype.peek = function () {
+  if (this.dequeue.length) {
+    return this.dequeue[this.dequeue.length - 1];
+  }
+  // Dequeue is empty
+  while (this.enqueue.length) {
+    this.dequeue.push(this.enqueue.pop());
+  }
+  return this.dequeue[this.dequeue.length - 1];
 };
 
 /**
  * Returns whether the queue is empty.
  * @return {boolean}
  */
-MyQueue.prototype.empty = function() {
-    if(!this.enqueue.length && !this.dequeue.length) {
-        return true;
-    } else {
-        return false;
-    }
+MyQueue.prototype.empty = function () {
+  if (!this.enqueue.length && !this.dequeue.length) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-/** 
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = Object.create(MyQueue).createNew()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
+
+// Option 2
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function () {
+  this.enqueue = [];
+  this.dequeue = [];
+};
+
+/**
+ * Push element x to the back of queue.
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  this.enqueue.push(x);
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  if (this.dequeue.length) {
+    return this.dequeue.pop();
+  } else {
+    while (this.enqueue.length) {
+      this.dequeue.push(this.enqueue.pop());
+    }
+    return this.dequeue.pop();
+  }
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  if (this.dequeue.length) {
+    return this.dequeue[this.dequeue.length - 1];
+  } else {
+    while (this.enqueue.length) {
+      this.dequeue.push(this.enqueue.pop());
+    }
+    return this.dequeue[this.dequeue.length - 1];
+  }
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  if (!this.enqueue.length && !this.dequeue.length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/**
  * Your MyQueue object will be instantiated and called as such:
  * var obj = Object.create(MyQueue).createNew()
  * obj.push(x)
