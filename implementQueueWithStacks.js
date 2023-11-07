adaf6525db4f1648cc1ecd2dade2257686778ce7
JBayona <jordavids_22@hotmail.com>
2023-11-06 23:23:23 -0500

    Implement Queue With Stacks.

--- implementQueueWithStacks.js
+++ implementQueueWithStacks.js
@@ -1,12 +1,11 @@
 /*
 Implement the following operations of a queue using stacks.
-
 push(x) -- Push element x to the back of queue.
 pop() -- Removes the element from in front of queue.
 peek() -- Get the front element.
 empty() -- Return whether the queue is empty.
-Example:
 
+Example:
 MyQueue queue = new MyQueue();
 
 queue.push(1);
