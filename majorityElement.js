624e47faf46d95c33a4578aa955f7344bedcd2dd
JBayona <jordavids_22@hotmail.com>
2023-11-13 23:10:23 -0500

    Majority Element.

--- majorityElement.js
+++ majorityElement.js
@@ -16,6 +16,26 @@ https://leetcode.com/problems/majority-element/
 */
 
 // Time O(N)
+// Space O(1)
+var majorityElement = function(nums) {
+    // Get the middle of the nums
+    let middle = nums.length / 2;
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        let n = nums[i];
+        if (!(n in map)) {
+            map[n] = 0;
+        }
+        map[n]++;
+        // As soon as we reach a number greater than middle
+        // we return the element, it's guaranteed a number
+        if (map[n] > middle){
+            return n;
+        }
+    }
+};
+
+// Time O(N)
 // Space O(N)
 var majorityElement = function(nums) {
     let hash = {};
