b31d2241a8725fca304d4205ce81bb366955afe5
JBayona <jordavids_22@hotmail.com>
2023-11-21 22:19:38 -0500

    Insert Interval.

--- insertInterval.js
+++ insertInterval.js
@@ -15,26 +15,32 @@ https://leetcode.com/problems/insert-interval/
 */
 
 // Time O(N)
-var insert = function(intervals, newInterval) {
+var insert = function (intervals, newInterval) {
   let result = [];
   let i = 0;
 
   // Here we handle all of the non overlapping cases that we are sure
-  while(i < intervals.length && intervals[i][1] < newInterval[0]) {
+  // There's no overlap if the current interval end is less that the start
+  // of the new interval
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
     result.push(intervals[i++]);
   }
 
-  while(i < intervals.length && intervals[i][0] <= newInterval[1]) {
+  // Combine all of the intervals, here we have an overlap
+  // We want the new interval to be always greater, we are forcing the
+  // interval start to be greater than the overlap to continue adding
+  // more elements if any
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
     newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
     newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
     i++;
   }
 
-  //Add the result of the overlapping 
+  //Add the result of the overlapping combined
   result.push(newInterval);
 
   // Add the rest of the cases we don´t match the conditions above
-  while(i < intervals.length) {
+  while (i < intervals.length) {
     result.push(intervals[i++]);
   }
 
@@ -47,33 +53,33 @@ function Interval(start, end) {
   this.end = end;
 }
 
-var insert = function(intervals, newInterval) {
+var insert = function (intervals, newInterval) {
   let result = [];
   let i = 0;
 
   // Here we handle all of the non overlapping cases that we are sure
-  while(i < intervals.length && intervals[i].end < newInterval.start) {
+  while (i < intervals.length && intervals[i].end < newInterval.start) {
     result.push(intervals[i++]);
   }
 
   // Add those overlaping scenarios
-  while(i < intervals.length && intervals[i].start <= newInterval.end) {
+  while (i < intervals.length && intervals[i].start <= newInterval.end) {
     newInterval.start = Math.min(intervals[i].start, newInterval.start);
     newInterval.end = Math.max(intervals[i].end, newInterval.end);
     i++;
   }
 
-  //Add the result of the overlapping 
+  //Add the result of the overlapping
   result.push(newInterval);
 
   // Add the rest of the cases we don´t match the conditions above
-  while(i < intervals.length) {
+  while (i < intervals.length) {
     result.push(intervals[i++]);
   }
 
   return result;
 };
 
-newInterval = new Interval(2,5);
-intervals = [new Interval(1,3), new Interval(6,9)];
+newInterval = new Interval(2, 5);
+intervals = [new Interval(1, 3), new Interval(6, 9)];
 console.log(insert(intervals, newInterval));
