db3abcab97d48203f6ab8e50c23e4f7b47f44856
JBayona <jordavids_22@hotmail.com>
2023-11-22 22:47:21 -0500

    Insert Intervals.

--- insertInterval.js
+++ insertInterval.js
@@ -1,5 +1,6 @@
 /*
-Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+Given a set of non-overlapping intervals, insert a new interval into the
+intervals (merge if necessary).
 You may assume that the intervals were initially sorted according to their start times.
 
 Example 1:
