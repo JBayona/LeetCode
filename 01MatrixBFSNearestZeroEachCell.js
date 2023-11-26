f37f13cbcc075560b5b2c3a98727f5206d5bca13
JBayona <jordavids_22@hotmail.com>
2023-11-25 20:47:17 -0500

    01 Matrix BFS Nearest Zero Each Cell.

--- 01MatrixBFSNearestZeroEachCell.js
+++ 01MatrixBFSNearestZeroEachCell.js
@@ -30,74 +30,152 @@ The cells are adjacent in only four directions: up, down, left and right.
 https://leetcode.com/problems/01-matrix/
 */
 
-// BFS
-var updateMatrix = function(matrix) {
-    let ROW = matrix.length;
-    let COL = matrix[0].length;
-    
-    // Queue to run BFS
-    let queue = [];
-    // Result array
-    let result = new Array(ROW);
-    // Visited matrix to avoid loops
-    let visited = new Array(ROW).fill(false);
-    // Fill out initial values
-    for(let i = 0; i < visited.length; i++) {
-        visited[i] = new Array(COL).fill(false);
-        // Add max numbers so we can track the min numbers
-        result[i] = new Array(COL).fill(Number.MAX_SAFE_INTEGER);
-    }
-    
-    // Fill initial visited
-    for(let i = 0; i < ROW; i++) {
-        for(let j = 0; j < COL; j++) {
-            if(matrix[i][j] === 0 && visited[i][j] === false) {
-                queue.push({row: i, col: j});
-                // Set 0 as there's no distance to reach a rotten orange
-                visited[i][j] = true;
-                // For 0's there's no distance to find
-                result[i][j] = 0;
-            }
-        }
-    }
-    //  Directions
-    let rowK = [-1,0,0,1];
-    let colK = [0,-1,1,0];
-    
-    let distance = 1;
-    // Launch BFS
-    while(queue.length) {
-        let size = queue.length;
-        // Get elements for every level
-        for(let n = 0; n < size; n++) {
-            let node = queue.shift();
-            let x = node.row;
-            let y = node.col;
+// Option 1
+var updateMatrix = function (mat) {
+  let ROW = mat.length;
+  let COL = mat[0].length;
+  let result = new Array(ROW);
+  let visited = new Array(ROW);
+  let queue = [];
 
-            // Visit all adjacent nodes
-            for(let i = 0; i < 4; i++) {
-                let newRow = x + rowK[i];
-                let newCol = y + colK[i];
-                if(isSafe(matrix, visited, newRow, newCol) && distance < result[newRow][newCol]) {
-                    visited[newRow][newCol] = true;
-                    result[newRow][newCol] = distance;
-                    queue.push({row: newRow, col: newCol});
-                }
-            }
-        }
-        distance++;
+  for (let i = 0; i < ROW; i++) {
+    // Mark as maximum value so we can find near distances
+    result[i] = new Array(COL).fill(Infinity);
+    visited[i] = new Array(COL).fill(false);
+  }
+
+  // Fill initial values, we will run BFS on zeros to find 1's
+  for (let i = 0; i < ROW; i++) {
+    for (let j = 0; j < COL; j++) {
+      if (mat[i][j] === 0 && visited[i][j] === false) {
+        queue.push({ row: i, col: j, distance: 1 });
+        // Mark as visited
+        visited[i][j] = true;
+        // We are already in the 0, nothing to validate
+        result[i][j] = 0;
+      }
     }
-    
-    return result;
-    
+  }
+
+  // Run BFS
+  let rowK = [0, -1, 0, 1];
+  let colK = [-1, 0, 1, 0];
+
+  while (queue.length) {
+    let size = queue.length;
+    // Get elements for every level
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift();
+      let x = node.row;
+      let y = node.col;
+      let d = node.distance;
+
+      // Check for distance in all 4 directions
+      for (let j = 0; j < 4; j++) {
+        let nextRow = x + rowK[j];
+        let nextCol = y + colK[j];
+        // Mininize result
+        if (
+          isSafe(nextRow, nextCol, mat, visited) &&
+          d < result[nextRow][nextCol]
+        ) {
+          visited[nextRow][nextCol] = true;
+          result[nextRow][nextCol] = d;
+          queue.push({ row: nextRow, col: nextCol, distance: d + 1 });
+        }
+      }
+    }
+  }
+  return result;
 };
 
-function isSafe(grid,visited,row,col){
+function isSafe(row, col, mat, visited) {
+  let ROW = mat.length;
+  let COL = mat[0].length;
+  return (
+    row >= 0 &&
+    row < ROW &&
+    col >= 0 &&
+    col < COL &&
+    mat[row][col] === 1 &&
+    visited[row][col] === false
+  );
+}
+
+// Option 2
+// BFS
+var updateMatrix = function (matrix) {
+  let ROW = matrix.length;
+  let COL = matrix[0].length;
+
+  // Queue to run BFS
+  let queue = [];
+  // Result array
+  let result = new Array(ROW);
+  // Visited matrix to avoid loops
+  let visited = new Array(ROW).fill(false);
+  // Fill out initial values
+  for (let i = 0; i < visited.length; i++) {
+    visited[i] = new Array(COL).fill(false);
+    // Add max numbers so we can track the min numbers
+    result[i] = new Array(COL).fill(Number.MAX_SAFE_INTEGER);
+  }
+
+  // Fill initial visited
+  for (let i = 0; i < ROW; i++) {
+    for (let j = 0; j < COL; j++) {
+      if (matrix[i][j] === 0 && visited[i][j] === false) {
+        queue.push({ row: i, col: j });
+        // Set 0 as there's no distance to reach a rotten orange
+        visited[i][j] = true;
+        // For 0's there's no distance to find
+        result[i][j] = 0;
+      }
+    }
+  }
+  //  Directions
+  let rowK = [-1, 0, 0, 1];
+  let colK = [0, -1, 1, 0];
+
+  let distance = 1;
+  // Launch BFS
+  while (queue.length) {
+    let size = queue.length;
+    // Get elements for every level
+    for (let n = 0; n < size; n++) {
+      let node = queue.shift();
+      let x = node.row;
+      let y = node.col;
+
+      // Visit all adjacent nodes
+      for (let i = 0; i < 4; i++) {
+        let newRow = x + rowK[i];
+        let newCol = y + colK[i];
+        if (
+          isSafe(matrix, visited, newRow, newCol) &&
+          distance < result[newRow][newCol]
+        ) {
+          visited[newRow][newCol] = true;
+          result[newRow][newCol] = distance;
+          queue.push({ row: newRow, col: newCol });
+        }
+      }
+    }
+    distance++;
+  }
+
+  return result;
+};
+
+function isSafe(grid, visited, row, col) {
   let ROW = grid.length;
   let COL = grid[0].length;
   return (
-      (row >= 0 && row < ROW) &&
-      (col >= 0 && col < COL) &&
-      grid[row][col] === 1 &&
-      visited[row][col] === false);
-} 
+    row >= 0 &&
+    row < ROW &&
+    col >= 0 &&
+    col < COL &&
+    grid[row][col] === 1 &&
+    visited[row][col] === false
+  );
+}
