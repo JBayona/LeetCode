7752fc79cd0c5e70959849ee1afc6e17bf6f2a9d
JBayona <jordavids_22@hotmail.com>
2023-12-02 15:31:29 -0500

    Level Order Tree.

--- levelOrderTree.js
+++ levelOrderTree.js
@@ -19,99 +19,100 @@ return its level order traversal as:
 https://leetcode.com/problems/binary-tree-level-order-traversal/description/
 */
 
-function Tree(val, left, right){
+function Tree(val, left, right) {
   this.val = val;
   this.left = left || null;
   this.right = right || null;
 }
 
 // BFS
-var levelOrder = function(root) {
-    if(!root) {
-        return [];
-    }
-    
+var levelOrder = function (root) {
+  if (!root) {
+    return [];
+  }
+  let queue = [root];
+  let result = [];
+  while (queue.length) {
     let tmp = [];
-    let queue = [];
-    let result = [];
-    queue.push(root);
-    while(queue.length) {
-        let size = queue.length;
-        for(let i = 0; i < size; i++) {
-            let node = queue.shift();
-            tmp.push(node.val);
-            if(node.left) {
-                queue.push(node.left);
-            }
-            if(node.right) {
-                queue.push(node.right);
-            }
-        }
-        result.push(tmp);
-        tmp = [];
-    }
-    return result;
-};
-
-var levelOrderTransversal = function(root) {
-    // Base case
-    if(!root) {
-        return null;
-    }
-    
-    let queue = [];
-    let result = [];
-    
-    queue.push(root);
-    while(queue.length) {
-        let n = queue.length;
-        for(let i = 0; i < n; i++) {
-            let node = queue.shift();
-            console.log(node.val);
-            if(node.left) {
-                queue.push(node.left);
-            }
-            if(node.right) {
-                queue.push(node.right);
-            }
-        }
-        console.log('\n');
-    }
-};
-
-var levelOrder = function(tree) {
-    if(!tree) return null;
-    let currentCount = 0; //Num de elementos del siguiente nivel
-    let queue = [];
-    let levelCount = 1;
-    let result = [];
-    let tmp = []; 
-    queue.push(tree);
-    while(queue.length){
-      while(levelCount > 0){
-        //Saca el primer nodo del queue
-        let node = queue.shift();
-        tmp.push(node.val);
-        if(node.left){
-          queue.push(node.left);
-          currentCount += 1;
-        }
-        if(node.right){
-          queue.push(node.right);
-          currentCount +=1
-        }
-        levelCount -= 1;
+    let size = queue.length;
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift();
+      tmp.push(node.val);
+      if (node.left) {
+        queue.push(node.left);
       }
-      /*Si ya metimos todos los hijos, metemos ese array
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+    result.push(tmp);
+  }
+  return result;
+};
+
+var levelOrderTransversal = function (root) {
+  // Base case
+  if (!root) {
+    return null;
+  }
+
+  let queue = [];
+  let result = [];
+
+  queue.push(root);
+  while (queue.length) {
+    let n = queue.length;
+    for (let i = 0; i < n; i++) {
+      let node = queue.shift();
+      console.log(node.val);
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+    console.log("\n");
+  }
+};
+
+var levelOrder = function (tree) {
+  if (!tree) return null;
+  let currentCount = 0; //Num de elementos del siguiente nivel
+  let queue = [];
+  let levelCount = 1;
+  let result = [];
+  let tmp = [];
+  queue.push(tree);
+  while (queue.length) {
+    while (levelCount > 0) {
+      //Saca el primer nodo del queue
+      let node = queue.shift();
+      tmp.push(node.val);
+      if (node.left) {
+        queue.push(node.left);
+        currentCount += 1;
+      }
+      if (node.right) {
+        queue.push(node.right);
+        currentCount += 1;
+      }
+      levelCount -= 1;
+    }
+    /*Si ya metimos todos los hijos, metemos ese array
       en el arreglo resultante y nuestro nivel actual tiene
       el valor del nivel que analizamos*/
-      result.push(tmp);
-      tmp = []; //Limpiamos el valor de nuestro temporal
-      levelCount = currentCount;
-      currentCount = 0;
-    }
-    return result;
+    result.push(tmp);
+    tmp = []; //Limpiamos el valor de nuestro temporal
+    levelCount = currentCount;
+    currentCount = 0;
+  }
+  return result;
 };
 
-tree = new Tree(1, new Tree(2, new Tree(3), new Tree(4)), new Tree(2, new Tree(4), new Tree(3)));
+tree = new Tree(
+  1,
+  new Tree(2, new Tree(3), new Tree(4)),
+  new Tree(2, new Tree(4), new Tree(3))
+);
 console.log(levelOrder(tree));
