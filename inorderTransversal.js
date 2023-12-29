e7838d2df42dbb20e3e6957d38f4fbf465b09dd9
JBayona <jordavids_22@hotmail.com>
2023-12-28 23:05:22 -0500

    Inorder Transversal.

--- inorderTransversal.js
+++ inorderTransversal.js
@@ -8,7 +8,6 @@ Given binary tree [1,null,2,3],
     /
    3
 return [1,3,2].
-
 Note: Recursive solution is trivial, could you do it iteratively?
 
 https://leetcode.com/problems/binary-tree-inorder-traversal/description/
