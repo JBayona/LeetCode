098ec721a5c42b0e079941ad3f3179783f1e1ca6
JBayona <jordavids_22@hotmail.com>
2024-01-18 21:55:12 -0500

    Merge Intervals.

--- mergeIntervals.js
+++ mergeIntervals.js
@@ -18,54 +18,82 @@ https://leetcode.com/problems/merge-intervals/
  * @param {number[][]} intervals
  * @return {number[][]}
  */
- var merge = function(intervals) {
-  if(!intervals.length) {
-      return [];
+
+// Option 1
+var merge = function (intervals) {
+  if (!intervals.length) {
+    return [];
   }
-  
+  // Sort based on start
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  let result = [];
+  let prev = intervals[0];
+  for (let i = 1; i < intervals.length; i++) {
+    let interval = intervals[i];
+    // If the end of the prev interval is greater than the start
+    // of the current interval, it means we have an override
+    if (prev[1] >= interval[0]) {
+      prev = [prev[0], Math.max(prev[1], interval[1])];
+    } else {
+      result.push(prev);
+      prev = interval;
+    }
+  }
+  // The last missing one
+  result.push(prev);
+  return result;
+};
+
+// Option 2
+var merge = function (intervals) {
+  if (!intervals.length) {
+    return [];
+  }
+
   intervals = intervals.sort((a, b) => a[0] - b[0]);
   let prev = intervals[0];
   let result = [];
-  for(let i = 1; i < intervals.length; i++) {
-      let current = intervals[i];
-      // No overlap exist
-      if(current[0] > prev[1]) {
-          result.push(prev);
-          prev = current;
-      } else {
-          // Overlap exists
-          // Get the first range, and the max of the current end or prev end
-          let merged = [prev[0], Math.max(current[1], prev[1])];
-          prev = merged
-      }
+  for (let i = 1; i < intervals.length; i++) {
+    let current = intervals[i];
+    // No overlap exist
+    if (current[0] > prev[1]) {
+      result.push(prev);
+      prev = current;
+    } else {
+      // Overlap exists
+      // Get the first range, and the max of the current end or prev end
+      let merged = [prev[0], Math.max(current[1], prev[1])];
+      prev = merged;
+    }
   }
-  
+
   // Add the last one
   result.push(prev);
   return result;
 };
 
 // Another option
-var merge = function(intervals) {
-  if(!intervals.length) {
-      return [];
+var merge = function (intervals) {
+  if (!intervals.length) {
+    return [];
   }
-  
+
   intervals = intervals.sort((a, b) => a[0] - b[0]);
   let prev = intervals[0];
   let result = [];
-  for(let i = 1; i < intervals.length; i++) {
-      let current = intervals[i];
-      // No overlap exist
-      if(current[0] > prev[1]) {
-          result.push(prev);
-          prev = current;
-      } else {
-          // Overlap exists
-          // Get the first range, and the max of the current end or prev end
-          let merged = [prev[0], Math.max(current[1], prev[1])];
-          prev = merged
-      }
+  for (let i = 1; i < intervals.length; i++) {
+    let current = intervals[i];
+    // No overlap exist
+    if (current[0] > prev[1]) {
+      result.push(prev);
+      prev = current;
+    } else {
+      // Overlap exists
+      // Get the first range, and the max of the current end or prev end
+      let merged = [prev[0], Math.max(current[1], prev[1])];
+      prev = merged;
+    }
   }
   // Add the last one
   result.push(prev);
@@ -73,32 +101,32 @@ var merge = function(intervals) {
 };
 
 // Updated
-var merge = function(intervals) {
+var merge = function (intervals) {
   let result = [];
   let index = null;
 
-  if(!intervals || !intervals.length) {
+  if (!intervals || !intervals.length) {
     return [];
   }
-  
+
   // Sort using start time as point of sorting
   // Use a comparator
   //[0] - Start time
   //[1] - End time
-  intervals = intervals.sort((a,b) => a[0] - b[0]);
+  intervals = intervals.sort((a, b) => a[0] - b[0]);
 
   let pre = intervals[0];
-    for(let i = 0; i < intervals.length; i++){
-      let curr = intervals[i];
-      // If there´s no overlap, added
-      if(curr[0] > pre[1]) {
-          result.push(pre);
-          pre = curr;
-      } else{
-          let merged = [pre[0], Math.max(pre[1], curr[1])];
-          pre = merged;
-      }
+  for (let i = 0; i < intervals.length; i++) {
+    let curr = intervals[i];
+    // If there´s no overlap, added
+    if (curr[0] > pre[1]) {
+      result.push(pre);
+      pre = curr;
+    } else {
+      let merged = [pre[0], Math.max(pre[1], curr[1])];
+      pre = merged;
     }
+  }
   // The missing one
   result.push(pre);
 
@@ -110,29 +138,29 @@ function Interval(start, end) {
   this.end = end;
 }
 
-var merge = function(intervals) {
+var merge = function (intervals) {
   let result = [];
 
-  if(!intervals || !intervals.length) {
+  if (!intervals || !intervals.length) {
     return [];
   }
-  
+
   // Sort using start time as point of sorting
   // Use a comparator
-  intervals = intervals.sort((a,b) => a.start - b.start);
+  intervals = intervals.sort((a, b) => a.start - b.start);
 
   // Insert first element
   let pre = intervals[0];
-  for(let i = 0; i < intervals.length; i++){
-      let curr = intervals[i];
-      // There´s no overlap
-      if(curr.start > pre.end) {
-          result.push(pre);
-          pre = curr;
-      } else{
-          let merged = new Interval(pre.start, Math.max(pre.end, curr.end));
-          pre = merged;
-      }
+  for (let i = 0; i < intervals.length; i++) {
+    let curr = intervals[i];
+    // There´s no overlap
+    if (curr.start > pre.end) {
+      result.push(pre);
+      pre = curr;
+    } else {
+      let merged = new Interval(pre.start, Math.max(pre.end, curr.end));
+      pre = merged;
+    }
   }
   // The missing one
   result.push(pre);
@@ -146,24 +174,27 @@ function Interval(start, end) {
   this.end = end;
 }
 
-var merge = function(intervals) {
+var merge = function (intervals) {
   let result = [];
   let index = null;
 
-  if(!intervals || !intervals.length) {
+  if (!intervals || !intervals.length) {
     return [];
   }
-  
+
   // Sort using start time as point of sorting
   // Use a comparator
-  intervals = intervals.sort((a,b) => a.start - b.start);
+  intervals = intervals.sort((a, b) => a.start - b.start);
 
   // Index is used to control the index of our result array, regardless the current interval array
-  for(let i = 0; i < intervals.length; i++) {
-   if(!result.length) {
+  for (let i = 0; i < intervals.length; i++) {
+    if (!result.length) {
       result.push(intervals[i]);
       index = 0;
-    } else if(intervals[i].start == result[index].end || intervals[i].start < result[index].end){
+    } else if (
+      intervals[i].start == result[index].end ||
+      intervals[i].start < result[index].end
+    ) {
       // Here it means we have an overlap
       let tmp = result.pop();
       let start = Math.min(intervals[i].start, tmp.start);
@@ -179,7 +210,7 @@ var merge = function(intervals) {
 };
 
 // intervals = [new Interval(1,4), new Interval(0,2), new Interval(3,5)]; // [0,5]
-// intervals = [new Interval(1,3), new Interval(2,6), new Interval(8,10), new Interval(15,18)]; 
+// intervals = [new Interval(1,3), new Interval(2,6), new Interval(8,10), new Interval(15,18)];
 //[[1,6],[8,10],[15,18]]
-intervals = [new Interval(1,4), new Interval(4,5)]; //[[1,5]]
+intervals = [new Interval(1, 4), new Interval(4, 5)]; //[[1,5]]
 console.log(merge(intervals));
