a771ec2c7d4efa34152040f8aa79a12fb5a0adff
JBayona <jordavids_22@hotmail.com>
2024-01-20 19:25:13 -0500

    Lowest Common Ancestor.

--- lowestCommonAncestorLCA.js
+++ lowestCommonAncestorLCA.js
@@ -1,4 +1,4 @@
-/**
+/*
 Given a binary tree, find the lowest common ancestor (LCA) of two given
 nodes in the tree.
 
@@ -7,7 +7,7 @@ between two nodes p and q as the lowest node in T that has both p and q as
 descendants (where we allow a node to be a descendant of itself).”
 
 https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
- * **/
+ */
 
 var lowestCommonAncestor = function (root, p, q) {
   // Base case
