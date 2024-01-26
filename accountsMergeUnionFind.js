1bc469aea04b8c2933d4c789abfabdead5ccb6be
JBayona <jordavids_22@hotmail.com>
2024-01-26 18:41:29 -0500

    Account Merge Union Find.

--- accountsMergeUnionFind.js
+++ accountsMergeUnionFind.js
@@ -68,6 +68,9 @@ var accountsMerge = function(accounts) {
           }
       }
   }
+
+  // console.log(emailToIndex); // {'johnsmith@mail.com': 0, 'john_newyork@mail.com': 0, 'john00@mail.com': 1, 'mary@mail.com': 2, 'johnnybravo@mail.com': 3}
+  // console.log(parent); // { '0': 0, '1': 0, '2': 2, '3': 3 }
   
   // console.log(parent); // {0:0, 1:0, 2:2, 3:3..}
   // Merge accounts
