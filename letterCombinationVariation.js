42e0301d329e1909aa91c3c99799275772f86e9b
JBayona <jordavids_22@hotmail.com>
2024-02-18 13:48:48 -0500

    Letter Combination Variation.

--- letterCombinationVariation.js
+++ letterCombinationVariation.js
@@ -4,55 +4,65 @@ phone numbers that match one or more vanity codes. The output must be a sorted a
 numbers from the input array of numbers
 
 2 = abc, 3 = def, 4 = ghi, 5 = jkl, 6 = mno, 7 = pqrs, 8 = tuv, 9 = xxyz
+
+Imput:
+codes = ['TWLO', 'CODE', 'HTCH', 'S'];
+numbers = ['+17474824380', '+14157088956', '+919810155555', '+15109926333', '+1415123456']
+Output: [ '+14157088956', '+15109926333', '+17474824380' ]
+
+Explanation:
+TWLO matches +14157088956 (+1415708 - TWLO)
+CODE matches +15109926333 (+151099 - CODE - 3)
+HTCH matches +17474824380 (+1747 - HTCH - 380)
 */
 
-function vanity(codes, numbers) {
-  mapping = {
-      "A": 2, "B": 2, "C": 2, "D": 3, "E": 3, "F": 3, "G": 4,
-      "H": 4, "I": 4, "J": 5, "K": 5, "L": 5, "M": 6, "N": 6,
-      "O": 6, "P": 7, "Q": 7, "R": 7, "S": 7, "T": 8, "U": 8,
-      "V": 8, "W": 9, "X": 9, "Y": 9, "Z": 9
-  }
-  let result = set()
+function vanity(codes, vanity) {
+  let res = new Set();
+  let numberSequences = new Set();
+  let map = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
 
-  for(word of codes) {
-    for(let i = 0; i < word.length; i++) {
-      if(word[i] in mapping) {
-        word = word.replace(word[i], str(mapping[word[i]]));
+  for (let i = 0; i < codes.length; i++) {
+    let numberSequence = "";
+    let code = codes[i];
+    for (let j = 0; j < code.length; j++) {
+      let c = code[j].toLowerCase();
+      for (let prop in map) {
+        if (map[prop].indexOf(c) >= 0) {
+          // Append number
+          numberSequence += prop;
+          break;
+        }
       }
     }
-    for(let i = numbers.length -1; i >= 0; i--) {
-      if(!(result.has(word) && numbers.includes(word)) {
-        result.push(numbers[i]);
-        numbers.splice(i, 1); // Remove one element
+    numberSequences.add(numberSequence.toString());
+  }
+
+  let opt = new Set();
+  for (let number of numbers) {
+    for (let sequence of numberSequences) {
+      if (number.includes(sequence)) {
+        opt.add(number);
       }
     }
   }
-  return Array.from(set).sorted();
+  return Array.from(opt).sort();
 }
 
-def vanity(codes, numbers):
-    # Write your code here
-    print(codes)
-    print(numbers)
-    mapping = {
-        "A": 2, "B": 2, "C": 2, "D": 3, "E": 3, "F": 3, "G": 4,
-        "H": 4, "I": 4, "J": 5, "K": 5, "L": 5, "M": 6, "N": 6,
-        "O": 6, "P": 7, "Q": 7, "R": 7, "S": 7, "T": 8, "U": 8,
-        "V": 8, "W": 9, "X": 9, "Y": 9, "Z": 9
-    }
-    result = set()
-
-    for word in codes:
-        for i in range(len(word)):
-            if word[i] in mapping:
-                word = word.replace(word[i], str(mapping[word[i]]))
-        for i in range(len(numbers) - 1, - 1, -1):
-            if word not in result and word in numbers[i]:
-                result.add(numbers[i])
-                numbers.pop(i)
-    return sorted(list(result))
-
-codes = ['TWLO'];
-numbers = ['+14157088956', '+151099926333', '+17474824380', '+1415123456'];
+codes = ["TWLO", "CODE", "HTCH", "S"];
+numbers = [
+  "+17474824380",
+  "+14157088956",
+  "+919810155555",
+  "+15109926333",
+  "+1415123456",
+];
 console.log(vanity(codes, numbers));
