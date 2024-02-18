9d47167b490092cad66124b141168902cfa9cb89
JBayona <jordavids_22@hotmail.com>
2024-02-17 19:26:03 -0500

    Construct Binary Tree From Preorder And Inorder.

--- constructTreeFromInorderPreorder.js
+++ constructTreeFromInorderPreorder.js
@@ -17,41 +17,60 @@ http://www.geeksforgeeks.org/construct-tree-from-given-inorder-and-preorder-trav
 // What do we know about preorder traversal?
 // We first scan the "root" and then go to it's children (left and then right).
 // With this we know what element is the root of the current level.
-function TreeNode(val, left, right){
-    this.val = val;
-    this.left = left || null;
-    this.right = right || null;
+function TreeNode(val, left, right) {
+  this.val = val;
+  this.left = left || null;
+  this.right = right || null;
 }
 
-function constructTreeInorderPreorder(inorder,preoder,inStart, inEnd, preIndex){
-    //Base Case
-    if(inStart > inEnd){
-        return null;
-    }
-    /*Toma el primer nodo de preoder, sabems que ese sera root*/
-    let node = new TreeNode(preoder[preIndex.index]);
-    preIndex.index++;
-
-    /*Si no tiene hijos retornamos*/
-    if(inStart === inEnd) return node;
-
-    //Buscamos el index del node en el array de inorder
-    let index = inorder.indexOf(node.val);
-
-    /*Usamos el index en inorder para construir el left y right subtree*/
-    node.left = constructTreeInorderPreorder(inorder,preoder,inStart, index-1, preIndex);
-    node.right = constructTreeInorderPreorder(inorder,preoder,index+1,inEnd,preIndex);
-    return node;
+function main(inorder, preoder) {
+  let n = inorder.length;
+  let preIndex = { index: 0 };
+  return constructTreeInorderPreorder(inorder, preoder, 0, n - 1, preIndex);
 }
 
-function main(inorder,preoder){
-    let n = inorder.length;
-    let preIndex = {index: 0}
-    return constructTreeInorderPreorder(inorder,preoder,0,n-1, preIndex);
+function constructTreeInorderPreorder(
+  inorder,
+  preoder,
+  inStart,
+  inEnd,
+  preIndex
+) {
+  //Base Case
+  if (inStart > inEnd) {
+    return null;
+  }
+  /*Toma el primer nodo de preoder, sabems que ese sera root*/
+  // Root Node
+  let node = new TreeNode(preoder[preIndex.index]);
+  preIndex.index++;
+
+  /*Si no tiene hijos retornamos*/
+  if (inStart === inEnd) return node;
+
+  //Buscamos el index del node en el array de inorder
+  let index = inorder.indexOf(node.val);
+
+  /*Usamos el index en inorder para construir el left y right subtree*/
+  node.left = constructTreeInorderPreorder(
+    inorder,
+    preoder,
+    inStart,
+    index - 1,
+    preIndex
+  );
+  node.right = constructTreeInorderPreorder(
+    inorder,
+    preoder,
+    index + 1,
+    inEnd,
+    preIndex
+  );
+  return node;
 }
 
 //inorder = [2, 1, 3]
 //post = [2, 3, 1];
-inorder = ['D', 'B', 'E', 'A', 'F', 'C'];
-preoder = ['A', 'B', 'D', 'E', 'C', 'F'];
-console.log(main(inorder,preoder));
+inorder = ["D", "B", "E", "A", "F", "C"];
+preoder = ["A", "B", "D", "E", "C", "F"];
+console.log(main(inorder, preoder));
