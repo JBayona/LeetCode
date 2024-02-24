a520c02386ab8a8354470b4a8563fec852a10a42
JBayona <jordavids_22@hotmail.com>
2024-02-23 22:33:38 -0500

    Task Scheduler.

--- taskScheduler.js
+++ taskScheduler.js
@@ -19,34 +19,39 @@ https://www.youtube.com/watch?v=ySTQCRya6B0
 https://leetcode.com/articles/task-scheduler/
 
 */
-
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         // Greedy Approach
         // https://www.youtube.com/watch?v=ySTQCRya6B0
-        // https://leetcode.com/articles/task-scheduler/
+        // We donn´t care about which process we are computing
+        // we take the biggest task to reduce the posibility to
+        // reduce the cooling period
         HashMap<Character, Integer> map = new HashMap<>();
         for(char c: tasks) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
         
         // Get the max element under the maxHeap in constant time
+        // Max heap, the bigger element is the one on the root so we
+        // can access at constant timee.
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> b -a);
         // Throw all values into the max heap
         maxHeap.addAll(map.values());
         
         int cycles = 0;
-        
         while(!maxHeap.isEmpty()) {
             List<Integer> tmp = new ArrayList<>();
-            for(int i = 0; i <= n; i++) {
+            // Try to run the tasks before the idle time
+            // Prepare the tasks to run during the cycle
+            for(int i = 0; i < n + 1; i++) {
                 if(!maxHeap.isEmpty()) {
                     tmp.add(maxHeap.remove());
                 }
             }
             
-            // Decrement the process and check if we still need to process more cycles
-            // for example if A is 3, we need to process A, 3 times
+            // Decrement the process and check if we still
+            // need to process more cycles, this cover cases
+            // where A was computed and we still need to compute more
             for(int i : tmp) {
                 if(--i > 0) {
                     maxHeap.add(i);
@@ -54,6 +59,9 @@ class Solution {
             }
             
             // n is the cool down
+            // If maxHeap is empty means we were able to process the heap, if not
+            // it means we need to wait for the cooling cycle that´s whys is +1
+            // becuase is inclusive
             cycles += maxHeap.isEmpty() ? tmp.size() : n + 1;
         }
         
