baadd4cba04740d1d52c736782ab9d1cc527e2a5
JBayona <jordavids_22@hotmail.com>
2024-03-09 21:33:38 -0500

    Can Reach.

--- Can Reach (m,n).js
+++ Can Reach (m,n).js
@@ -5,25 +5,23 @@ traversing through blank cells only. You can traverse up, down, right and left.
 https://www.geeksforgeeks.org/find-whether-path-two-cells-matrix/
 */
 function canReach(grid) {
-
   let row = grid.length;
   let col = grid[0].length;
 
   let visited = new Array(grid.length);
-  for(let i = 0; i < visited.length; i++) {
+  for (let i = 0; i < visited.length; i++) {
     visited[i] = new Array(visited.length).fill(0);
   }
 
-  for(let i = 0; i < row; i++) {
-    for(let j = 0; j < col; j++) {
-      if(grid[i][j] === 1 && !visited[i][j]) {
-        if(checkPath(grid, i, j, visited)) {
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (grid[i][j] === 1 && !visited[i][j]) {
+        if (checkPath(grid, i, j, visited)) {
           return true;
         }
       }
     }
   }
-
   return false;
 }
 
@@ -31,23 +29,25 @@ function checkPath(grid, row, col, visited) {
   let rowK = [0, -1, 0, 1];
   let colK = [-1, 0, 1, 0];
 
-  if(isSafe(grid, visited, row, col)) {
-    // Mark cell as visited
-    visited[row][col] = 1;
+  if (!isSafe(grid, visited, row, col)) {
+    return false;
+  }
 
-    // Base case to break recursion
-    if(grid[row][col] === 2) {
-      return true;
-    }
+  // Mark cell as visited
+  visited[row][col] = 1;
 
-    if(
-      checkPath(grid, row + 0, col -1, visited) ||
-      checkPath(grid, row -1, col + 0, visited) ||
-      checkPath(grid, row + 0, col + 1, visited) ||
-      checkPath(grid, row + 1, col + 0, visited)
-    ) {
-      return true;
-    }
+  // Base case to break recursion
+  if (grid[row][col] === 2) {
+    return true;
+  }
+
+  if (
+    checkPath(grid, row + 0, col - 1, visited) ||
+    checkPath(grid, row - 1, col + 0, visited) ||
+    checkPath(grid, row + 0, col + 1, visited) ||
+    checkPath(grid, row + 1, col + 0, visited)
+  ) {
+    return true;
   }
   // No path has been found
   return false;
@@ -55,8 +55,10 @@ function checkPath(grid, row, col, visited) {
 
 function isSafe(grid, visited, row, col) {
   return (
-    (row >= 0 && row < grid.length) &&
-    (col >= 0 && col < grid[0].length) &&
+    row >= 0 &&
+    row < grid.length &&
+    col >= 0 &&
+    col < grid[0].length &&
     // we have not visited
     visited[row][col] === 0 &&
     // If we have a free space, we are on the destination or we reach the goal
@@ -64,14 +66,13 @@ function isSafe(grid, visited, row, col) {
   );
 }
 
-
 // 1 = source
 // 2 = destination
 // 3 = free space
 // 0 = block
-grid = [ 
-[0, 3, 2 ],
-[3, 3, 0 ],
-  [1, 3, 0 ]
+grid = [
+  [0, 3, 2],
+  [3, 3, 0],
+  [1, 3, 0],
 ];
 console.log(canReach(grid));
