b7c39b57c46613d00be34deda6471b15dcb03d14
JBayona <jordavids_22@hotmail.com>
2024-03-14 00:01:54 -0400

    Binary Subarray With Sum Prefix Sum.

--- binarySubArraysWithSumPrefixSum.js
+++ binarySubArraysWithSumPrefixSum.js
@@ -37,3 +37,21 @@ var numSubarraysWithSum = function (nums, goal) {
   }
   return res;
 };
+
+// Brute force
+var numSubarraysWithSum = function (nums, goal) {
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+      if (sum === goal) {
+        result++;
+      }
+      if (sum > goal) {
+        break;
+      }
+    }
+  }
+  return result;
+};
