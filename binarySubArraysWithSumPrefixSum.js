31920fa5d98c0cc1c6041ca5a914e7d6de027458
JBayona <jordavids_22@hotmail.com>
2024-03-15 00:04:45 -0400

    Binary subArrays With Sum Prefix Sum.

--- binarySubArraysWithSumPrefixSum.js
+++ binarySubArraysWithSumPrefixSum.js
@@ -1,6 +1,6 @@
 /*
-Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
-
+Given a binary array nums and an integer goal, return the number of non-empty
+subarrays with a sum goal.
 A subarray is a contiguous part of the array.
 
 Example 1:
