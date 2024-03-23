0e5f6a38c22585718eb9fb0a084aa941da6e5c4a
JBayona <jordavids_22@hotmail.com>
2024-03-23 17:00:30 -0400

    Reorder Linked Lists.

--- reorderLinkedList.js
+++ reorderLinkedList.js
@@ -10,54 +10,59 @@ https://leetcode.com/problems/reorder-list/
 
 // Time O(N)
 // Space O(1)
-var reorderList = function(head) {
-    let mid = middleNode(head);
-    let reversedList = reverse(mid.next);
-    // Set the middle node as null as it´s the new final
-    mid.next = null;
-    // Merge lists
-    return merge(head, reversedList);
+/*
+1 -> 2 -> 3 -> 4
+l1 = 1 -> 2
+l2 = 4 -> 3
+r = 1 -> 4 -> 2 -> 3
+*/
+var reorderList = function (head) {
+  let mid = middleNode(head);
+  let reversedList = reverse(mid.next);
+  // Set the middle node as null as it´s the new final
+  mid.next = null;
+  // Merge lists, one is the middle, and the other is middle reversed
+  return merge(head, reversedList);
 };
 
-// Two pointers
-var middleNode = function(head) {
-    if(!head) {
-        return null;
-    }
-    // If there's only one node in the list
-    if(head && !head.next) {
-        return head;
-    }
+var middleNode = function (head) {
+  if (!head) {
+    return null;
+  }
+  // If there's only one node in the list
+  if (head && !head.next) {
+    return head;
+  }
 
-    let slow = head;
-    let fast = head;
-    while(fast && fast.next) {
-        slow = slow.next;
-        fast = fast.next.next;   
-    }
-    return slow;
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
 };
 
 function reverse(node) {
-    let current = node;
-    let prev = null;
-    let next = null;
-    while(current) {
-        next = current.next;
-        current.next = prev;
-        prev = current;
-        current = next;
-    }
-    return prev;
+  let current = node;
+  let prev = null;
+  let next = null;
+  while (current) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
 }
 
 function merge(head1, head2) {
-    let head = head1;
-    while(head2) {
-        let next = head1.next;
-        head1.next = head2;
-        head1 = head2;
-        head2 = next;
-    }
-    return head;
+  let head = head1;
+  while (head2) {
+    let next = head1.next;
+    head1.next = head2;
+    head1 = head2;
+    head2 = next;
+  }
+  return head;
 }
