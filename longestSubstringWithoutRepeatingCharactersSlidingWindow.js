ae36d2e5c782eb7d3ee32065a5d7255fb29df03c
JBayona <jordavids_22@hotmail.com>
2024-04-07 17:34:53 -0400

    Longest Substring Without Repeating Character.

--- longestSubstringWithoutRepeatingCharactersSlidingWindow.js
+++ longestSubstringWithoutRepeatingCharactersSlidingWindow.js
@@ -23,7 +23,6 @@ https://leetcode.com/problems/longest-substring-without-repeating-characters/
 https://leetcode.com/problems/find-all-anagrams-in-a-string/discuss/92007/sliding-window-algorithm-template-to-solve-all-the-leetcode-substring-search-problem
 
 */
-
 // Option 1
 // Time O(N)
 var lengthOfLongestSubstring = function (s) {
