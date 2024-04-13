40cfafe193a73aeb3ea04d589372d5871503f1c9
JBayona <jordavids_22@hotmail.com>
2024-04-12 22:23:47 -0400

    Maximal Rectangle.

--- maximalRectangle.js
+++ maximalRectangle.js
@@ -15,56 +15,48 @@ https://leetcode.com/problems/maximal-rectangle/
 */
 // Time: O(n^2*m^2)
 // Space: O(1)
-var maximalRectangle = function(matrix) {
-    let maxArea = 0;
-    
-    if(!matrix || !matrix.length) {
-        return 0;
-    }
-    
-    // Recorremos toda la matriz
-    for(let i = 0; i < matrix.length; i++) {
-        for(let j = 0; j < matrix[i].length; j++) {
-            // Estas variables nos ayudarán para sacar el área
-            let rowCount = 1;
-            let colCount = 1;
-            let rowIndex = i;
-            if(matrix[i][j] === '1') {
-                // Siempre que encontremos un uno, lo intentaremos expander hacia la derecha
-                // y hacia abajo
-                while(rowIndex < matrix.length) {
-                    // Expandemos columnas
-                    let colCountTmp = 1;
-                    for(let k = j + 1; k < matrix[i].length; k++) {
-                        if(matrix[rowIndex][k] === '1') {
-                            colCountTmp++;
-                        } else {
-                            break
-                        }
-                    }
-                    // Si ya analizamos la primer columna, entonces nos quedamos con el mínimo de las
-                    // columnas que pudimos expander porque puede ser que en u renglón pudimos expandir
-                    // más columnas que en otra
-                    if(rowCount > 1) {
-                        colCount = Math.min(colCount, colCountTmp);
-                    } else {
-                        // Si es la primera, lo que pudimos expander sólo fué la primera
-                        colCount = colCountTmp;
-                    }
-                    // Calculamos el max area
-                    maxArea = Math.max(rowCount * colCount, maxArea);
-                    // Expandemos las columnas si es posible movernos hacia abajo, es decir
-                    // que sea un uno
-                    if(rowIndex + 1 < matrix.length && matrix[rowIndex + 1][j] === '1') {
-                        rowIndex++;
-                        rowCount++;
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    return maxArea;
-};
+var maximalRectangle = function (matrix) {
+  let maxArea = 0;
 
+  if (!matrix || !matrix.length) {
+    return 0;
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      let rowCount = 1;
+      let colCount = 1;
+      let rowIndex = i;
+      if (matrix[i][j] === "1") {
+        while (rowIndex < matrix.length) {
+          // Extend columns
+          let colCountTmp = 1;
+          for (let k = j + 1; k < matrix[i].length; k++) {
+            if (matrix[rowIndex][k] === "1") {
+              colCountTmp++;
+            } else {
+              break;
+            }
+          }
+          // We want a rectangle
+          if (rowCount > 1) {
+            colCount = Math.min(colCount, colCountTmp);
+          } else {
+            colCount = colCountTmp;
+          }
+          maxArea = Math.max(rowCount * colCount, maxArea);
+          // Expand rows
+          // Here is just to try to expand but the area work
+          // is above
+          if (rowIndex + 1 < matrix.length && matrix[rowIndex + 1][j] === "1") {
+            rowIndex++;
+            rowCount++;
+          } else {
+            break;
+          }
+        }
+      }
+    }
+  }
+  return maxArea;
+};
