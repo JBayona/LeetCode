1d7b6ff605282dbad0779f6b7395dbb561fddf35
JBayona <jordavids_22@hotmail.com>
2024-05-01 21:41:38 -0400

    Largest Positive Integer That Exist With Its Negative.

---
+++ largestPositiveIntegerThatExistWithItsNegative.js
@@ -0,0 +1,55 @@
+/*
+Given an integer array nums that does not contain any zeros, find the largest positive integer k such that -k also exists in the array.
+
+Return the positive integer k. If there is no such integer, return -1.
+
+Example 1:
+Input: nums = [-1,2,-3,3]
+Output: 3
+Explanation: 3 is the only valid k we can find in the array.
+
+Example 2:
+Input: nums = [-1,10,6,7,-7,1]
+Output: 7
+Explanation: Both 1 and 7 have their corresponding negative values in the array. 7 has a larger value.
+
+Example 3:
+Input: nums = [-10,8,6,7,-2,-3]
+Output: -1
+Explanation: There is no a single valid k, we return -1.
+
+https://leetcode.com/problems/largest-positive-integer-that-exists-with-its-negative/description/
+*/
+
+// Time O(N)
+var findMaxK = function (nums) {
+  let set = new Set();
+  let result = -1;
+  for (let n of nums) {
+    if (set.has(-n)) {
+      result = Math.max(result, Math.abs(n));
+    } else {
+      set.add(n);
+    }
+  }
+  return result;
+};
+
+// Two pointers
+// Time O(NLogN)
+var findMaxK = function (nums) {
+  nums.sort((a, b) => a - b);
+  let start = 0;
+  let end = nums.length - 1;
+  while (start < end) {
+    if (Math.abs(nums[start]) === nums[end] && nums[start] !== nums[end]) {
+      return Math.abs(nums[end]);
+    }
+    if (Math.abs(nums[start]) > nums[end]) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+  return -1;
+};
