02dabbcc2c4dcfb3932aee8f6b50a12fa7d7dfef
JBayona <jordavids_22@hotmail.com>
2024-05-04 13:11:11 -0400

    Restore Ip Addresses.

--- restoreIpAddresses.js
+++ restoreIpAddresses.js
@@ -7,8 +7,7 @@ Given "25525511135",
 return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
 
 https://leetcode.com/problems/restore-ip-addresses/description/
-*/
-
+/
 var restoreIpAddresses = function (s) {
   var result = [];
   if (s.length > 12) {
