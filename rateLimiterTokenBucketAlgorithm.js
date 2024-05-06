038675ab615867133e8124bb3d0ff8e7c581239c
JBayona <jordavids_22@hotmail.com>
2024-05-05 20:46:18 -0400

    Token Bucket Algorithm.

---
+++ rateLimiterTokenBucketAlgorithm.js
@@ -0,0 +1,38 @@
+class RateLitimerTocketBucket {
+  // capacity: the maximum number of tokens allowed in the bucket
+  // refillRate: number of tokens put into the bucket every second or other specified time period.
+  constructor(capacity, refillRate) {
+    this.capacity = capacity;
+    this.refillRate = refillRate;
+    this.availableTokens = capacity;
+    this.requestTime = new Date().getTime();
+  }
+  getAvailableTokens() {
+    return this.availableTokens;
+  }
+  consume() {
+    // Try to refill after every second
+    this.refill();
+    if (this.availableTokens > 0) {
+      this.availableTokens--;
+      return "PASS";
+    }
+    return "REJECT";
+  }
+  refill() {
+    let now = new Date().getTime();
+    // Refill only every second
+    let second = 1000;
+    if (now - this.requestTime >= second) {
+      if (this.availableTokens < this.capacity) {
+        let elapsedTime = now - this.requestTime;
+        let delta = this.refillRate * elapsedTime;
+        this.availableTokens = Math.min(
+          this.capacity,
+          this.availableTokens + delta
+        );
+      }
+      this.requestTime = now;
+    }
+  }
+}
