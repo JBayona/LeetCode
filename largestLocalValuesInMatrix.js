d3dec43f677ffddbe05df2fe7962ee1f8daed9af
JBayona <jordavids_22@hotmail.com>
2024-05-12 17:39:01 -0400

    Largest Local Values In Matrix.

--- largestLocalValuesInMatrix.js
+++ largestLocalValuesInMatrix.js
@@ -10,36 +10,47 @@ Return the generated matrix.
 
 https://leetcode.com/problems/largest-local-values-in-a-matrix/description/?envType=daily-question&envId=2024-05-12
 */
-
+// Option 1
 // Time O(N)
 // Space O(1)
 var largestLocal = function (grid) {
-  let n = grid.length;
-  let result = new Array(n - 2);
-  for (let i = 0; i < result.length; i++) {
-    result[i] = new Array(n - 2).fill(0);
+  let result = new Array(grid.length - 2);
+  let max = -Infinity;
+  for (let i = 0; i <= grid.length - 3; i++) {
+    let max = getMax(i, grid[0].length - 3, grid);
   }
-
-  // Iterate through the matrix excluding borders
-  for (let i = 1; i < n - 1; i++) {
-    for (let j = 1; j < n - 1; j++) {
-      // Calculate the max in the 3x3 matrix window
-      let max = getMaxValue(grid, i, j);
-      // Save the result
-      result[i - 1][j - 1] = max;
-    }
-  }
-  return result;
 };
 
-function getMaxValue(grid, row, col) {
+function traverseMatrix(row, col, grid) {
   let max = -Infinity;
-  // Iterate over the 3x3 matrix
-  for (let i = row - 1; i <= row + 1; i++) {
-    for (let j = col - 1; j <= col + 1; j++) {
-      // Update the mmax result
+  for (let i = 0; i <= row; i++) {
+    for (let j = 0; j <= col; j++) {
       max = Math.max(max, grid[i][j]);
     }
   }
   return max;
 }
+
+// Option 2
+// Time O(N)
+// Space O(N)
+var largestLocal = function (grid) {
+  let result = [];
+  let n = grid.length;
+  let max = -Infinity;
+  let tmp = [];
+  for (let i = 0; i < n - 2; i++) {
+    for (let j = 0; j < n - 2; j++) {
+      for (let x = i; x < i + 3; x++) {
+        for (let y = j; y < j + 3; y++) {
+          max = Math.max(max, grid[x][y]);
+        }
+      }
+      tmp.push(max);
+      max = -Infinity;
+    }
+    result.push(tmp);
+    tmp = [];
+  }
+  return result;
+};
