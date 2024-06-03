b54334a9bf86b53f1ad2b8f29ad4596a01487669
JBayona <jordavids_22@hotmail.com>
2024-06-02 21:55:18 -0400

    Reorder Routes Path To City Zero BFS.

--- reorderRoutesPathToCityZeroBFS.js
+++ reorderRoutesPathToCityZeroBFS.js
@@ -1,6 +1,6 @@
 /*
-There are n cities numbered from 0 to n - 1 and n - 1 roads such that there is only one way to travel between
-two different cities (this network form a tree). Last year, The ministry of transport decided to orient the roads in one direction because they are too narrow.
+There are n cities numbered from 0 to n - 1 and n - 1 roads such that there is only one way to travel between two different cities (this network form a tree). Last year, The
+ministry of transport decided to orient the roads in one direction because they are too narrow.
 
 Roads are represented by connections where connections[i] = [ai, bi] represents a road from city ai to city bi.
 
