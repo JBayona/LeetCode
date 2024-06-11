e0649a81892d9b09e4f7f345570bd80fc432726b
JBayona <jordavids_22@hotmail.com>
2024-06-10 22:51:55 -0400

    Word Ladder BFS.

--- wordLadderBFS.js
+++ wordLadderBFS.js
@@ -32,43 +32,89 @@ Explanation: The endWord "cog" is not in wordList, therefore no possible transfo
 https://leetcode.com/problems/word-ladder/
 */
 
-// BFS
+// Time O(M * N)
+// Space O(M * N)
 var ladderLength = function (beginWord, endWord, wordList) {
-  let visited = new Set();
+  if (beginWord === endWord) {
+    return 0;
+  }
 
+  let visited = new Set();
+  let list = new Set(wordList);
   // Set initial state of the queue
   let queue = [{ word: beginWord, level: 1 }];
+  visited.add(beginWord);
 
   // BFS
   while (queue.length) {
-    let { word, level } = queue.shift();
-    // If we reach the word we were looking
-    if (word === endWord) {
-      return level;
-    }
-    // Look over all available words
-    for (let i = 0; i < wordList.length; i++) {
-      let wordTmp = wordList[i];
-      // We saw the word before
-      if (visited.has(wordTmp)) {
-        continue;
+    let len = queue.length;
+    for (let i = 0; i < len; i++) {
+      let { word, level } = queue.shift();
+      // If we reach the word we were looking
+      if (word === endWord) {
+        return level;
       }
-      // Compute how many chars are different on the
-      // same position
-      let charDiff = 0;
+
+      // Replace char by char and see if that's valid
       for (let j = 0; j < word.length; j++) {
-        if (word[j] !== wordTmp[j]) {
-          charDiff++;
+        for (let k = 0; k < 26; k++) {
+          // a = 97, b = 98, ...
+          let c = String.fromCharCode(97 + k);
+          let tmp = word.substring(0, j) + c + word.substring(j + 1);
+
+          if (visited.has(tmp)) {
+            continue;
+          }
+          if (list.has(tmp)) {
+            visited.add(tmp);
+            queue.push({ word: tmp, level: level + 1 });
+          }
         }
-      }
-      // If diff is only one, add it into the queue
-      if (charDiff === 1) {
-        queue.push({ word: wordTmp, level: level + 1 });
-        // Visit the queue as we want to make sure we already
-        // compute it on the queue
-        visited.add(wordTmp);
       }
     }
   }
   return 0;
 };
+
+// Option 2 - Slower
+/*
+var ladderLength = function(beginWord, endWord, wordList) {
+  let visited = new Set();
+
+  // Set initial state of the queue
+  let queue = [{word: beginWord, level: 1}];
+
+  // BFS
+  while (queue.length) {
+      let {word, level} = queue.shift();
+      // If we reach the word we were looking
+      if (word === endWord) {
+          return level;
+      }
+      // Look over all available words
+      for (let i = 0; i < wordList.length; i++) {
+          let wordTmp = wordList[i];
+          // We saw the word before
+          if (visited.has(wordTmp)) {
+              continue;
+          }
+          // Compute how many chars are different on the
+          // same position
+          let charDiff = 0;
+          for (let j = 0; j < word.length; j++) {
+              if(word[j] !== wordTmp[j]) {
+                  charDiff++;
+              }
+          }
+          // If diff is only one, add it into the queue
+          if (charDiff === 1) {
+              queue.push({word: wordTmp, level: level + 1});
+              // Visit the queue as we want to make sure we already
+              // compute it on the queue
+              visited.add(wordTmp);
+          }
+      }
+  }
+  return 0;
+};
+*/
