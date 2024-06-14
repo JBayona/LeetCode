7d0ecb6e1527bce2c66bfb6ba9de28dfbe0f97f6
JBayona <jordavids_22@hotmail.com>
2024-06-13 23:18:53 -0400

    Jump Game BFS and DFS.

--- jumpGameBFSDFS.js
+++ jumpGameBFSDFS.js
@@ -1,5 +1,6 @@
 /*
-Given an array of non-negative integers arr, you are initially positioned at start index of the array.
+Given an array of non-negative integers arr, you are initially positioned at
+start index of the array.
 When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach any index with value 0.
 
 Notice that you can not jump outside of the array at any time.
