a56f0d692a00a167545db82028bb6e5281d7369f
JBayona <jordavids_22@hotmail.com>
2024-06-22 14:21:07 -0400

    Minimum Number Of Vertices To Reach All Nodes.

--- minimumNumberOfVerticesToReachAllNodes.js
+++ minimumNumberOfVerticesToReachAllNodes.js
@@ -1,66 +1,29 @@
 /*
-You are given two strings word1 and word2. Merge the strings by adding letters
-in alternating order, starting with word1. If a string is longer than the other
-append the additional letters onto the end of the merged string.
-Return the merged string.
+Given a directed acyclic graph, with n vertices numbered from 0 to n-1, and an array edges where edges[i] = [fromi, toi] represents a directed edge from node fromi to node toi.
 
-Example 1:
-Input: word1 = "abc", word2 = "pqr"
-Output: "apbqcr"
-Explanation: The merged string will be merged as so:
-word1:  a   b   c
-word2:    p   q   r
-merged: a p b q c r
+Find the smallest set of vertices from which all nodes in the graph are reachable. It's guaranteed that a unique solution exists.
 
-Example 2:
-Input: word1 = "ab", word2 = "pqrs"
-Output: "apbqrs"
-Explanation: Notice that as word2 is longer, "rs" is appended to the end.
-word1:  a   b 
-word2:    p   q   r   s
-merged: a p b q   r   s
+Notice that you can return the vertices in any order.
 
-Example 3:
-Input: word1 = "abcd", word2 = "pq"
-Output: "apbqcd"
-Explanation: Notice that as word1 is longer, "cd" is appended to the end.
-word1:  a   b   c   d
-word2:    p   q 
-merged: a p b q c   d
-
-https://leetcode.com/problems/merge-strings-alternately/description/?envType=study-plan-v2&id=leetcode-75
+https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/description/?envType=study-plan-v2&envId=graph-theory
 */
 
-var mergeAlternately = function (word1, word2) {
-  let indexA = 0;
-  let indexB = 0;
-  let result = "";
-  while (indexA < word1.length && indexB < word2.length) {
-    result += word1[indexA++] + word2[indexB++];
+// Option 1
+var findSmallestSetOfVertices = function(n, edges) {
+  // Nodes with zero degree
+  let degree = new Array(n).fill(0);
+  for(let edge of edges) {
+      let [from, to] = edge;
+      degree[to]++;
   }
 
-  if (indexA < word1.length) {
-    result += word1.substring(indexA);
+  let minNodes = [];
+  for (let i = 0; i < n; i++) {
+      // Add in the result those values that either are parents or
+      // does not have any connection
+      if(degree[i] === 0) {
+          minNodes.push(i);
+      }
   }
-
-  if (indexB < word2.length) {
-    result += word2.substring(indexB);
-  }
-  return result;
-};
-
-// Option 2
-var mergeAlternately = function (word1, word2) {
-  let index = 0;
-  let result = "";
-  while (index < word1.length || index < word2.length) {
-    if (index < word1.length) {
-      result += word1[index];
-    }
-    if (index < word2.length) {
-      result += word2[index];
-    }
-    index++;
-  }
-  return result;
+  return minNodes;
 };
