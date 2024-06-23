c62cfacd05ca2b2a380716c7a4873eecdb3bd2b0
JBayona <jordavids_22@hotmail.com>
2024-06-23 14:16:53 -0400

    Minimum Number Of Vertices To Reach All Nodes.

--- minimumNumberOfVerticesToReachAllNodes.js
+++ minimumNumberOfVerticesToReachAllNodes.js
@@ -2,7 +2,6 @@
 Given a directed acyclic graph, with n vertices numbered from 0 to n-1, and an array edges where edges[i] = [fromi, toi] represents a directed edge from node fromi to node toi.
 
 Find the smallest set of vertices from which all nodes in the graph are reachable. It's guaranteed that a unique solution exists.
-
 Notice that you can return the vertices in any order.
 
 https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/description/?envType=study-plan-v2&envId=graph-theory
