5763117f910fddb3df0297c36ff8293218bb26e5
JBayona <jordavids_22@hotmail.com>
2024-06-27 23:16:04 -0400

    Is Graph Bipartite.

--- isGraphBipartite.js
+++ isGraphBipartite.js
@@ -33,65 +33,30 @@ We cannot find a way to divide the set of nodes into two independent subsets.
 // Space complexity: O(V)
 var isBipartite = function(graph) {
   let n = graph.length;
-  let colors = new Array(n).fill(0);
-
-  // Check all nodes
-  for (let i = 0; i < n; i++) {
-    if (colors[i] == 0 && !dfs(graph, colors, i, 1)) {
-      return false;
-    }
+  // 0 for unvisited, 1 for group A, -1 for group B
+  let colors  = new Array(n).fill(0);
+  
+  for(let i = 0; i < n; i++) {
+      if(colors[i] === 0 && !dfs(graph, colors, i, 1)) {
+          return false;
+      }
   }
-  return true;
-}
+  return true
+};
 
 function dfs(graph, colors, node, color) {
   // Mark node as visited
   colors[node] = color;
-  for(let i = 0; i < graph[node].length; i++) {
-    let next = graph[node][i];
-    // if the next node has the same color, then return false
-    if (colors[next] == colors[node]) {
-        return false;
-    }
-    // if the next node hasn't been dyed, check it can be successfully
-    if (colors[next] == 0 && !dfs(graph, colors, next, -color)) {
-        return false;
-    }
-  }
-  return true;
-}
-
-// BFS
-// Time complexity: O(V+E)
-// Space complexity: O(V)
-var isBipartite = function(graph) {
-  let n = graph.length;
-  let colors = new Array(n).fill(0);
-  
-  let queue = [];
-
-  for(let i = 0; i < n; i++) {
-    // If it's not visited
-    if(colors[i] === 0) {
-      queue.push(i);
-      colors[i] = 1;
-      
-      while(queue.length) {
-        let node = queue.shift();
-        let nextColor = -colors[node];
-        for(let i = 0; i < graph[node].length; i++) {
-          let next = graph[node][i];
-          // If it's not visited
-          if(colors[next] === 0) {
-            queue.push(next);
-            colors[next] = nextColor; 
-          } else if(colors[next] === colors[node]) {
-            // If we have the same color as the prev node, it's not a bipartite graph
-            return false;
-          }
-        }
+  for (let neighbor of graph[node]) {
+      // if the next node has the same color, then return false
+      // it means two connected nodes has the same label
+      if (colors[neighbor] == colors[node]) {
+          return false;
       }
-    }
+      // if the next node hasn't been checked, check it can be successfully
+      if (colors[neighbor] == 0 && !dfs(graph, colors, neighbor, -color)) {
+          return false;
+      }
   }
   return true;
 }
