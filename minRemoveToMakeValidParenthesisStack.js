0a8be55380ce9a888f458f141348b9e8ba205dda
JBayona <jordavids_22@hotmail.com>
2024-08-08 20:35:52 -0400

    Min Remove To Make Valid Parenthesis.

--- minRemoveToMakeValidParenthesisStack.js
+++ minRemoveToMakeValidParenthesisStack.js
@@ -10,23 +10,21 @@ It is the empty string, contains only lowercase characters, or
 It can be written as AB (A concatenated with B), where A and B are valid strings, or
 It can be written as (A), where A is a valid string.
  
-
 Example 1:
-
 Input: s = "lee(t(c)o)de)"
 Output: "lee(t(c)o)de"
 Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
-Example 2:
 
+Example 2:
 Input: s = "a)b(c)d"
 Output: "ab(c)d"
-Example 3:
 
+Example 3:
 Input: s = "))(("
 Output: ""
 Explanation: An empty string is also valid.
-Example 4:
 
+Example 4:
 Input: s = "(a(b(c)d)"
 Output: "a(b(c)d)"
 
