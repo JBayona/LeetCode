dab2e1a4d290ae0e588787dd07fa99df95d501c3
JBayona <jordavids_22@hotmail.com>
2024-08-11 20:02:10 -0400

    Linked List Cycle.

--- linkedListCycle.js
+++ linkedListCycle.js
@@ -8,16 +8,53 @@ Return true if there is a cycle in the linked list. Otherwise, return false.
 https://leetcode.com/problems/linked-list-cycle/description/
 */
 
-var hasCycle = function (head) {
-  if (head === null) return false;
+var hasCycle = function(head) {
+  if (!head) {
+      return false;
+  }
   let slow = head;
   let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) {
-      return true;
-    }
+
+  // If both are met, the linked list has a cycle
+  while(fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow === fast) {
+          return true;
+      }
+      /*
+      //Remover el ciclo si existe, fast es en donde existe el loop
+      if(slow === fast){
+          slow = head;
+          while(show.next !== fast.next){
+              slow = slow.next;
+              fast = fast.next;
+          }
+          //Fast es el causante del loop
+          fast.next = null;
+      }
+      */
+      // Different to return where the cycle starts, continuation below
+      /*
+      if(slow == fast) {
+          break;
+      }
+      */
   }
   return false;
+  // Continue to return where the cycle starts
+  /*
+  // No loops found
+  if(fast == null || fast.next == null) {
+      return null;
+  }
+  
+  // Set fast to the head and try to meet slow
+  slow = head;
+  while(slow != fast) {
+      slow = slow.next;
+      fast = fast.next;
+  }
+  return slow;
+  */
 };
