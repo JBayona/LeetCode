4f8727d46aa9c7615f12dcefc08af6dd151e20eb
JBayona <jordavids_22@hotmail.com>
2024-08-15 21:07:15 -0400

    Top K Frequent.

--- topKFrequentJavascriptHeap.js
+++ topKFrequentJavascriptHeap.js
@@ -51,6 +51,51 @@ var topKFrequent = function(nums, k) {
    return result;
 };
 
+// Option 2
+var topKFrequent = function(nums, k) {
+    let hash = {};
+    // This array saves on each space, the number of ocurrences, for
+    // example, if array is [1, 1, 2], the number 1 has two ocurrendes,
+    // that means it will store the number "1" in the index "2".
+    let count = new Array(nums.length + 1).fill(0);
+    let num = 0;
+    let result = [];
+    
+    // Count frecuencies
+    for (let i = 0; i < nums.length; i++) {
+        num = nums[i];
+        if (!(num in hash)) {
+            hash[num] = 0;
+        }
+        hash[num]++;
+    }
+    
+    //Form our array with frecuencies
+    let frequencyVal = 0;
+    for(let num in hash) {
+        frequencyVal = hash[num];
+        if(!count[frequencyVal]) {
+            count[frequencyVal] = [parseInt(num)]
+        } else {
+            count[frequencyVal].push(parseInt(num))
+        }
+    }
+    
+    // This array saves on each space, the number of ocurrences, for
+    // example, if array is [1, 1, 2], the number 1 has two ocurrendes,
+    // that means it will store the number "1" in the index "2".
+
+    // Iterate from the back as it has larger elements
+    for (let i = count.length-1; i > 0 && result.length < k; i--) {
+    	if (count[i]) {
+    		result = result.concat(count[i]);
+    	}
+    }
+    return result;
+    //return result.splice(0, k);
+};
+
+
 // Time O(LogN)
 // Space O(N)
 var topKFrequent = function(nums, k) {
