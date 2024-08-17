a85cf4dc07b95212d7bcfde28b9875768aa3e859
JBayona <jordavids_22@hotmail.com>
2024-08-17 16:13:19 -0400

    Is Valind Palindrome.

---
+++ Neetcode/validPalindrome.js
@@ -0,0 +1,44 @@
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+
+https://leetcode.com/problems/valid-palindrome/description/
+
+*/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+// Time O(N)
+// Space O(1)
+var isPalindrome = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+  let alphanumeric = /^[a-z0-9]+$/i;
+
+  while (left < right) {
+    // Skip non-alphanumeric chars
+    while (left < right && !alphanumeric.test(s[left])) {
+      left++;
+    }
+
+    // Skip non-alphanumeric chars
+    while (left < right && !alphanumeric.test(s[right])) {
+      right--;
+    }
+
+    if (s[left++].toLowerCase() !== s[right--].toLowerCase()) {
+      return false;
+    }
+  }
+  return true;
+};
