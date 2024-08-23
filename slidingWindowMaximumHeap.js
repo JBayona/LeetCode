cc443cc8c6a6827cfb2f712531d6b7635fdcebda
JBayona <jordavids_22@hotmail.com>
2024-08-23 10:32:11 -0400

    Sliding Window Maximum Heap.

--- slidingWindowMaximumHeap.js
+++ slidingWindowMaximumHeap.js
@@ -30,6 +30,42 @@ Constraints:
 https://leetcode.com/problems/sliding-window-maximum/
 */
 
+// Time O(Log N)
+var maxSlidingWindow = function(nums, k) {
+    let result = [];
+
+    // Create a Priority Queue to keep track of the maximum elements
+    // in the sliding window
+    const maxHeap = new PriorityQueue({ compare: (p1, p2) => p2.val - p1.val });
+
+    // Add the first k elements in the window
+    for (let i = 0; i < k; i++) {
+        maxHeap.enqueue({index: i, val: nums[i]}); // Store the index and value
+    }
+
+    // Add the first element in the result, the maximum
+    // Front does not remove the element from the queue
+    result.push(maxHeap.front().val);
+
+    // Iterate over the last elements starting from k
+    for (let i = k; i < nums.length; i++) {
+        maxHeap.enqueue({index: i, val: nums[i]});
+        
+        // Check if the index of the maximum element
+        // in the priority queue is outside the current window
+        let currentiMaxIndex = maxHeap.front().index;
+        while (currentiMaxIndex <= i - k) {
+            // Dequeue until the max is within the k window
+            maxHeap.dequeue();
+            currentiMaxIndex = maxHeap.front().index;
+        }
+
+        // Add to the result the maximum from the window
+        result.push(maxHeap.front().val);
+    }
+    return result;    
+};
+
 // Run two loops. In the outer loop, take all subarrays of size K.
 // In the inner loop, get the maximum of the current subarray.
 //  O((n-k+1)*k) which can also be written as O(N * K).
