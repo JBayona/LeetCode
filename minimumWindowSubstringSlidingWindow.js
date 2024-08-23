48f961b397771a319a5d3a7573fd5f3dddcd264a
JBayona <jordavids_22@hotmail.com>
2024-08-22 20:20:15 -0400

    Minimum Window Substring.

--- minimumWindowSubstringSlidingWindow.js
+++ minimumWindowSubstringSlidingWindow.js
@@ -47,13 +47,17 @@ var minWindow = function (s, t) {
   while (end < s.length) {
     let current = s[end];
     // Check if the element is in the map, if yes, decrement the target
-    if (current in map && map[current] > 0) {
-      count--;
+    if (current in map) {
+      // As long as we have elements to decrese we deduct
+      if (map[current] > 0) {
+        count--;
+      }
+      // Update the map always, we can have numbers < 0 if our window does not have
+      // a number in the "t" string
+      map[current]--;
     }
-    // Update the map
-    map[current]--;
 
-    // Check if the window is eligible for a result
+    // Elegible window for a result
     while (count === 0) {
       // Try to get the min result
       if (end - start + 1 < minLength) {
@@ -62,83 +66,19 @@ var minWindow = function (s, t) {
         minStart = start;
       }
       // Move the window
-      let head = s[start];
+      let l = s[start];
       // We only care about those being tracked
-      if (map[head] >= 0) {
+      // We need to find them again later
+      if (map[l] >= 0) {
         count++;
       }
-      map[head]++;
+      map[l]++;
       start++;
     }
     // Increase the end window
     end++;
   }
-  return minLength === Infinity ? "" : s.substring(minStart, minStart + minLength);
-};
-
-/*
-ADOBECODEBANC
-BANC
-[ADOBEC]ODEBANC, MIN WINDOW = 6
-A[DOBECODEBA]NC, MIN WINDOW = 6
-ADOBE[CODEBA]NC, MIN WINDOW = 6
-ADOBECODE[BANC], MIN WINDOW = 4
-
-// Sliding window technique
-1. Cuando la window está calificada para tener el resultado, movemos el head
-2. Cuando no, movemos la cola del window
-*/
-var minWindow = function (s, t) {
-  let map = {};
-
-  // Initialize the characters we have on S
-  for (let i = 0; i < s.length; i++) {
-    map[s[i]] = 0;
-  }
-
-  // We only care about the frequency of S in T
-  for (let i = 0; i < t.length; i++) {
-    if (t[i] in map) {
-      map[t[i]]++;
-    } else {
-      // If there is no such window in S that covers all characters in T, return the empty string "".
-      return "";
-    }
-  }
-
-  // Create window
-  let start = 0;
-  let end = 0;
-  let minLength = Number.MAX_SAFE_INTEGER;
-  let minStart = 0;
-  // Indicator to know whether the window is qualified or not, in this case the elements in T that are mapped
-  let numberOfTargets = t.length;
-  while (end < s.length) {
-    let current = s[end];
-    if (current in map && map[current] > 0) {
-      numberOfTargets--;
-    }
-    map[current]--;
-    // This indicates thw window is qualified
-    while (numberOfTargets === 0) {
-      if (minLength > end - start + 1) {
-        minLength = end - start + 1;
-        minStart = start;
-      }
-      let head = s[start];
-      // Move forward the head
-      if (map[head] >= 0) {
-        numberOfTargets++;
-      }
-      map[head]++;
-      // Move the head forward
-      start++;
-    }
-    // Increase the rail of the window
-    end++;
-  }
-
-  return minLength === Number.MAX_SAFE_INTEGER
+  return minLength === Infinity
     ? ""
     : s.substring(minStart, minStart + minLength);
 };
