4396cc9b40a470058c284577afe326f4df171187
JBayona <jordavids_22@hotmail.com>
2024-08-22 20:33:36 -0400

    Permutation String Sligin Window.

--- Neetcode/permutationInStringSlidingWindow.js
+++ Neetcode/permutationInStringSlidingWindow.js
@@ -71,138 +71,55 @@ var checkInclusion = function(s1, s2) {
     return false;
 };
 
-// O(N) where N is the length of s1
-var checkInclusion = function(s1, s2) {
+// Another option
+// Time O(N)
+ // Space O(1)
+ var checkInclusion = function(s1, s2) {
     let map = {};
-    
-    // Count the frequency of the s1
-    for(let i = 0; i < s1.length; i++) {
-        let item = s1[i];
-        if(item in map) {
-            map[item]++;
-        } else {
-            map[item] = 1;
+    // Count the frequency of S1 to check the window
+    for (let i = 0; i < s1.length; i++) {
+        let c = s1[i];
+        if (!(c in map)) {
+            map[c] = 0;
         }
+        map[c]++;
     }
     
-    // window size using the s1 lengt
-    let left = 0;
-    let right = 0;
+    let start = 0;
+    let end = 0;
     // These are the number we need to match
+    // we are interested on the number of keys
     let count = Object.keys(map).length;
-    // Traverse over the whole s2 string
-    while(right < s2.length) {
-        // Get the right element
-        let c = s2[right];
-        right++;
-        // If that char is in the map, it means we have it as part of s1
-        if(c in map) {
-            // We decrement the count we have in the map as we already saw the element
+    while (end < s2.length) {
+        let c = s2[end];
+        if (c in map) {
             map[c]--;
-            // If we reached 0 it means what we already saw the characters, then decrease
-            // the count of the numbers we want as we already satisfy the characters
+            // If we have removed all ocurrence, keep the key
+            // but decreased our number to match
             if(map[c] === 0) {
                 count--;
             }
         }
-        // Check the length of the window
-        if(right - left === s1.length) {
-            // If count is 0,it means we have found the characters
-            if(count === 0) {
+        
+        while (count === 0) {
+            if (end - start + 1 === s1.length) {
                 return true;
             }
-            // Get the left character
-            c = s2[left];
-            // Increment the left window
-            left++;
-            // If the character is part of the s1 string
-            if(c in map) {
-                // If we already set the counter to 0, increment as we need to find the element
-                if(map[c] === 0) {
+            // Move the window
+            let l = s2[start];
+            start++;
+            // If the new left char is within left, count it and
+            // add it to the map
+            if (l in map) {
+                // Increase the ocurrence as we have increased the
+                // key to match
+                if (map[l] === 0) {
                     count++;
                 }
-                // Increase the count of frequency
-                map[c]++;
+                map[l]++;
             }
         }
+        end++;
     }
     return false;
 };
-
-
-class Solution {
-    /*
-        Where n is the length of s2 and m the length of s1
-        Time: O(n*m)
-        Space: O(n+m)
-    */
-    public boolean checkInclusion(String s1, String s2) {
-        if(s1 == null || s2 == null
-           || (s1.isEmpty() && s2.isEmpty())
-           || s1.length() > s2.length()) {
-            return false;
-        }
-        Map<Character, Integer> map1 = new HashMap<>();
-        for(char c : s1.toCharArray()) {
-            map1.put(c, map1.getOrDefault(c, 0) + 1);
-        }
-        Map<Character, Integer> map2 = null;
-        for(int i = 0; i < s2.length() - s1.length() + 1; i++) {
-            map2 = new HashMap<>();
-            for(int j = i; j < i + s1.length(); j++) {
-                char c = s2.charAt(j);
-                map2.put(c, map2.getOrDefault(c, 0) + 1);
-            }
-            if(map1.equals(map2)) {
-                return true;
-            }
-        }
-        return false;
-    }
-}
-
-
-
-class Solution {
-    /*
-        Where n is the length of s2 and m the length of s1
-        Time: O(n*m)
-        Space: O(n+m)
-    */
-    public boolean checkInclusion(String s1, String s2) {
-        if(s1 == null || s2 == null
-           || (s1.isEmpty() && s2.isEmpty())
-           || s1.length() > s2.length()) {
-            return false;
-        }
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
-        for(int i = 0; i < s1.length(); i++) {
-            char c = s2.charAt(i);
-            map2.put(c, map2.getOrDefault(c, 0) + 1);
-            c = s1.charAt(i);
-            map1.put(c, map1.getOrDefault(c, 0) + 1);
-        }
-        if(map1.equals(map2)) {
-            return true;
-        }
-        int lastIndex = 0;
-        for(int i = s1.length(); i < s2.length(); i++) {
-            char c = s2.charAt(lastIndex);
-            // remove
-            Integer count = map2.get(c);
-            if(count == 1) {
-               map2.remove(c);
-            } else {
-                map2.put(c, count - 1);
-            }
-            c = s2.charAt(i);
-            map2.put(c, map2.getOrDefault(c, 0) + 1);
-            if(map1.equals(map2)) {
-                return true;
-            }
-            lastIndex++;
-        }
-        return false;
-    }
-}
