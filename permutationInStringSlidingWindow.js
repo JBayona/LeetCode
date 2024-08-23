ff8b6c97d6830526ddf021181542385bf4e9ebe9
JBayona <jordavids_22@hotmail.com>
2024-08-22 20:32:16 -0400

    Permutation In String.

--- permutationInStringSlidingWindow.js
+++ permutationInStringSlidingWindow.js
@@ -206,3 +206,56 @@ class Solution {
         return false;
     }
 }
+
+// Another option
+// Time O(N)
+ // Space O(1)
+ var checkInclusion = function(s1, s2) {
+    let map = {};
+    // Count the frequency of S1 to check the window
+    for (let i = 0; i < s1.length; i++) {
+        let c = s1[i];
+        if (!(c in map)) {
+            map[c] = 0;
+        }
+        map[c]++;
+    }
+    
+    let start = 0;
+    let end = 0;
+    // These are the number we need to match
+    // we are interested on the number of keys
+    let count = Object.keys(map).length;
+    while (end < s2.length) {
+        let c = s2[end];
+        if (c in map) {
+            map[c]--;
+            // If we have removed all ocurrence, keep the key
+            // but decreased our number to match
+            if(map[c] === 0) {
+                count--;
+            }
+        }
+        
+        while (count === 0) {
+            if (end - start + 1 === s1.length) {
+                return true;
+            }
+            // Move the window
+            let l = s2[start];
+            start++;
+            // If the new left char is within left, count it and
+            // add it to the map
+            if (l in map) {
+                // Increase the ocurrence as we have increased the
+                // key to match
+                if (map[l] === 0) {
+                    count++;
+                }
+                map[l]++;
+            }
+        }
+        end++;
+    }
+    return false;
+};
