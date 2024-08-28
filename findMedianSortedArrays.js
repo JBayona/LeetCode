377c789422467d07ce81001a4ad873130f2a41b1
JBayona <jordavids_22@hotmail.com>
2024-08-27 21:15:56 -0400

    Find Median Sorted Array.

--- findMedianSortedArrays.js
+++ findMedianSortedArrays.js
@@ -68,3 +68,37 @@ var findMedianSortedArrays = function(nums1, nums2) {
         return merged[mid];
     }
 };
+
+/*
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+            
+        int low = 0, high = n1;
+        while (low <= high) {
+            int cut1 = (low + high) / 2;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            int left1 = (cut1 == 0) ? -1e7 : nums1[cut1 - 1];
+            int left2 = (cut2 == 0) ? -1e7 : nums2[cut2 - 1];
+            int right1 = (cut1 == n1) ? 1e7 : nums1[cut1];
+            int right2 = (cut2 == n2) ? 1e7 : nums2[cut2];
+
+            if (left1 <= right2 && left2 <= right1) {
+                if ((n1 + n2) % 2 == 1)
+                    return max (left1, left2);
+                else
+                    return (max (left1, left2) + min (right1, right2)) / 2.0;    
+            } 
+            else if (left1 > right2) 
+                high = cut1 - 1;
+            else 
+                low = cut1 + 1;
+        }    
+        return 0.0;
+    }
+};
+*/
