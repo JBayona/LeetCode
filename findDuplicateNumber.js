107ffcb939070cf9bebb47a13e9149fa5a360cb7
JBayona <jordavids_22@hotmail.com>
2024-08-31 21:13:25 -0400

    Find Duplication Number.

--- findDuplicateNumber.js
+++ findDuplicateNumber.js
@@ -18,33 +18,35 @@ https://leetcode.com/problems/find-the-duplicate-number/description/
 
 // Time O(N)
 // Space O(1)
-var findDuplicate = function(nums) {
+var findDuplicate = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
     // Como los elementos son positivos no debemos hacer ningún shift
-    for(let i = 0; i < nums.length; i++) {
-      if(nums[Math.abs(nums[i])] >= 0) {
-        nums[Math.abs(nums[i])] = -nums[Math.abs(nums[i])];
-      } else {
-        // this is the repeated
-        return Math.abs(nums[i]);
-      }
+    if (nums[Math.abs(num)] >= 0) {
+      nums[Math.abs(num)] = -nums[Math.abs(num)];
+    } else {
+      // This is the repeated number
+      return Math.abs(nums[i]);
     }
   }
+  return -1;
+};
 
 // Time O(N)
 // Space O(N)
-var findDuplicate = function(nums) {
-    var map = {};
-    let item;
-    for(let i = 0; i < nums.length; i++){
-        item = nums[i];
-        if(item in map){
-            return item;
-        }else{
-            map[item] = true;
-        }
+var findDuplicate = function (nums) {
+  var map = {};
+  let item;
+  for (let i = 0; i < nums.length; i++) {
+    item = nums[i];
+    if (item in map) {
+      return item;
+    } else {
+      map[item] = true;
     }
-    return false;
-}
+  }
+  return false;
+};
 
-array = [1,2,5,6,2];
+array = [1, 2, 5, 6, 2];
 console.log(findDuplicate(array));
