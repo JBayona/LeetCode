dcc14ccfa36dccca61e16b0656d01fc344b98f6d
JBayona <jordavids_22@hotmail.com>
2024-09-01 13:59:50 -0400

    Merge K Sorted Linked Lists Min Heap.

--- mergeKSortedLinkedListsMinHeap.js
+++ mergeKSortedLinkedListsMinHeap.js
@@ -14,6 +14,42 @@ Output: 1->1->2->3->4->4->5->6
 https://leetcode.com/problems/merge-k-sorted-lists/description/
 */
 
+// Time O(N Log K)
+// Space O(1)
+var mergeKLists = function(lists) {
+  // Min heap
+  let heap = new PriorityQueue({
+      compare: (a, b) => a.val - b.val
+  });
+
+  // Iterate the first node for each list, we know that each node has
+  // a reference to the next nodes with the "next" pointer, so we have
+  // everything. We need to get one by one and traverse
+  for (let list of lists) {
+      if (list) {
+          heap.enqueue(list);
+      }
+  }
+
+  let head = new ListNode(0);
+  let current = head;
+  while (!heap.isEmpty()) {
+      // This will get the smaller node so we need to add it
+      let node = heap.dequeue();
+      current.next = new ListNode(node.val);
+      // Move the list
+      current = current.next;
+      // Move the node, as they are already sorted
+      // and if it's not null, push it again to the heap
+      node = node.next;
+      if (node) {
+          heap.enqueue(node);
+      }
+  }
+  return head.next;
+};
+
+
 // O(nk Log k)
 public class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
