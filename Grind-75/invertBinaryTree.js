fd11fd41c3cfee1c967c2c408afa25a216f54e5a
JBayona <jordavids_22@hotmail.com>
2024-09-02 14:04:54 -0400

    Invert Binary Tree.

--- Grind-75/invertBinaryTree.js
+++ Grind-75/invertBinaryTree.js
@@ -21,13 +21,43 @@ https://leetcode.com/problems/invert-binary-tree/description/
 // for both its children, first inverting them.
 // Time O(n) where n is the number of nodes
 // Time O(h) where h is the height of the tree, calls to the stack
+// Recursion
 var invertTree = function (root) {
-  // Return the root
   if (!root) {
-    return root;
+    return null;
   }
-  let l = invertTree(root.left);
-  root.left = invertTree(root.right);
-  root.right = l;
+  let left = invertTree(root.left);
+  let right = invertTree(root.right);
+  root.left = right;
+  root.right = left;
+  return root;
+};
+
+// Iterative BFS
+var invertTree = function (root) {
+  if (!root) {
+    return null;
+  }
+  let queue = [];
+  queue.push(root);
+
+  while (queue.length) {
+    let len = queue.length;
+    for (let i = 0; i < len; i++) {
+      let node = queue.shift();
+      let left = node.left;
+      let right = node.right;
+      // Invert
+      node.left = right;
+      node.right = left;
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+  }
   return root;
 };
