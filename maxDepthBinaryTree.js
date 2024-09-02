11ca9525ab26ff610cf3bb87f242e4046cb01122
JBayona <jordavids_22@hotmail.com>
2024-09-02 14:14:05 -0400

    Max Depth Binary Tree.

--- maxDepthBinaryTree.js
+++ maxDepthBinaryTree.js
@@ -12,21 +12,22 @@ function findHeight(t){
 https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
 
 /*Recursion para obtener el height*/
-function Tree(val, left, right){
+function Tree(val, left, right) {
   this.val = val;
   this.left = left ? left : null;
   this.right = right ? right : null;
 }
 
-
-function getHeight(t){
-  if(t === null){
+// Time O(N)
+// Space O(1)
+var maxDepth = function (root) {
+  if (!root) {
     return 0;
   }
-  let leftHeigth = getHeight(t.left);
-  let rightHeigth = getHeight(t.right);
-  return Math.max(leftHeigth, rightHeigth) + 1;
-}
+  let left = maxDepth(root.left);
+  let right = maxDepth(root.right);
+  return Math.max(left, right) + 1;
+};
 
 /*
        1
@@ -38,5 +39,9 @@ function getHeight(t){
       8 9
 */
 
-tree = new Tree(1, new Tree(2, new Tree(4), new Tree(5, new Tree(8), new Tree(9))), new Tree(3, new Tree(6), new Tree(7)));
+tree = new Tree(
+  1,
+  new Tree(2, new Tree(4), new Tree(5, new Tree(8), new Tree(9))),
+  new Tree(3, new Tree(6), new Tree(7))
+);
 console.log(getHeight(tree));
