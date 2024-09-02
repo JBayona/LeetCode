5a16e622190cae6650c3198be0b29e947181eb31
JBayona <jordavids_22@hotmail.com>
2024-09-02 16:16:40 -0400

    Shortest Path With Obstacles.

--- shortestPathWithObstaclesBFSMaze.js
+++ shortestPathWithObstaclesBFSMaze.js
@@ -18,36 +18,39 @@ https://leetcode.com/problems/shortest-path-in-a-grid-with-obstacles-elimination
 var shortestPath = function(grid, k) {
     let ROW = grid.length;
     let COL = grid[0].length;
-    let blocked = [];
-    
-    // Visited matrix
-    let visited = new Array(ROW);
-    for(let i = 0; i < ROW; i++) {
-        visited[i] = new Array(COL).fill(Number.MAX_SAFE_INTEGER);
+    let visited = [];
+    for (let i = 0; i < ROW; i++) {
+        visited[i] = new Array(COL).fill(Infinity);
     }
     
-    
-    let row = [-1, 0, 1, 0];
-    let col = [0, 1, 0, -1];
-    
     let queue = [];
-    queue.push({x: 0, y: 0, n: 0, d: 0});
+    queue.push({x: 0, y: 0, d: 0, n: 0});
+
+    let row = [0, -1, 0, 1];
+    let col = [-1, 0, 1, 0];
+
     while(queue.length) {
-        let node = queue.shift();
-        if(node.x === ROW - 1 && node.y === COL - 1) {
-            return node.d;
-        }
-        for(let i = 0; i < 4; i++) {
-            let nextRow = node.x + row[i];
-            let nextCol = node.y + col[i];
-            
-            if(isSafe(nextRow, nextCol, grid)) {
-                // Only move when the next move is smaller and the result can be improved
-                let numberObstacles = node.n + grid[nextRow][nextCol];
-                if(numberObstacles < visited[nextRow][nextCol] && numberObstacles <= k) {
-                    // Fewer number of obstacles to reach this point
-                    visited[nextRow][nextCol] = numberObstacles;
-                    queue.push({x: nextRow, y: nextCol, n: numberObstacles, d: node.d + 1});
+        let len = queue.length;
+        for (let i = 0; i < len; i++) {
+            let {x, y, d, n} = queue.shift();
+            // We reach to the lower right corner
+            if (x === ROW - 1 && y === COL - 1) {
+                return d;
+            }
+
+            for (let j = 0; j < 4; j++) {
+                let nextRow = x + row[j];
+                let nextCol = y + col[j];
+                if (isSafe(nextRow, nextCol, grid)) {
+                    let numberOfObstacles = n + grid[nextRow][nextCol];
+                    // In order to qualify for a result, we need to improve the number of obstacles
+                    // we have seen before and we need to be under or equal the limit "k" to be a
+                    // candidate for improvement
+                    if (numberOfObstacles < visited[nextRow][nextCol] && numberOfObstacles <= k) {
+                        // Fewer number of obstacles to reach this point
+                        visited[nextRow][nextCol] = numberOfObstacles;
+                        queue.push({x: nextRow, y: nextCol, d: d + 1, n: numberOfObstacles});
+                    }
                 }
             }
         }
@@ -58,7 +61,6 @@ var shortestPath = function(grid, k) {
 function isSafe(row, col, grid) {
     let ROW = grid.length;
     let COL = grid[0].length;
-    
     return (
         row >= 0 && row < ROW &&
         col >= 0 && col < COL
