e0405b5d9febe5b32490add4b10b9f7ca3b83bec
JBayona <jordavids_22@hotmail.com>
2024-09-02 14:56:50 -0400

    Count And Say.

--- countAndSay.js
+++ countAndSay.js
@@ -25,6 +25,65 @@ https://leetcode.com/problems/count-and-say/description/
 
 */
 
+let memo = {};
+var countAndSay = function (n) {
+  if (n in memo) {
+    return memo[n];
+  }
+  if (n === 1) {
+    return "1";
+  }
+  let tmp = countAndSay(n - 1);
+  memo[n] = formStr(tmp);
+  return memo[n];
+};
+
+function formStr(str) {
+  let count = 1;
+  let tmp = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      tmp += `${count}${str[i]}`;
+      count = 1;
+    }
+  }
+  return tmp;
+}
+
+/*
+Option 2
+var countAndSay = function(n) {
+    if (n === 1) {
+        return "1";
+    }
+
+    let memo = {1: "1"}
+    for (let i = 2; i <= n; i++) {
+        let c = helper(memo[i - 1]);
+        memo[i] = c; 
+    }
+    return memo[n];
+};
+
+function helper(str) {
+    let count = 1;
+    let res = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === str[i + 1]) {
+            count++;
+        } else {
+            res += (count + str[i]).toString();
+            count = 1;
+        }
+    }
+    return res;
+}
+*/
+
+/*
+// Option 3
 var countAndSay = function(n){
   if(n === 1) return "1";
   if(n === 2) return "11";
@@ -51,6 +110,7 @@ function formStr(str){
   }
   return tmp;
 };
+*/
 
 n = 6;
 console.log(countAndSay(n));
