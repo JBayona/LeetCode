31d93b773b6299011202a2a9197af016d90a42fd
JBayona <jordavids_22@hotmail.com>
2024-09-07 17:24:26 -0400

    String Compression.

--- stringCompression.js
+++ stringCompression.js
@@ -1,17 +1,13 @@
 /*
 Given an array of characters, compress it in-place.
-
 The length after compression must always be smaller than or equal to the original array.
-
 Every element of the array should be a character (not int) of length 1.
 
-After you are done modifying the input array in-place, return the new length of the array.
- 
+After you are done modifying the input array in-place, return the new length of the array. 
 Follow up:
 Could you solve it using only O(1) extra space?
  
 Example 1:
-
 Input:
 ["a","a","b","b","c","c","c"]
 
@@ -23,30 +19,34 @@ Explanation:
 
 https://leetcode.com/problems/string-compression/
 */
-var compress = function(chars) {
-  if(chars.length <= 1) {
-      return chars.length;
-  }
-  let count = 0;
-  let index = 0;
-  let current = 0;
-  let tmp = 0;
-  while(current < chars.length) {
-      if(tmp < chars.length && chars[tmp] === chars[current]) {
-          count++;
-          tmp++;
-      } else {
-          chars[index++] = chars[current];
-          if(count > 1) {
-              let countChars = count.toString().split('');
-              for(let i = 0; i < countChars.length; i++) {
-                  chars[index++] = countChars[i];
-              }
-          }
-          current = tmp;
-          count = 0;
-      }
-  }
-  return index;
-};
 
+// Time O(N)
+// Space O(1)
+var compress = function(chars) {
+    let index = 0;
+    let indexWrite = 0;
+    while (index < chars.length) {
+        let c = chars[index];
+        let count = 0;
+
+        // Count the number of repeated chards.
+        while (index < chars.length && chars[index] === c) {
+            count++;
+            index++;
+        }
+        // Write the char
+        chars[indexWrite] = c;
+        // Move to now write the number of frequency
+        indexWrite++;
+        if (count > 1) {
+            // This will cover if we have strings with different sizes
+            let str = count.toString();
+            for (let i = 0; i < str.length; i++) {
+                let d = str[i];
+                chars[indexWrite] = d;
+                indexWrite++;
+            }
+        }
+    }
+    return indexWrite;
+};
