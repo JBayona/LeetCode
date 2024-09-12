563107da8bc71de27c0c583eba9fc078fc671bad
JBayona <jordavids_22@hotmail.com>
2024-09-11 21:46:57 -0400

    Right Side View BFS.

--- rightSideViewTreeBFS.js
+++ rightSideViewTreeBFS.js
@@ -41,3 +41,53 @@ var rightSideView = function(root) {
     }
     return result;
 };
+
+// DFS Postorder
+// Time O(N)
+// Space O(1)
+let lastLevel = -1;
+var rightSideView = function (root) {
+  let result = [];
+  if (!root) {
+    return [];
+  }
+  dfs(root, 0, result);
+  return result;
+};
+
+function dfs(root, level, result) {
+  if (!root) {
+    return;
+  }
+  if (level === result.length) {
+    result.push(root.val);
+  }
+  dfs(root.right, level + 1, result);
+  dfs(root.left, level + 1, result);
+}
+
+// DFS Postorder
+// Time O(N)
+// Space O(1)
+var rightSideView = function (root) {
+  let hash = {};
+  let result = [];
+
+  if (!root) {
+    return [];
+  }
+  dfs(root, 0, hash, result);
+  return result;
+};
+
+function dfs(root, depth, hash, result) {
+  if (!root) {
+    return;
+  }
+  if (!(depth in hash)) {
+    result.push(root.val);
+    hash[depth] += 1;
+  }
+  dfs(root.right, depth + 1, hash, result);
+  dfs(root.left, depth + 1, hash, result);
+}
