3e8365afb86998a2b675f13abbca349c9f5433a1
JBayona <jordavids_22@hotmail.com>
2024-09-12 21:01:05 -0400

    Kth Smallest BST.

--- Grind-75/kthSmallestElementInBSTTree.js
+++ Grind-75/kthSmallestElementInBSTTree.js
@@ -12,28 +12,75 @@ Output: 3
 https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 */
 
-var kthSmallest = function (root, k) {
+var kthSmallest = function(root, k) {
   let node = root;
   let stack = [];
-  let count = 0;
-
+  
   // Smallest elements come from left branches
-  while (node) {
-    stack.push(node);
-    node = node.left;
+  while(node) {
+      stack.push(node);
+      node = node.left;
   }
 
-  while (stack.length) {
-    let node = stack.pop();
-    count++;
-    if (count === k) {
-      return node.val;
-    }
-    let right = node.right;
-    while (right) {
-      stack.push(right);
-      right = right.left;
-    }
+  let count = 0;
+  while(stack.length) {
+      let node = stack.pop();
+      count++;
+      if(count === k) {
+          return node.val;
+      }
+      let right = node.right;
+      while(right) {
+          stack.push(right);
+          right = right.left;
+      }
   }
   return -1;
 };
+
+/*
+// Option 2
+// DFS
+var kthSmallest = function(root, k) {
+  count = k;
+  result = null;
+  inorder(root);
+  return result;
+};
+
+function inorder(node) {
+  if(!node) return;
+  inorder(node.left);
+  
+  count--;
+  if(count===0) {
+      result = node.val;
+      return;
+  }
+  
+  inorder(node.right);
+}
+*/
+
+// Option 3 = Heap
+/*
+class Solution {
+  public int kthSmallest(TreeNode root, int k) {
+      Queue<Integer> heap = new PriorityQueue<>((a,b) -> b-a);
+      preorder(root, heap, k);
+      return heap.poll();
+  }
+  
+  public void preorder(TreeNode node, Queue<Integer> heap, int k) {
+      if(node == null) {
+          return;
+      }
+      heap.add(node.val);
+      if(heap.size() > k) {
+          heap.poll();
+      }
+      preorder(node.left, heap, k);
+      preorder(node.right, heap, k);
+  }
+}
+*/
