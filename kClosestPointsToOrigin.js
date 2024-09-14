6812194e475bf3f1a2da0c56011921cf5276989f
JBayona <jordavids_22@hotmail.com>
2024-09-14 15:02:21 -0400

    K Closest Point To Origin.

--- kClosestPointsToOrigin.js
+++ kClosestPointsToOrigin.js
@@ -21,30 +21,55 @@ https://leetcode.com/problems/k-closest-points-to-origin/
 // https://www.youtube.com/watch?v=eaYX0Ee0Kcg
 // O(N log N)
 
-// NOTE: We can user a max heap too (max heap has the root as greater element)
-var kClosest = function (points, K) {
-  // If there's no points array of no K.
-  if (!points.length || !K) {
-    return [];
+// Time O(NLogN)
+// Space O(1)
+var kClosest = function(points, k) {
+  let minHeap = new PriorityQueue({
+      compare: (a, b) => a.val - b.val
+  });
+
+  // Add all elements to the queue and compute
+  // the distance to the closest element
+  for (let point of points) {
+      let [x, y] = point;
+      minHeap.enqueue({point: point, val: calculateDistance(x, y, 0, 0)});
   }
 
-  let origin = [0, 0];
+  let result = [];
+  for (let i = 0; i < k; i++) {
+      result.push(minHeap.dequeue().point)
+  }
+  return result;
+};
+
+function calculateDistance(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+}
+
+// Option 1 - Sort
+// Reference: https://en.wikipedia.org/wiki/Euclidean_distance
+// https://www.youtube.com/watch?v=eaYX0Ee0Kcg
+// O(N log N)
+var kClosest = function(points, K) {
+  // If there's no points array of no K.
+  if(!points.length || !K) {
+      return [];
+  }
+  
+  let origin = [0,0];
   let array = [];
   let result = [];
-  for (let i = 0; i < points.length; i++) {
-    // Calculate the distance from the point to the origin.
-    array.push({
-      coordinate: points[i],
-      distance: getEuclideanDistance(points[i], origin),
-    });
+  for(let i = 0; i < points.length; i++) {
+      // Calculate the distance from the point to the origin.
+      array.push({coordinate: points[i], distance: getEuclideanDistance(points[i], origin)});
   }
-
+  
   // Custom sort - comparator using distance for sorting
-  array.sort((a, b) => a.distance - b.distance);
-
+  array.sort((a,b) => (a.distance - b.distance));
+  
   // Create the array result
-  for (let i = 0; i < K; i++) {
-    result.push(array[i].coordinate);
+  for(let i = 0; i < K; i++) {
+      result.push(array[i].coordinate);
   }
   return result;
 };
