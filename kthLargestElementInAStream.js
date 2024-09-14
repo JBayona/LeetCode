987fb13c39c9b31cbb0cb33a3842065bb128f5a7
JBayona <jordavids_22@hotmail.com>
2024-09-14 14:31:38 -0400

    kth Largest Element Is A Stream.

---
+++ kthLargestElementInAStream.js
@@ -0,0 +1,85 @@
+/*
+You are part of a university admissions office and need to keep track of the kth highest test
+score from applicants in real-time. This helps to determine cut-off marks for interviews and admissions
+dynamically as new applicants submit their scores.
+
+You are tasked to implement a class which, for a given integer k, maintains a stream of test scores
+and continuously returns the kth highest test score after a new score has been submitted.
+More specifically, we are looking for the kth highest score in the sorted list of all scores.
+
+Implement the KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of test scores nums.
+int add(int val) Adds a new test score val to the stream and returns the element representing the kth
+largest element in the pool of test scores so far.
+ 
+Example 1:
+Input:
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+
+Output: [null, 4, 5, 5, 8, 8]
+Explanation:
+
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3); // return 4
+kthLargest.add(5); // return 5
+kthLargest.add(10); // return 5
+kthLargest.add(9); // return 8
+kthLargest.add(4); // return 8
+
+Example 2:
+Input:
+["KthLargest", "add", "add", "add", "add"]
+[[4, [7, 7, 7, 7, 8, 3]], [2], [10], [9], [9]]
+
+Output: [null, 7, 7, 7, 8]
+
+Explanation:
+KthLargest kthLargest = new KthLargest(4, [7, 7, 7, 7, 8, 3]);
+kthLargest.add(2); // return 7
+kthLargest.add(10); // return 7
+kthLargest.add(9); // return 7
+kthLargest.add(9); // return 8
+
+https://leetcode.com/problems/kth-largest-element-in-a-stream/description/
+*/
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function(k, nums) {
+    this.k = k
+    this.heap = new PriorityQueue({
+        compare: (a, b) => a - b
+    });
+
+    // Add all but keep the K elements only
+    for (let num of nums) {
+        this.heap.enqueue(num);
+        if (this.heap.size() > k) {
+            this.heap.dequeue();
+        }
+    }
+};
+
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    this.heap.enqueue(val);
+    if (this.heap.size() > this.k) {
+        this.heap.dequeue();
+        return this.heap.front();
+    } else {
+        return this.heap.front();
+    }
+};
+
+/** 
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
