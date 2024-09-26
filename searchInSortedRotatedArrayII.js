026aa3d2a85c3d4e36304de350bd609e483003b6
JBayona <jordavids_22@hotmail.com>
2024-09-25 23:52:24 -0400

    Search And Rotated.

--- searchInSortedRotatedArrayII.js
+++ searchInSortedRotatedArrayII.js
@@ -15,6 +15,78 @@ https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
 */
 
 // Option 1
+// Pivot: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/solutions/5298223/pivot-find-minimum-rotated-sorted-1/
+// Time O(LogN)
+var search = function(nums, target) {
+    // Find the pivot where the rotation ends
+    let pivot = findPivotWithDuplicates(nums, 0, nums.length - 1);
+
+    // We are looking for the pivot
+    if(nums[pivot] === target) {
+        return true;
+    }
+
+    // The array is already sorted so we just need to run a normal Binary Search
+    // in the entire array
+    if (pivot === -1) {
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
+
+    return binarySearch(nums, target, 0, pivot) || binarySearch(nums, target, pivot + 1, nums.length - 1);
+};
+
+// Pivot the one before flips
+function findPivotWithDuplicates(nums, start, end) {
+    let n = nums.length;
+    // Skip all duplicates
+    while (start < n - 1 && nums[start] == nums[start + 1]) {
+        start++;
+    }
+    while (end > 0 && nums[end] == nums[end - 1]) {
+        end--;
+    }
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        // If any of the next two conditions is met, then that's 
+        // the pivot
+        if (mid < end && nums[mid] > nums[mid + 1]) {
+            return mid;
+        }
+        if (mid > start && nums[mid - 1] > nums[mid]) {
+            return mid - 1;
+        }
+        // If nums[mid] <= nums[start] it means that the left side is the
+        // rotated part and the mid might also be the pivot, example
+        // [4 5 6 0 1 2 3] -> 0 is the mid
+        // If this condition is not met, then, the right side is the one
+        // where the target is
+        if(nums[mid] <= nums[start]) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
+
+function binarySearch(nums, target, start, end) {
+    while(start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (nums[mid] === target) {
+            return true;
+        }
+        if (nums[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    // Target not found
+    return false;
+}
+
+
+// Option 2
 // Time O(LogN)
 var search = function(nums, target) {
     let start = 0;
@@ -39,7 +111,7 @@ var search = function(nums, target) {
     return false;
 };
 
-// Option 2
+// Option 1
 var search = function(nums, target) {
   let start = 0,
   let end = nums.length - 1,
