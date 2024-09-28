38421a756dd662c6fcc69bc96adda5439ee20308
JBayona <jordavids_22@hotmail.com>
2024-09-27 22:09:22 -0400

    Combination Sum.

--- combinationSum.js
+++ combinationSum.js
@@ -19,15 +19,11 @@ A solution set is:
 https://leetcode.com/problems/combination-sum/description/
 */
 
-/**
- * @param {number[]} candidates
- * @param {number} target
- * @return {number[][]}
- */
+// Time O(2^N)
+// Space O(N)
 var combinationSum = function(candidates, target) {
     // We should remove duplicates in case of found
     candidates.sort((a,b) => a - b);
-    
     let result = [];
     let tmp = [];
     let start = 0;
@@ -36,18 +32,15 @@ var combinationSum = function(candidates, target) {
 };
 
 function helper(array, target, tmp, result, start) {
-    
     // If the number becomes negative
     if(target < 0) {
         return;
     }
-    
     // We have reached our goal
     if(target === 0) {
         result.push(tmp);
         return;
     }
-    
     // Recur for all remaining elements that have values
     // smaller than our targets
     for(let i = start; i < array.length; i++) {
@@ -58,7 +51,8 @@ function helper(array, target, tmp, result, start) {
         tmp.push(array[i]);
         // Cambiandolo i por start, te da todas las posibles combinaciones, permutando el número
         // i.e [2,2,3], [3,2,2], [2,3,2], [7]
-        helper(array, target - array[i], tmp.concat(), result, i);
+        // SENDING ONLY "I" IS MAKING US TO USE THE ELEMENT UNLIMITED TIMES.
+        helper(array, target - array[i], tmp.concat(), result, i); // Cambiandolo a start, te da todas las posibles combinaciones (permutaciones)
         tmp.pop();
     }
 }
