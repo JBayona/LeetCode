cec931c10a2b643f054bea7537db54028f349c05
JBayona <jordavids_22@hotmail.com>
2024-09-29 14:04:03 -0400

    Letter Combination.

--- Grind-75/letterCombinations.js
+++ Grind-75/letterCombinations.js
@@ -23,58 +23,14 @@ function combinations(result, tmp, map, digits, index){
       return;
   }
   
-  let possibleChars = map[Number(digits[index])];
+  let number = Number(digits[index]);
+  let possibleChars = map[number];
   for(let i = 0; i < possibleChars.length; i++) {
-      combinations(result, tmp + possibleChars.charAt(i), map, digits, index + 1)
+      combinations(result, tmp + possibleChars.charAt(i), map, digits, index + 1);
   }
 }
 
-// Option 1
-var letterCombinations = function(digits) {
-  // Para matcharlo con los elementos exactos sin tener que hacer alguna otra operación
-  let map = ['0', '1', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
-  let result = [];
-  if(digits.length === 0) return []
-  combinations(result, '', map, digits, 0);
-  return result;
-};
-
-function combinations(result, tmp, map, digits, index){
-  if(index === digits.length) {
-      result.push(tmp);
-      return;
-  }
-  
-  let possibleChars = map[digits[index]];
-  for(let i = 0; i < possibleChars.length; i++) {
-      combinations(result, tmp + possibleChars.charAt(i), map, digits, index + 1)
-  }
-}
-
-var letterCombinations = function(digits) {
-    // Para matcharlo con los elementos exactos sin tener que hacer alguna otra operación
-    let map = ['0', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
-    let result = [];
-
-    if(digits.length === 0) return []
-
-    combinations(result, '', map, digits, 0);
-    return result;
-};
-
-function combinations(result, tmp, map, digits, count){
-    if(count === digits.length) {
-        result.push(tmp);
-        return;
-    }
-    
-    for(let i = 0; i < map[(digits[count]) * 1].length; i++) { // *1 will make it number
-        combinations(result, tmp + map[(digits[count]) * 1].charAt(i), map, digits, count + 1)
-    }
-}
-
-
-
+// Option 2
 var letterCombinations = function(digits) {
   var length = digits.length;
   var hash = {	
