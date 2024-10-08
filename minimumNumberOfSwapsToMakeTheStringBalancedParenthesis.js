7d98e1343c8f5e4fc4943473a032a86ce5ec527e
JBayona <jordavids_22@hotmail.com>
2024-10-07 23:27:01 -0400

    Minimum Number Of Swaps To Make The String Balanced Parenthesis.

---
+++ minimumNumberOfSwapsToMakeTheStringBalancedParenthesis.js
@@ -0,0 +1,53 @@
+/*
+You are given a 0-indexed string s of even length n. The string consists of exactly n / 2 opening brackets '[' and n / 2 closing brackets ']'.
+
+A string is called balanced if and only if:
+
+It is the empty string, or
+It can be written as AB, where both A and B are balanced strings, or
+It can be written as [C], where C is a balanced string.
+You may swap the brackets at any two indices any number of times.
+
+Return the minimum number of swaps to make s balanced.
+
+ 
+
+Example 1:
+
+Input: s = "][]["
+Output: 1
+Explanation: You can make the string balanced by swapping index 0 with index 3.
+The resulting string is "[[]]".
+Example 2:
+
+Input: s = "]]][[["
+Output: 2
+Explanation: You can do the following to make the string balanced:
+- Swap index 0 with index 4. s = "[]][][".
+- Swap index 1 with index 5. s = "[[][]]".
+The resulting string is "[[][]]".
+Example 3:
+
+Input: s = "[]"
+Output: 0
+Explanation: The string is already balanced.
+
+https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/description
+*/
+
+// Option 2
+// Time O(N)
+var minSwaps = function (s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    let c = s[i];
+    if (stack.length && c === "]") {
+      stack.pop();
+    } else {
+      stack.push(c);
+    }
+  }
+  // The stack has the number of unbalanced parenthesis, so the minimum swaps we need to do
+  // is the length of the stack / 2
+  return stack.length / 2;
+};
