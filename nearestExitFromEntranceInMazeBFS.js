292aa4844d28b41ebfbd59a43d5bde8fc0060fb5
JBayona <jordavids_22@hotmail.com>
2024-10-15 00:21:28 -0400

    Nearest Exit From Entrance In Maze BFS.

--- nearestExitFromEntranceInMazeBFS.js
+++ nearestExitFromEntranceInMazeBFS.js
@@ -15,61 +15,61 @@ https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/description/
 // Time O(M * N)
 // Space O(M * N)
 var nearestExit = function(grid, entrance) {
-  if (!grid.length) {
-      return 0;
-  }
+    if (!grid.length) {
+        return 0;
+    }
 
-  let ROW = grid.length;
-  let COL = grid[0].length;
+    let ROW = grid.length;
+    let COL = grid[0].length;
 
-  let visited = new Array(ROW);
-  for (let i = 0; i < ROW; i++) {
-      visited[i] = new Array(COL).fill(false);
-  }
+    let visited = new Array(ROW);
+    for (let i = 0; i < ROW; i++) {
+        visited[i] = new Array(COL).fill(false);
+    }
 
-  let ROWK = [0, -1, 0, 1];
-  let COLK = [-1, 0, 1, 0];
+    let ROWK = [0, -1, 0, 1];
+    let COLK = [-1, 0, 1, 0];
 
-  let queue = [];
-  queue.push({x: entrance[0], y: entrance[1], d: 0});
-  visited[entrance[0]][entrance[1]] = true;
+    let queue = [];
+    queue.push({x: entrance[0], y: entrance[1], d: 0});
+    visited[entrance[0]][entrance[1]] = true;
 
-  while (queue.length) {
-      let len = queue.length;
-      for (let i = 0; i < len; i++) {
-          let {x, y, d} = queue.shift();
-          // We are not allowed to exit in the same entrance if it's border
-          if (isExit(x, y, grid) && (x !== entrance[0] || y !== entrance[1])) {
-              return d;
-          }
-          for (let i = 0; i < 4; i++) {
-              let nextRow = x + ROWK[i];
-              let nextCol = y + COLK[i];
-              if (isSafe(nextRow, nextCol, grid, visited)) {
-                  visited[nextRow][nextCol] = true;
-                  queue.push({x: nextRow, y: nextCol, d: d + 1});
-              }
-          }
-      }
-  }
-  return -1;
+    while (queue.length) {
+        let len = queue.length;
+        for (let i = 0; i < len; i++) {
+            let {x, y, d} = queue.shift();
+            // We are not allowed to exit in the same entrance if it's border
+            if (isExit(x, y, grid) && (x !== entrance[0] || y !== entrance[1])) {
+                return d;
+            }
+            for (let j = 0; j < 4; j++) {
+                let nextRow = x + ROWK[j];
+                let nextCol = y + COLK[j];
+                if (isSafe(nextRow, nextCol, grid, visited)) {
+                    visited[nextRow][nextCol] = true;
+                    queue.push({x: nextRow, y: nextCol, d: d + 1});
+                }
+            }
+        }
+    }
+    return -1;
 };
 
 function isExit(x, y, grid) {
-  let ROW = grid.length;
-  let COL = grid[0].length;
-  return (
-      x === 0 || x === ROW - 1 || y === 0 || y === COL - 1
-  );
+    let ROW = grid.length;
+    let COL = grid[0].length;
+    return (
+        x === 0 || x === ROW - 1 || y === 0 || y === COL - 1
+    );
 }
 
 function isSafe(row, col, grid, visited) {
-  let ROW = grid.length;
-  let COL = grid[0].length;
-  return (
-      row >= 0 && row < ROW &&
-      col >= 0 && col < COL &&
-      visited[row][col] === false &&
-      grid[row][col] === '.'
-  );
+    let ROW = grid.length;
+    let COL = grid[0].length;
+    return (
+        row >= 0 && row < ROW &&
+        col >= 0 && col < COL &&
+        visited[row][col] === false &&
+        grid[row][col] === '.'
+    );
 }
