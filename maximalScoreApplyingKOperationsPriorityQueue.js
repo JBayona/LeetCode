64ba4ac5304254c2c56f2d80c6f614418f9a21fe
JBayona <jordavids_22@hotmail.com>
2024-10-14 23:12:01 -0400

    Maximal Score Applying K Operations Priority Queue.

--- maximalScoreApplyingKOperationsPriorityQueue.js
+++ maximalScoreApplyingKOperationsPriorityQueue.js
@@ -1,8 +1,6 @@
 /*
 You are given a 0-indexed integer array nums and an integer k. You have a starting score of 0.
-
 In one operation:
-
 choose an index i such that 0 <= i < nums.length,
 increase your score by nums[i], and
 replace nums[i] with ceil(nums[i] / 3).
