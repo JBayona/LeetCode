4e69ad0aac7ae0fce1901e41480b0e5e66a74e81
JBayona <jordavids_22@hotmail.com>
2024-10-18 19:51:05 -0400

    Count Number Max Bitwise Subset Backtrack.

--- countNumberMaxBitwiseSubsetBacktrack.js
+++ countNumberMaxBitwiseSubsetBacktrack.js
@@ -1,7 +1,6 @@
 /*
 Given an integer array nums, find the maximum possible bitwise OR of a subset of nums and
 return the number of different non-empty subsets with the maximum bitwise OR.
-
 An array a is a subset of an array b if a can be obtained from b by deleting some (possibly zero) elements of b.
 Two subsets are considered different if the indices of the elements chosen are different.
 
