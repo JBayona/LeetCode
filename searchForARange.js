18220f832720d9661abfb13225eb4250913400b8
JBayona <jordavids_22@hotmail.com>
2024-10-28 22:56:51 -0400

    Find First And Last Range.

--- searchForARange.js
+++ searchForARange.js
@@ -13,54 +13,52 @@ https://leetcode.com/problems/search-for-a-range/description/
 
 */
 
-function searchRange(nums, target){
-  return [findLeftMost(nums,target), findRightMost(nums,target)];
+function searchRange(nums, target) {
+  return [findLeftMost(nums, target), findRightMost(nums, target)];
 }
 
 function findLeftMost(nums, target) {
-  var len = nums.length;
-  var start = 0;
-  var end = len - 1;
-  var mid = 0;
+  let len = nums.length;
+  let start = 0;
+  let end = len - 1;
   /*Una vez que encontramos el numero deseado solo
-  decrementamos el end hasta que sea mayor que start
-  para romper el ciclo y verificar el resultado*/
+    decrementamos el end hasta que sea mayor que start
+    para romper el ciclo y verificar el resultado*/
   while (start <= end) {
-      mid = Math.floor((start + end) / 2);
-      
-      if (nums[mid] > target) {
-          end = mid - 1;
-      } else if (nums[mid] < target) {
-          start = mid + 1;
-      } else {
-        /*Aqui se decrementa hasta romper el ciclo*/
-          end = mid - 1;
-      }
+    mid = Math.floor((start + end) / 2);
+
+    if (nums[mid] > target) {
+      end = mid - 1;
+    } else if (nums[mid] < target) {
+      start = mid + 1;
+    } else {
+      /*Aqui se decrementa hasta romper el ciclo*/
+      end = mid - 1;
+    }
   }
   if (start >= 0 && start < len && nums[start] === target) {
-      return start;
+    return start;
   }
   return -1;
 }
 
 function findRightMost(nums, target) {
-  var len = nums.length;
-  var start = 0;
-  var end = len - 1;
-  var mid = 0;
-      
+  let len = nums.length;
+  let start = 0;
+  let end = len - 1;
+
   while (start <= end) {
-      mid = Math.floor((start + end) / 2);
-      if (nums[mid] > target) {
-          end = mid - 1;
-      } else if (nums[mid] < target) {
-          start = mid + 1;
-      } else {
-          start = mid + 1;
-      }
+    let mid = Math.floor((start + end) / 2);
+    if (nums[mid] > target) {
+      end = mid - 1;
+    } else if (nums[mid] < target) {
+      start = mid + 1;
+    } else {
+      start = mid + 1;
+    }
   }
   if (end >= 0 && end < len && nums[end] === target) {
-      return end;
+    return end;
   }
   return -1;
 }
