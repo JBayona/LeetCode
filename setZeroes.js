98256735e0d9e850ec2f9e56d2a01870b54b482f
JBayona <jordavids_22@hotmail.com>
2024-11-05 18:35:45 -0500

    Set Zeroes.

--- setZeroes.js
+++ setZeroes.js
@@ -1,6 +1,5 @@
 /*
 Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
-
 click to show follow up.
 
 https://leetcode.com/problems/set-matrix-zeroes/description/
