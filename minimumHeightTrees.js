960a617079e7bc10ebe1c5b40a4018be43ecc3d4
JBayona <jordavids_22@hotmail.com>
2024-11-10 10:50:57 -0500

    Minimum Height Tree.

--- minimumHeightTrees.js
+++ minimumHeightTrees.js
@@ -3,7 +3,9 @@ For an undirected graph with tree characteristics, we can choose any node as the
 The result graph is then a rooted tree. Among all possible rooted trees, those with
 minimum height are called minimum height trees (MHTs). Given such a graph, write a function
 to find all the MHTs and return a list of their root labels.
+
 Format:
+
 The graph contains n nodes which are labeled from 0 to n - 1. You will be given the number
 n and a list of undirected edges (each edge is a pair of labels).
 
