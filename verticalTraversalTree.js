3947e9d9320e615df8dd3040ae1bc854300f3652
JBayona <jordavids_22@hotmail.com>
2024-11-14 22:48:03 -0500

    Vertical Traversal Tree.

--- verticalTraversalTree.js
+++ verticalTraversalTree.js
@@ -1,7 +1,8 @@
 /*
 Given a binary tree, return the vertical order traversal of its nodes values.
 
-For each node at position (X, Y), its left and right children respectively will be at positions (X-1, Y-1) and (X+1, Y-1).
+For each node at position (X, Y), its left and right children respectively
+will be at positions (X-1, Y-1) and (X+1, Y-1).
 
 Running a vertical line from X = -infinity to X = +infinity, whenever the vertical line touches some nodes, we report the values of the nodes in order from top to bottom (decreasing Y coordinates).
 
