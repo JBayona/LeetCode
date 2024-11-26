78345f33c0e67544a993aa7b1106bf8e23622110
JBayona <jordavids_22@hotmail.com>
2024-11-25 22:40:14 -0600

    Word Pattern.

--- wordPattern.js
+++ wordPattern.js
@@ -2,7 +2,6 @@
 Given a pattern and a string str, find if str follows the same pattern.
 Here follow means a full match, such that there is a bijection between a letter
 in pattern and a non-empty word in str.
-
 Example 1:
 Input: pattern = "abba", str = "dog cat cat dog"
 Output: true
