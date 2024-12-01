46aba2b3323872b39b5b63b460dd1ccd0db1bbb0
JBayona <jordavids_22@hotmail.com>
2024-12-01 14:33:19 -0600

    Minimum Time To Visit Cell BFS.

--- minimumTimeToVisitCellBFSPriorityQueueHard.js
+++ minimumTimeToVisitCellBFSPriorityQueueHard.js
@@ -6,8 +6,7 @@ you can visit the cell (row, col) only when the time you visit it is greater tha
 You are standing in the top-left cell of the matrix in the 0th second, and you must move to any
 adjacent cell in the four directions: up, down, left, and right. Each move you make takes 1 second.
 
-Return the minimum time required in which you can visit the bottom-right cell of the matrix. If you cannot
-visit the bottom-right cell, then return -1.
+Return the minimum time required in which you can visit the bottom-right cell of the matrix. If you cannot visit the bottom-right cell, then return -1.
 
 Example 1:
 Input: grid = [[0,1,3,2],[5,1,2,5],[4,3,8,6]]
