c3b796213e139a580a3a559487b9475aecaf27fc
JBayona <jordavids_22@hotmail.com>
2024-12-02 21:08:18 -0600

    Adding Spaces To A String.

---
+++ addingSpacesToAString.js
@@ -0,0 +1,70 @@
+/*
+You are given a 0-indexed string s and a 0-indexed integer array spaces that describes the indices in
+the original string where spaces will be added. Each space should be inserted before the character at the
+given index.
+
+For example, given s = "EnjoyYourCoffee" and spaces = [5, 9], we place spaces before 'Y' and 'C', which are at
+indices 5 and 9 respectively. Thus, we obtain "Enjoy Your Coffee".
+Return the modified string after the spaces have been added.
+
+Example 1:
+Input: s = "LeetcodeHelpsMeLearn", spaces = [8,13,15]
+Output: "Leetcode Helps Me Learn"
+Explanation: 
+The indices 8, 13, and 15 correspond to the underlined characters in "LeetcodeHelpsMeLearn".
+We then place spaces before those characters.
+
+Example 2:
+Input: s = "icodeinpython", spaces = [1,5,7,9]
+Output: "i code in py thon"
+Explanation:
+The indices 1, 5, 7, and 9 correspond to the underlined characters in "icodeinpython".
+We then place spaces before those characters.
+
+Example 3:
+Input: s = "spacing", spaces = [0,1,2,3,4,5,6]
+Output: " s p a c i n g"
+Explanation:
+We are also able to place spaces before the first character of the string.
+*/
+// Option 1
+// Time O(N)
+// Space O(N)
+var addSpaces = function (s, spaces) {
+  let n = s.length;
+  let m = spaces.length;
+  let arr = new Array(n + m);
+
+  let index = 0;
+  let index2 = 0;
+  for (let i = 0; i < s.length; i++) {
+    let c = s[i];
+    if (index2 < m && index === spaces[index2]) {
+      arr[index + index2] = " ";
+      index2++;
+    }
+    arr[index + index2] = c;
+    index++;
+  }
+  return arr.join("");
+};
+
+// Option 2
+// Time O(N)
+// Space O(N)
+// var addSpaces = function(s, spaces) {
+//     let map = {};
+//     for (let space of spaces) {
+//         map[space] = true;
+//     }
+
+//     let result = '';
+//     for (let i = 1; i <= s.length; i++) {
+//         let c = s[i - 1];
+//         if (i - 1 in map) {
+//             result += ' ';
+//         }
+//         result += c;
+//     }
+//     return result;
+// };
