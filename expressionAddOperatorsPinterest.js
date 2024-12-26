6aa3dc56a9540fa30940c3b080ac8b094e056fb8
JBayona <jordavids_22@hotmail.com>
2024-12-26 16:41:02 -0600

    Expression Evaluate Add Operator.

--- expressionAddOperatorsPinterest.js
+++ expressionAddOperatorsPinterest.js
@@ -3,19 +3,18 @@ Given a string that contains only digits 0-9 and a target value, return all poss
 between the digits so they evaluate to the target value.
 
 Example 1:
-
 Input: num = "123", target = 6
 Output: ["1+2+3", "1*2*3"] 
-Example 2:
 
+Example 2:
 Input: num = "232", target = 8
 Output: ["2*3+2", "2+3*2"]
-Example 3:
 
+Example 3:
 Input: num = "105", target = 5
 Output: ["1*0+5","10-5"]
-Example 4:
 
+Example 4:
 Input: num = "00", target = 0
 Output: ["0+0", "0-0", "0*0"]
 Example 5:
