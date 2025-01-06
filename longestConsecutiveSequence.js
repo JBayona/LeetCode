bbafaf66f56ca7df3d4e22eca534fb5cb50fda73
JBayona <jordavids_22@hotmail.com>
2025-01-05 19:43:59 -0600

    Longest Consecutive Sequence.

--- longestConsecutiveSequence.js
+++ longestConsecutiveSequence.js
@@ -1,6 +1,11 @@
 /*
-Given an unsorted array of integers, find the length of the longest
-consecutive elements sequence.
+Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+For example,
+Given [100, 4, 200, 1, 3, 2],
+The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
+Your algorithm should run in O(n) complexity.
+
+https://leetcode.com/problems/longest-consecutive-sequence/description/
 
 For example:
 Given [100,4,200,1,3,2] the longest consecutive sequence is [1,2,3,4];
