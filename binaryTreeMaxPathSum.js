c281b599de5646dd6b14992c58aa72170d2e3e3a
JBayona <jordavids_22@hotmail.com>
2025-01-29 22:39:19 -0500

    Binary Tree Max Path Sum.

--- binaryTreeMaxPathSum.js
+++ binaryTreeMaxPathSum.js
@@ -3,6 +3,7 @@ Given a non-empty binary tree, find the maximum path sum.
 
 For this problem, a path is defined as any sequence of nodes from some starting node to any node
 in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
+
 Example 1:
 Input: [1,2,3]
        1
@@ -20,7 +21,6 @@ Input: [-10,9,20,null,null,15,7]
 Output: 42
 
 https://leetcode.com/problems/binary-tree-maximum-path-sum/
-
 */
 
 // Opción 1
