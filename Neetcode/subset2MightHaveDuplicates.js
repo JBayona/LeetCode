06288eac7dc0048e5f88e8664a462a3874cd6cd1
JBayona <jordavids_22@hotmail.com>
2025-02-02 19:58:10 -0500

    Subset Might Have Duplicates.

--- Neetcode/subset2MightHaveDuplicates.js
+++ Neetcode/subset2MightHaveDuplicates.js
@@ -1,11 +1,9 @@
 /*
 Given a collection of integers that might contain duplicates, nums, return all possible subsets.
-
 Note: The solution set must not contain duplicate subsets.
 
 For example,
 If nums = [1,2,2], a solution is:
-
 [
   [2],
   [1],
@@ -17,7 +15,6 @@ If nums = [1,2,2], a solution is:
 
 https://leetcode.com/problems/subsets-ii/description/
 */
-
 // Time O(2^N)
 // Space O(N)
 var subsetsWithDup = function (nums) {
