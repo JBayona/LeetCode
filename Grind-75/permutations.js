686b4b94ec4f35f939aa25ede9851b7c0444228b
JBayona <jordavids_22@hotmail.com>
2025-02-02 19:34:05 -0500

    Permutations.

--- Grind-75/permutations.js
+++ Grind-75/permutations.js
@@ -16,66 +16,67 @@ https://leetcode.com/problems/permutations/description/
 Reference: https://leetcode.com/problems/permutations/solutions/18239/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partioning
 */
 
-// Option 1
-// Backtrack
-var permute = function(nums) {
+// Time O(N!)
+// Space O(N)
+var permute = function (nums) {
   let result = [];
   let tmp = [];
-  helper(tmp, result, nums);
+  dfs(result, tmp, nums);
   return result;
 };
 
-function helper(tmp, result, nums) {
-  if(tmp.length == nums.length) {
-      result.push(tmp);
-      return;
+function dfs(result, tmp, nums) {
+  if (tmp.length === nums.length) {
+    result.push(tmp);
+    return;
   }
-  for(let i = 0; i < nums.length; i++) {
-      if(!tmp.includes(nums[i])) {
-          tmp.push(nums[i]);
-          helper(tmp.concat(), result, nums);
-          tmp.pop();
-      }
+  for (let i = 0; i < nums.length; i++) {
+    if (tmp.includes(nums[i])) {
+      continue;
+    }
+    tmp.push(nums[i]);
+    dfs(result, tmp.concat(), nums);
+    tmp.pop();
   }
 }
 
 // Option 2
 //Permutacion Heaps Algorithm
-var permute = function(nums){
+var permute = function (nums) {
   var result = [];
   heapPermutation(nums, nums.length, nums.length, result);
   return result;
-}
+};
 
 //Generating permutation using Heap Algorithm
-function heapPermutation(a, size, n, result){
-    // if size becomes 1 then prints the obtained
-    // permutation
-    if (size == 1){
-      result.push([...a]);
-      //console.log(a);
+function heapPermutation(a, size, n, result) {
+  // if size becomes 1 then prints the obtained
+  // permutation
+  if (size == 1) {
+    result.push([...a]);
+    //console.log(a);
+  }
+
+  for (var i = 0; i < size; i++) {
+    heapPermutation(a, size - 1, n, result);
+
+    // if size is odd, swap first and last
+    // element
+    if (size % 2 == 1) {
+      var temp = a[0];
+      a[0] = a[size - 1];
+      a[size - 1] = temp;
     }
 
-    for (var i=0; i<size; i++){
-        heapPermutation(a, size-1, n, result);
-
-        // if size is odd, swap first and last
-        // element
-        if (size % 2 == 1){
-            var temp = a[0];
-            a[0] = a[size-1];
-            a[size-1] = temp;
-        }
-
-        // If size is even, swap ith and last
-        // element
-        else{
-            var temp = a[i];
-            a[i] = a[size-1];
-            a[size-1] = temp;
-        }
+    // If size is even, swap ith and last
+    // element
+    else {
+      var temp = a[i];
+      a[i] = a[size - 1];
+      a[size - 1] = temp;
     }
+  }
 }
 
-array = [1,2,3];
+array = [1, 2, 3];
 console.log(permute(array));
