6ccbf914f3814a2514d220cdfac03547287d7abb
JBayona <jordavids_22@hotmail.com>
2025-02-03 21:21:14 -0500

    Palindrome Partitioning.

--- Neetcode/palindromePartitioningBacktrack.js
+++ Neetcode/palindromePartitioningBacktrack.js
@@ -10,47 +10,46 @@ Output:
 ]
 https://leetcode.com/problems/palindrome-partitioning/
 */
+
+// Backtrack
 // Time O(n^2)
-var partition = function(s) {
-    let result = [];
-    findPossiblePalindrome(s, result, [], 0);
-    return result;
+var partition = function (s) {
+  let result = [];
+  findPossiblePalindrome(s, result, [], 0);
+  return result;
 };
 
 const findPossiblePalindrome = (str, result, tmp, index) => {
-    // Means we have reached the end of the string and it's a valid answer
-    if(index === str.length) {
-        // tmp has all the cuts we are getting with the combinations
-        result.push([...tmp]);
-        return;
+  // Means we have reached the end of the string and it's a valid answer
+  if (index === str.length) {
+    // tmp has all the cuts we are getting with the combinations
+    result.push([...tmp]);
+    return;
+  }
+  // Check all the possible options
+  for (let i = index; i < str.length; i++) {
+    // Verify if the string is a palindrome starting from index to all
+    // the characters from the string
+    if (isPalindrome(str, index, i)) {
+      // If it's a palindrome, add it to tmp and check all possible combinations
+      // taking the next character of the string
+      tmp.push(str.substring(index, i + 1));
+      findPossiblePalindrome(str, result, tmp, i + 1);
+      // Remove the string that we already check in our combinations
+      // Backtrack
+      tmp.pop();
     }
-    // Check all the possible options
-    for(let i = index; i < str.length; i++) {
-        // Verify if the string is a palindrome starting from index to all
-        // the characters from the string
-        if(isPalindrome(str, index, i)) {
-            // If it's a palindrome, add it to tmp and check all possible combinations
-            // taking the next character of the string
-            tmp.push(str.substring(index, i + 1));
-            findPossiblePalindrome(str, result, tmp, i + 1);
-            // Remove the string that we already check in our combinations
-            // Backtrack
-            tmp.pop();
-        }
-    }
-}
+  }
+};
 
 const isPalindrome = (str, indexA, indexB) => {
-    while(indexA <= indexB) {
-        if(str[indexA] === str[indexB]) {
-            indexA++;
-            indexB--;
-        } else {
-            return false;
-        }
+  while (indexA <= indexB) {
+    if (str[indexA] === str[indexB]) {
+      indexA++;
+      indexB--;
+    } else {
+      return false;
     }
-    return true;
-}
-
-let str = "aab";
-console.log(partition(str));
+  }
+  return true;
+};
