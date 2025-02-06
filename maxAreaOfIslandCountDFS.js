a83990f885a0a887f75e76f3d8545f8a5c322845
JBayona <jordavids_22@hotmail.com>
2025-02-05 23:38:41 -0500

    Max Area Island Count.

--- maxAreaOfIslandCountDFS.js
+++ maxAreaOfIslandCountDFS.js
@@ -4,7 +4,6 @@ Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (rep
 Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
 
 Example 1:
-
 [[0,0,1,0,0,0,0,1,0,0,0,0,0],
  [0,0,0,0,0,0,0,1,1,1,0,0,0],
  [0,1,1,0,1,0,0,0,0,0,0,0,0],
@@ -16,7 +15,6 @@ Example 1:
 Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
 
 Example 2:
-
 [[0,0,0,0,0,0,0,0]]
 Given the above grid, return 0.
 Note: The length of each dimension in the given grid does not exceed 50.
