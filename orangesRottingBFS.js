e781b0fefaabd203ece88a934269c79e58a48a27
JBayona <jordavids_22@hotmail.com>
2025-02-07 21:32:47 -0500

    Orange Rotting.

--- orangesRottingBFS.js
+++ orangesRottingBFS.js
@@ -6,7 +6,6 @@ the value 2 - representing a rotten orange.
 
 Every minute, any fresh orange that is adjacent (4-directionally) to a rotten
 orange becomes rotten.
-
 Return the minimum number of minutes that must elapse until no cell has a fresh orange. 
 If this is impossible, return -1 instead.
 
