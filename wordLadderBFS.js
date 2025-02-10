c3e09b057d4e049796dfb83faf9a04acfa1b1f5a
JBayona <jordavids_22@hotmail.com>
2025-02-09 19:07:45 -0500

    Word Ladder.

--- wordLadderBFS.js
+++ wordLadderBFS.js
@@ -4,7 +4,6 @@ transformation sequence from beginWord to endWord, such that:
 Only one letter can be changed at a time.
 
 Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
-
 Note:
 Return 0 if there is no such transformation sequence.
 All words have the same length.
