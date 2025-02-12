cd408cb9bb60bf14004bf560e56b41f87b073149
JBayona <jordavids_22@hotmail.com>
2025-02-11 21:43:29 -0500

    Find Cheapest Flight Withink K Stops.

--- findCheapestFlightsWithinKStopsGraphBFS.js
+++ findCheapestFlightsWithinKStopsGraphBFS.js
@@ -1,5 +1,6 @@
 /*
-There are n cities connected by m flights. Each flight starts from city u and arrives at v with a price w.
+There are n cities connected by m flights. Each flight starts from city u
+and arrives at v with a price w.
 
 Now given all the cities and flights, together with starting city src and the destination dst, your task is to
 find the cheapest price from src to dst with up to k stops. If there is no such route, output -1.
@@ -12,8 +13,9 @@ Output: 200
 Explanation: 
 The graph looks like this:
 
+The cheapest price from city 0 to city 2 with at most 1 stop costs 200, as marked
+red in the picture.
 
-The cheapest price from city 0 to city 2 with at most 1 stop costs 200, as marked red in the picture.
 Example 2:
 Input: 
 n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
@@ -22,12 +24,10 @@ Output: 500
 Explanation: 
 The graph looks like this:
 
-
 The cheapest price from city 0 to city 2 with at most 0 stop costs 500, as marked blue in the picture.
 
 https://leetcode.com/problems/cheapest-flights-within-k-stops/
 */
-
 // Time O(V + E)
 // Space O(V)
 var findCheapestPrice = function(n, flights, src, dst, k) {
