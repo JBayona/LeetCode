ccffd5e841c6439eaaa66a86baf6496f97d72040
JBayona <jordavids_22@hotmail.com>
2025-02-12 21:53:00 -0500

    Merge Interval.

--- mergeIntervals.js
+++ mergeIntervals.js
@@ -13,7 +13,6 @@ Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 
 https://leetcode.com/problems/merge-intervals/
 */
-
 /**
  * @param {number[][]} intervals
  * @return {number[][]}
