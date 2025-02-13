58ba6b7eb926efd948b0f3913dac7937d8112a03
JBayona <jordavids_22@hotmail.com>
2025-02-12 21:56:01 -0500

    Non Overlapping Intervals.

--- nonOverlappingIntervals.js
+++ nonOverlappingIntervals.js
@@ -5,7 +5,6 @@ minimum number of intervals you need to remove to make the rest of the intervals
 Example 1:
 Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
 Output: 1
-
 Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
 
 Example 2:
@@ -20,7 +19,6 @@ Explanation: You don't need to remove any of the intervals since they're already
 
 https://leetcode.com/problems/non-overlapping-intervals/
 */
-
 // Time O(N)
 var eraseOverlapIntervals = function(intervals) {
     // Sorted based on the start interval
