f013efabc04bd5ec590a7f733c6b7f0e22dd039d
JBayona <jordavids_22@hotmail.com>
2025-02-16 15:31:22 -0500

    Basic Calculator 3 Parenthesis.

---
+++ basicCalculator3ParenthesisAndOrderexpression.js
@@ -0,0 +1,93 @@
+/*
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string contains only non-negative integers, '+', '-', '*', '/' operators
+and open '(' and closing parentheses ')'. The integer division should truncate toward zero.
+
+You may assume that the given expression is always valid. All intermediate results will be in the range of [-231, 231 - 1].
+
+Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+ 
+Example 1:
+Input: s = "1+1"
+Output: 2
+
+Example 2:
+Input: s = "6-4/2"
+Output: 4
+
+Example 3:
+
+Input: s = "2*(5+5*2)/3+(6/2+8)"
+Output: 21
+
+https://leetcode.com/problems/basic-calculator-iii/description/
+*/
+
+// Time O(2^N)
+var calculate = function (s) {
+  return helper(s.split(""));
+};
+
+var helper = function (tokens) {
+  if (tokens.length === 0) {
+    return 0;
+  }
+  // Remove spaces around string
+  let stack = [];
+  let num = 0;
+  let sign = "+";
+
+  while (tokens.length) {
+    let elem = tokens.shift();
+    // No blank space needed
+    if (elem === " ") {
+      continue;
+    }
+    // If it´s a digit
+    if (!isNaN(elem)) {
+      // For consecutive digits 98 => 9x10 + 8 = 98
+      num = num * 10 + Number(elem);
+    }
+
+    if (elem === "(") {
+      // Remove the open parenthesis
+      num = helper(tokens);
+    }
+
+    if (isNaN(elem) || tokens.length === 0) {
+      // Push the initial number into the stack
+      switch (sign) {
+        case "+":
+          stack.push(num);
+          break;
+        case "-":
+          stack.push(-num);
+          break;
+        case "*":
+          stack.push(stack.pop() * num);
+          break;
+        case "/":
+          stack.push(parseInt(stack.pop() / num));
+          break;
+      }
+      sign = elem;
+      num = 0;
+    }
+
+    // Break for as we already computed the element
+    if (elem === ")") {
+      break;
+    }
+  }
+  console.log(stack);
+  let result = 0;
+  result = stack.reduce((a, b) => a + b);
+  return result;
+};
+
+// let s = "1 + 1"; // 2
+// let s = "6-4/2"; // 4
+// let s = "2*(5+5*2)/3+(6/2+8)"; // 21
+let s = "(2+6*3+5-(3*14/7+2)*5)+3";
+console.log(calculate(s));
