396a5c4a990570562d608bdb021486a69ec405fb
JBayona <jordavids_22@hotmail.com>
2025-02-17 22:46:35 -0500

    Check If One String Swap Can Make String.

--- checkIfOneStringSwapCanMakeStringEqualDoordash.js
+++ checkIfOneStringSwapCanMakeStringEqualDoordash.js
@@ -1,7 +1,6 @@
 /*
 You are given two strings s1 and s2 of equal length. A string swap is an operation where you
 choose two indices in a string (not necessarily different) and swap the characters at these indices.
-
 Return true if it is possible to make both strings equal by performing at most one string swap on
 exactly one of the strings. Otherwise, return false.
 
@@ -22,7 +21,6 @@ Explanation: The two strings are already equal, so no string swap operation is r
 
 https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/description/
 */
-
 // Time O(N)
  // Space O(26) = O(1)
  var areAlmostEqual = function(s1, s2) {
