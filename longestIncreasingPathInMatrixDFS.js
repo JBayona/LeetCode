8c8ef0331aeb32711fdb818d9d71331e9aed421c
JBayona <jordavids_22@hotmail.com>
2025-02-17 22:28:59 -0500

    Longest Increasing Path In Matrix DFS.

--- longestIncreasingPathInMatrixDFS.js
+++ longestIncreasingPathInMatrixDFS.js
@@ -13,50 +13,51 @@ https://leetcode.com/problems/longest-increasing-path-in-a-matrix/description/
 // has been processed previously, we can use memoization to hold values.
 // For each iteration try to get the max path.
 var longestIncreasingPath = function(matrix) {
+    if (matrix.lenght) {
+        return -1;
+    }
+
     let ROW = matrix.length;
     let COL = matrix[0].length;
     
-    let memo = new Array(ROW).fill(0);
-    for(let i = 0; i < ROW; i++) {
+    let memo = new Array(ROW);
+    for (let i = 0; i < ROW; i++) {
         memo[i] = new Array(COL).fill(0);
     }
-    
+
     let max = 0;
-    for(let i = 0; i < ROW; i++){
-        for(let j = 0; j < COL; j++){
-            // Try to maximize the length for every position
+    for (let i = 0; i < ROW; i++) {
+        for (let j = 0; j < COL; j++) {
             max = Math.max(max, dfs(matrix, memo, i, j));
         }
     }
     return max;
 };
 
-function dfs(matrix, memo, row, col){
-    // Already cached
+function dfs(matrix, memo, row, col) {
+    // If the element has been computed already
     if (memo[row][col] !== 0) {
         return memo[row][col];
     }
 
+    let rowK = [0, -1, 0, 1];
+    let colK = [-1, 0, 1, 0];
     let max = 1;
-
-    let rowK = [ 0, -1, 0, 1];
-    let colK = [-1,  0, 1, 0];
-
-    for(let i = 0; i < 4; i++){            
+    for (let i = 0; i < 4; i++) {
         let nextRow = row + rowK[i];
         let nextCol = col + colK[i];
-
-        if (isSafe(nextRow, nextCol, matrix) && matrix[nextRow][nextCol] < matrix[row][col]) {
+        if (isSafe(matrix, nextRow, nextCol) && matrix[nextRow][nextCol] < matrix[row][col]) {
             max = Math.max(max, 1 + dfs(matrix, memo, nextRow, nextCol));
         }
-    }    
+    }
+    // Save in the memo for the max path at that point
     memo[row][col] = max;
     return memo[row][col];
 }
 
-function isSafe(row, col, grid) {
-    let ROW = grid.length;
-    let COL = grid[0].length;
+function isSafe(matrix, row, col) {
+    let ROW = matrix.length;
+    let COL = matrix[0].length;
     return (
         row >= 0 && row < ROW &&
         col >= 0 && col < COL
