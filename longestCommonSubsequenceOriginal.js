369649c6e4dca17368cb8e0491483240b47eb648
JBayona <jordavids_22@hotmail.com>
2025-02-19 22:22:14 -0500

    Longest Common Subsequence.

--- longestCommonSubsequenceOriginal.js
+++ longestCommonSubsequenceOriginal.js
@@ -1,20 +1,18 @@
 /*
 Example 1:
-
 Input: text1 = "abcde", text2 = "ace" 
 Output: 3  
 Explanation: The longest common subsequence is "ace" and its length is 3.
-Example 2:
 
+Example 2:
 Input: text1 = "abc", text2 = "abc"
 Output: 3
 Explanation: The longest common subsequence is "abc" and its length is 3.
-Example 3:
 
+Example 3:
 Input: text1 = "abc", text2 = "def"
 Output: 0
-Explanation: There is no such common subsequence, so the result is 0.
- 
+Explanation: There is no such common subsequence, so the result is 0. 
 
 Constraints:
 
