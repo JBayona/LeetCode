44d2c6d86a4d2389f1b2721355f195b0ff2c1290
JBayona <jordavids_22@hotmail.com>
2025-02-22 18:00:23 -0500

    Ways To Make Fair Array Even Odd Prefix Sum.

--- waysToMakeFairArrayEvenOddPrefixSum.js
+++ waysToMakeFairArrayEvenOddPrefixSum.js
@@ -1,6 +1,5 @@
 /*
 You are given an integer array nums. You can choose exactly one index (0-indexed) and remove the element.
-
 Notice that the index of the elements may change after the removal.
 For example, if nums = [6,1,7,4,1]:
 
