733fd10ff50aae31889609c4f327bcac161a327d
JBayona <jordavids_22@hotmail.com>
2025-03-16 19:01:48 -0400

    Lowest Common Ancestor.

--- lowestCommonAncestorBinarySearchTree.js
+++ lowestCommonAncestorBinarySearchTree.js
@@ -2,9 +2,9 @@
 Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
 
 According to the definition of LCA on Wikipedia:
-“The lowest common ancestor is defined between two nodes p and q as the lowest
+The lowest common ancestor is defined between two nodes p and q as the lowest
 node in T that has both p and q as descendants.
-(where we allow a node to be a descendant of itself).”
+(where we allow a node to be a descendant of itself).
 
 Example 1:
 Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
