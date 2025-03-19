3cd6306c22b3b3f3509705befac2438add3e082a
JBayona <jordavids_22@hotmail.com>
2025-03-18 22:47:43 -0400

    K Closest Point To Origin.

--- kClosestPointsToOrigin.js
+++ kClosestPointsToOrigin.js
@@ -1,6 +1,5 @@
 /*
 Example 1:
-
 Input: points = [[1,3],[-2,2]], K = 1
 Output: [[-2,2]]
 Explanation: 
@@ -8,8 +7,8 @@ The distance between (1, 3) and the origin is sqrt(10).
 The distance between (-2, 2) and the origin is sqrt(8).
 Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
 We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
-Example 2:
 
+Example 2:
 Input: points = [[3,3],[5,-1],[-2,4]], K = 2
 Output: [[3,3],[-2,4]]
 (The answer [[-2,4],[3,3]] would also be accepted.)
