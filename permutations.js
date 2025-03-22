fb8d0e633d3cc5c145814854924c5d40ff2a89d9
JBayona <jordavids_22@hotmail.com>
2025-03-22 16:54:21 -0400

    Permutations.

--- permutations.js
+++ permutations.js
@@ -15,6 +15,36 @@ https://leetcode.com/problems/permutations/description/
 Reference: https://leetcode.com/problems/permutations/solutions/18239/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partioning
 */
 
+// Option 1
+// Time O(N!)
+// Space O(N)
+var permute = function (nums) {
+  let result = [];
+  let tmp = [];
+  let set = new Set();
+  dfs(result, tmp, nums, set);
+  return result;
+};
+
+function dfs(result, tmp, nums, set) {
+  if (tmp.length === nums.length) {
+    result.push(tmp);
+    return;
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      continue;
+    }
+    set.add(nums[i]);
+    tmp.push(nums[i]);
+    dfs(result, tmp.concat(), nums, set);
+    tmp.pop();
+    set.delete(nums[i]);
+  }
+}
+
+
+// Option 3
 // Time O(N!)
 // Space O(N)
 var permute = function (nums) {
