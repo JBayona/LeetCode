3175d605d42897a28014b7b7b28023daccad8e13
JBayona <jordavids_22@hotmail.com>
2025-03-30 18:36:54 -0400

    Resumable Iterator.

---
+++ resumableIterator.js
@@ -0,0 +1,33 @@
+class ResumableIterator {
+  constructor(data) {
+    this.index = 0;
+    this.data = data;
+  }
+  next() {
+    if (this.index < this.data.length) {
+      return {value: this.data[this.index++], done: false};
+    }
+    return {done: true};
+  }
+  reset() {
+    this.index = 0;
+  }
+  getIndex() {
+    return this.index;
+  }
+  hasNext() {
+    return this.index < this.data.length;
+  }
+}
+
+
+const iterator = new ResumableIterator([10, 20, 30, 40]);
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.hasNext());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.hasNext());
+console.log(iterator.reset());
+console.log(iterator.next());
+console.log(iterator.next());
