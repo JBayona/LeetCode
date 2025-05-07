efa63e10029f3179c7111c997230d519262d9208
JBayona <jordavids_22@hotmail.com>
2025-05-06 22:04:25 -0400

    Binary Search Tree Strictly Greater.

--- binarySearchTreeDifference.js
+++ binarySearchTreeDifference.js
@@ -35,3 +35,22 @@ function binarySearchMatch(arr, target) {
 }
 // Used when array has duplicates.
 // Returns insertion position.
+
+// 3) First Occurence > target
+// Finds the first index where the element is strictly greater than the target.
+function binarySearchMatch(arr, target) {
+    let left = 0;
+    let right = arr.length;
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        // mid = left + (right - left) // 2  # avoids overflow
+        if (arr[mid] <= target) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return left; // May return len(arr) if target > all numbers
+}
+// Used when array has duplicates.
+// Returns insertion position.
