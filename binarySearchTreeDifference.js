b221bbd23c0c3f7a91bfac2e301416fbbbf9b0a6
JBayona <jordavids_22@hotmail.com>
2025-05-06 21:58:12 -0400

    Binary Search Match.

---
+++ binarySearchTreeDifference.js
@@ -0,0 +1,18 @@
+// 1) Standar and Exact Match
+
+function binarySearchMatch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        // mid = left + (right - left) // 2  # avoids overflow
+        if (arr[mid] === target) {
+            return mid;
+        } else if(arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
