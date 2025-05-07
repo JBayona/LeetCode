1771e5c4e5c9ac6893e6e2ad8f4d116e9a4001d3
JBayona <jordavids_22@hotmail.com>
2025-05-06 22:11:50 -0400

    Binary Search Continue Right.

--- binarySearchTreeDifference.js
+++ binarySearchTreeDifference.js
@@ -55,7 +55,7 @@ function binarySearchFirstGreater(arr, target) {
 // Used when array has duplicates.
 // Returns insertion position.
 
-// 4) First First Occurence of Target
+// 4) Left First Occurence of Target
 // Like standard binary search but continues searching to the left after finding the target.
 function binarySearchContinueMatch(arr, target) {
     let left = 0;
@@ -67,6 +67,27 @@ function binarySearchContinueMatch(arr, target) {
         if (arr[mid] === target) {
             result = mid;
             right = mid - 1; // Continue to left
+        }else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return result;
+}
+
+// 5) Last Occurence of Target
+// Similar, but go right after a match.
+function binarySearchContinueMatch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    let result = null;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        // mid = left + (right - left) // 2  # avoids overflow
+        if (arr[mid] === target) {
+            result = mid;
+            right = mid + 1; // Continue to right
         }else if (arr[mid] < target) {
             left = mid + 1;
         } else {
