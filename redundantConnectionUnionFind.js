807504a6cdfe610eef551e7ed93602dd7bf6f295
JBayona <jordavids_22@hotmail.com>
2025-05-10 18:19:55 -0400

    Redundant Connection Union Find.

--- redundantConnectionUnionFind.js
+++ redundantConnectionUnionFind.js
@@ -38,7 +38,6 @@ For the directed graph follow up please see Redundant Connection II). We apologi
 
 https://leetcode.com/problems/redundant-connection/
 */
-
 // Union find
 // Time O(N)
 var findRedundantConnection = function(edges) {
