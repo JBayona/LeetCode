bdbf38b86953c55fa7886a881ee4153eef8eb101
JBayona <jordavids_22@hotmail.com>
2025-05-10 17:20:17 -0400

    Course Schedule Graph.

--- courseScheduleGraph.js
+++ courseScheduleGraph.js
@@ -22,7 +22,6 @@ have finished course 1. So it is impossible.
 
 https://leetcode.com/problems/course-schedule/
 */
-
 // Time O(V + E)
 // Space O(V + E)
 var canFinish = function(numCourses, prerequisites) {
