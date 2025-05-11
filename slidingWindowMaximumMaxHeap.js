926a1720a922dffb19f0a2c29622bced231ab0b4
JBayona <jordavids_22@hotmail.com>
2025-05-11 17:46:54 -0400

    Sliding Window Maximum Max Heap.

--- slidingWindowMaximumMaxHeap.js
+++ slidingWindowMaximumMaxHeap.js
@@ -7,6 +7,7 @@ Return the max sliding window.
 Example 1:
 Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
 Output: [3,3,5,5,6,7]
+
 Explanation: 
 Window position                Max
 ---------------               -----
