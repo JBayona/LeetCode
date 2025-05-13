21b589b08b87f0e0278758f2c3c82b72d22bc89d
Jorge <jordavids_22@hotmail.com>
2025-05-12 21:38:33 -0400

    Can Reach Add Operator Multiplication Subset Nextdoor.

--- canReachAddOperatorMultiplicationSubsetNextdoor.js
+++ canReachAddOperatorMultiplicationSubsetNextdoor.js
@@ -11,6 +11,7 @@ There's no constraint on the order, it could either start from left to right or 
  *  -> [1*2]+[3]
  *  -> [1]+[2*3]
 /
+
 // Time O(2^n)
 function canReachTargetSubset(nums, target) {
   const subsets = generateSubsets(nums, 0, new Set([1])); // Start with 1 to avoid multiplication by 0
