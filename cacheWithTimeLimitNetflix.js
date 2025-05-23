91e643930eb22613de7b047329b6e4424e67a278
JBayona <jordavids_22@hotmail.com>
2025-05-23 15:09:28 -0400

    Cache With Time Limit Netflix.

--- cacheWithTimeLimitNetflix.js
+++ cacheWithTimeLimitNetflix.js
@@ -2,7 +2,6 @@
 Write a class that allows getting and setting key-value pairs, however a time until expiration is associated with each key.
 
 The class has three public methods:
-
 set(key, value, duration): accepts an integer key, an integer value, and a duration in milliseconds.
 Once the duration has elapsed, the key should be inaccessible. The method should return true if the same un-expired key already
 exists and false otherwise. Both the value and duration should be overwritten if the key already exists.
