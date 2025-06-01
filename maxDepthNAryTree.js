e2f61a6453345fb9c97fb57d62c4539a75f4736c
JBayona <jordavids_22@hotmail.com>
2025-05-31 21:47:45 -0400

    Max Depth N Ary Tree.

--- maxDepthNAryTree.js
+++ maxDepthNAryTree.js
@@ -9,15 +9,14 @@ is separated by the null value (See examples).
 
 https://leetcode.com/problems/maximum-depth-of-n-ary-tree/description/
 */
-
-var maxDepth = function(root) {
-  if(!root) {
-      return 0;
+// Time O(N)
+var maxDepth = function (root) {
+  if (!root) {
+    return 0;
   }
-  let result = 0;
-  for(let node of root.children) {
-      let tmp = maxDepth(node)
-      result = Math.max(result, tmp);
+  let max = 0;
+  for (let neighbor of root.children) {
+    max = Math.max(max, maxDepth(neighbor));
   }
-  return result + 1;
+  return max + 1;
 };
