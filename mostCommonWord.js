550eaba946197fd89871d5519217dcae6b802462
JBayona <jordavids_22@hotmail.com>
2025-05-31 22:48:16 -0400

    Most Common Word.

--- mostCommonWord.js
+++ mostCommonWord.js
@@ -1,12 +1,12 @@
 /*
-Given a paragraph and a list of banned words, return the most frequent word that is not in the list of banned words.  It is guaranteed there is at least one word that isn't banned, and that the answer is unique.
+Given a paragraph and a list of banned words, return the most frequent word that is not in
+the list of banned words.  It is guaranteed there is at least one word that isn't banned, and that
+the answer is unique.
 
-Words in the list of banned words are given in lowercase, and free of punctuation.  Words in the paragraph are not case sensitive.  The answer is in lowercase.
-
- 
+Words in the list of banned words are given in lowercase, and free of punctuation. 
+Words in the paragraph are not case sensitive.  The answer is in lowercase.
 
 Example:
-
 Input: 
 paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
 banned = ["hit"]
