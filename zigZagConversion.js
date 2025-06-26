a5d4f6654f58902efeea43bceb1191dd36a7d8c8
Jorge <jordavids_22@hotmail.com>
2025-06-25 23:54:37 -0400

    Zig Zag Conversion.

--- zigZagConversion.js
+++ zigZagConversion.js
@@ -14,18 +14,19 @@ string convert(string s, int numRows);
  
 
 Example 1:
-
 Input: s = "PAYPALISHIRING", numRows = 3
 Output: "PAHNAPLSIIGYIR"
-Example 2:
 
+Example 2:
 Input: s = "PAYPALISHIRING", numRows = 4
 Output: "PINALSIGYAHRPI"
+
 Explanation:
 P     I    N
 A   L S  I G
 Y A   H R
 P     I
+
 Example 3:
 
 Input: s = "A", numRows = 1
