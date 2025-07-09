14f29688d432e412f5d35280743694377fcbd9f6
JBayona <jordavids_22@hotmail.com>
2025-07-08 23:08:20 -0400

    Jump Game.

--- jumpGame.js
+++ jumpGame.js
@@ -3,7 +3,6 @@ You are given an integer array nums. You are initially positioned at the array's
 in the array represents your maximum jump length at that position.
 
 Return true if you can reach the last index, or false otherwise.
-
 Example 1:
 Input: nums = [2,3,1,1,4]
 Output: true
