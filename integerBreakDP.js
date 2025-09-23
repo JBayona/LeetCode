a8db8c5d9c6e87dd2a810cacc4a60b8fe4e6b153
Jorge <jordavids_22@hotmail.com>
2025-09-22 20:14:44 -0400

    Integer Break Up.

--- integerBreakDP.js
+++ integerBreakDP.js
@@ -1,7 +1,6 @@
 /*
 Given an integer n, break it into the sum of k positive integers, where k >= 2
 and maximize the product of those integers.
-
 Return the maximum product you can get.
 Example 1:
 Input: n = 2
