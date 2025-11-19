57c2b4c1040c0f297067e0090da6ec47c8c435cb
Jorge <jordavids_22@hotmail.com>
2025-11-18 20:06:08 -0500

    Word Pattern.

--- wordPattern.js
+++ wordPattern.js
@@ -23,7 +23,6 @@ You may assume pattern contains only lowercase letters, and str contains lowerca
 
 https://leetcode.com/problems/word-pattern/
 */
-
 var wordPattern = function(pattern, str) {
     let strArray = str.split(' ');
     
