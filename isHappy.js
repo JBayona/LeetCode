5a1b71156fa479777fed312eb596ec8ee746b583
Jorge <jordavids_22@hotmail.com>
2026-01-08 16:40:31 -0500

    Is Happy.

--- isHappy.js
+++ isHappy.js
@@ -7,7 +7,6 @@ until the number equals 1 (where it will stay), or it loops endlessly in a cycle
 not include 1. Those numbers for which this process ends in 1 are happy numbers.
 
 Example: 
-
 Input: 19
 Output: true
 
