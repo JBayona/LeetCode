62da8b2db0702d3b1776f0ed251afdca1e6032f0
JBayona <jordavids_22@hotmail.com>
2026-01-20 22:12:00 -0500

    Valid Anagram.

--- validAnagram.js
+++ validAnagram.js
@@ -16,6 +16,39 @@ https://leetcode.com/problems/valid-anagram/description/
 
 // Time O(N)
 // Space O(N)
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    let map = getFrequency(s);
+    for (let i = 0; i < t.length; t++) {
+        let c = t[i];
+        // Less or equal than zero as this will denote there
+        // are more elements not contained on the map as we already
+        // have a validation that length for both should be equal
+        if (!(c in map) || map[c] <= 0) {
+            return false;
+        }
+        map[c]--;
+    }
+    return true;
+};
+
+function getFrequency(s) {
+    let map = {};
+    for (let i = 0; i < s.length; i++) {
+        let c = s[i];
+        if (!(c in map)) {
+            map[c] = 0;
+        }
+        map[c]++;
+    }
+    return map;
+}
+
+// Time O(N)
+// Space O(N)
 var isAnagram = function (s, t) {
   if (s.length !== t.length) {
     return false;
