1a3cd2364413622d01e9618e66617fde715beaef
JBayona <jordavids_22@hotmail.com>
2026-01-22 22:29:21 -0500

    Two Sum.

--- twoSum.js
+++ twoSum.js
@@ -6,6 +6,7 @@ You may assume that each input would have exactly one solution, and you may not
 use the same element twice.
 
 You can return the answer in any order.
+
 Example 1:
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
