bddf0e72587b0718847f988516db1b4e8be3597d
JBayona <jordavids_22@hotmail.com>
2026-02-13 23:44:44 -0500

    Has Cycle LinkedList.

--- hasCycleLinkedList.js
+++ hasCycleLinkedList.js
@@ -3,7 +3,6 @@ Given a linked list, determine if it has a cycle in it.
 
 https://leetcode.com/problems/linked-list-cycle/description/
 */
-
 var hasCycle = function(head) {
     if (!head) {
         return false;
