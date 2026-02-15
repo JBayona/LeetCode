398786031be63b0cf23e82ab55e29a1227f5cfab
JBayona <jordavids_22@hotmail.com>
2026-02-14 21:40:32 -0500

    Has Path Acyclic.

--- hasPathAcyclic.js
+++ hasPathAcyclic.js
@@ -3,7 +3,6 @@ Has path, given a graph and a source and destination node. Find if it´s possibl
 to destination.
 Assume the graph is acyclic.
 */
-
 // DFS
 const hasPathDFS = (graph, src, dst) => {
   if(src === dst) {
