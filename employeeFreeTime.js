008b8ced00bada2f04e2b1f528b9b2dc01fc9dce
Jorge <jordavids_22@hotmail.com>
2026-03-25 22:52:00 -0400

    Employee Free Time.

--- employeeFreeTime.js
+++ employeeFreeTime.js
@@ -1,8 +1,6 @@
 /*
 We are given a list schedule of employees, which represents the working time for each employee.
-
 Each employee has a list of non-overlapping Intervals, and these intervals are in sorted order.
-
 Return the list of finite intervals representing common, positive-length free time for all employees, also in sorted order.
 
 (Even though we are representing Intervals in the form [x, y], the objects inside are Intervals, not lists or arrays.
