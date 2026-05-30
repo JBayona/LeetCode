b556797f7d8329b2257b4008433fe7103a553080
Jorge <jordavids_22@hotmail.com>
2026-05-30 15:12:35 -0400

    Get Decimal Value.

--- getDecimalValueLinkedList.js
+++ getDecimalValueLinkedList.js
@@ -5,24 +5,23 @@ Given head which is a reference node to a singly-linked list. The value of each 
 Return the decimal value of the number in the linked list.
 
 Example 1:
-
 Input: head = [1,0,1]
 Output: 5
 Explanation: (101) in base 2 = (5) in base 10
-Example 2:
 
+Example 2:
 Input: head = [0]
 Output: 0
-Example 3:
 
+Example 3:
 Input: head = [1]
 Output: 1
-Example 4:
 
+Example 4:
 Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
 Output: 18880
-Example 5:
 
+Example 5:
 Input: head = [0,0]
 Output: 0
 
