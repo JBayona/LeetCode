dd53467cfbc3b86839d06dc726a923f3bb556953
Jorge <jordavids_22@hotmail.com>
2026-06-09 21:42:35 -0400

    Rotate Matrix.

--- rotateMatrix.js
+++ rotateMatrix.js
@@ -6,7 +6,6 @@ right edge to the bottom edge, the bottom edge to the left
 edge and the left edge to the top edge.
 Swap index by index - Complex O(N^2)
 */
-
 // In place.
 function rotate(matrix, n){
   for(var i = 0; i < n/2; i++){
