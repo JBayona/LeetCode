ceb883b7e17808defe835b0c03de94607ee05133
Jorge <jordavids_22@hotmail.com>
2026-06-24 22:06:18 -0400

    Jump Game.

--- jumpGame2.js
+++ jumpGame2.js
@@ -1,6 +1,5 @@
 /*
 You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
-
 Each element nums[i] represents the maximum length of a forward jump from index i. In other words
 if you are at nums[i], you can jump to any nums[i + j] where:
 
