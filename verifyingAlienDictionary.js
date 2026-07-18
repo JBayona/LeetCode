30654f260dcb4f6c7496d7488c4944b98dd800a1
JBayona <jordavids_22@hotmail.com>
2026-07-18 18:08:40 -0400

    Verifying Allien Dictionary.

--- verifyingAlienDictionary.js
+++ verifyingAlienDictionary.js
@@ -6,18 +6,17 @@ Given a sequence of words written in the alien language, and the order of the al
 return true if and only if the given words are sorted lexicographicaly in this alien language.
 
 Example 1:
-
 Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
 Output: true
 Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
-Example 2:
 
+Example 2:
 Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
 Output: false
 Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1]
 hence the sequence is unsorted.
-Example 3:
 
+Example 3:
 Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
 Output: false
 Explanation: The first three characters "app" match, and the second string is
